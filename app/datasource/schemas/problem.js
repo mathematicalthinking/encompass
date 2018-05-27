dca7d80f53ca8a293cdaf2344f81d46fd515e163
Daniel Kelly <djk8552@gmail.com>
2018-05-27 16:55:09 -0400

    Add answer, problem, section schemas

---
+++ app/datasource/schemas/problem.js
@@ -0,0 +1,86 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  _ = require('underscore'),
+  ObjectId = Schema.ObjectId;
+
+/**
+  * @public
+  * @class Problem
+  * @description Problems are submitted by teachers
+  */
+var ProblemSchema = new Schema({
+  //== Shared properties (Because Mongoose doesn't support schema inheritance)
+  createdBy: { type: ObjectId, ref: 'User' },
+  createDate: { type: Date, 'default': Date.now() },
+  isTrashed: { type: Boolean, 'default': false },
+  //====
+  name: { type: String },
+  text: { type: String },
+  image: { type: String },
+  url: { type: String },
+  sectionId: { type: ObjectId, ref: 'Section' },
+  additionalInfo: { type: String },
+  isPublic: { type: Boolean, default: false },
+  access: { groups: [{ type: ObjectId, ref: 'Group' }], individuals: [{ type: ObjectId, ref: 'User' }] }
+}, { versionKey: false });
+
+/**
+  * ## Pre-Validation
+  * Before saving we must verify (synchonously) that:
+  */
+// ProblemSchema.pre('save', function (next) {
+//   var toObjectId = function (elem, ind, arr) {
+//     if (!(elem instanceof mongoose.Types.ObjectId) && !_.isUndefined(elem)) {
+//       arr[ind] = mongoose.Types.ObjectId(elem);
+//     }
+//   };
+
+//   /** + Every ID reference in our object is properly typed. 
+//     *   This needs to be done BEFORE any other operation so
+//     *   that native lookups and updates don't fail.
+//     */
+//   try {
+//     this.selections.forEach(toObjectId);
+//     this.comments.forEach(toObjectId);
+//     next();
+//   }
+//   catch (err) {
+//     next(new Error(err.message));
+//   }
+// });
+
+/**
+  * ## Post-Validation
+  * After saving we must ensure (synchonously) that:
+  */
+// ProblemSchema.post('save', function (Problem) {
+//   var update = { $addToSet: { 'Problems': Problem } };
+//   if (Problem.isTrashed) {
+//     var ProblemIdObj = mongoose.Types.ObjectId(Problem._id);
+//     /* + If deleted, all references are also deleted */
+//     update = { $pull: { 'Problems': ProblemIdObj } };
+//   }
+
+//   if (Problem.workspace) {
+//     mongoose.models.Workspace.update({ '_id': Problem.workspace },
+//       update,
+//       function (err, affected, result) {
+//         if (err) {
+//           throw new Error(err.message);
+//         }
+//       });
+//   }
+
+//   if (Problem.submission) {
+//     mongoose.models.Submission.update({ '_id': Problem.submission },
+//       update,
+//       function (err, affected, result) {
+//         if (err) {
+//           throw new Error(err.message);
+//         }
+//       });
+//   }
+
+// });
+
+module.exports.Problem = mongoose.model('Problem', ProblemSchema);
---
+++ app/datasource/schemas/section.js
@@ -0,0 +1,83 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  _ = require('underscore'),
+  ObjectId = Schema.ObjectId;
+
+/**
+  * @public
+  * @class Section
+  * @description Sections belong to a school and can contain many students, teachers, and problems
+  */
+var SectionSchema = new Schema({
+  //== Shared properties (Because Mongoose doesn't support schema inheritance)
+  createdBy: { type: ObjectId, ref: 'User' },
+  createDate: { type: Date, 'default': Date.now() },
+  isTrashed: { type: Boolean, 'default': false },
+  //====
+  name: { type: String, required: true },
+  schoolId: String,
+  teachers: [{ type: String }],
+  students: [{ type: String }],
+  problems: [{ type: String }],
+}, { versionKey: false });
+
+/**
+  * ## Pre-Validation
+  * Before saving we must verify (synchonously) that:
+  */
+SectionSchema.pre('save', function (next) {
+  var toObjectId = function (elem, ind, arr) {
+    if (!(elem instanceof mongoose.Types.ObjectId) && !_.isUndefined(elem)) {
+      arr[ind] = mongoose.Types.ObjectId(elem);
+    }
+  };
+
+  /** + Every ID reference in our object is properly typed. 
+    *   This needs to be done BEFORE any other operation so
+    *   that native lookups and updates don't fail.
+    */
+  try {
+    this.teachers.forEach(toObjectId);
+    this.problems.forEach(toObjectId);
+    next();
+  }
+  catch (err) {
+    next(new Error(err.message));
+  }
+});
+
+// /**
+//   * ## Post-Validation
+//   * After saving we must ensure (synchonously) that:
+//   */
+// SectionSchema.post('save', function (Section) {
+//   var update = { $addToSet: { 'Sections': Section } };
+//   if (Section.isTrashed) {
+//     var SectionIdObj = mongoose.Types.ObjectId(Section._id);
+//     /* + If deleted, all references are also deleted */
+//     update = { $pull: { 'Sections': SectionIdObj } };
+//   }
+
+//   if (Section.workspace) {
+//     mongoose.models.Workspace.update({ '_id': Section.workspace },
+//       update,
+//       function (err, affected, result) {
+//         if (err) {
+//           throw new Error(err.message);
+//         }
+//       });
+//   }
+
+//   if (Section.submission) {
+//     mongoose.models.Submission.update({ '_id': Section.submission },
+//       update,
+//       function (err, affected, result) {
+//         if (err) {
+//           throw new Error(err.message);
+//         }
+//       });
+//   }
+
+// });
+
+module.exports.Section = mongoose.model('Section', SectionSchema);
