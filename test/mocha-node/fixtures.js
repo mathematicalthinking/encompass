a8c24107601aa4efe847d300c7adaf9d29236032
okputadora <okputadora@gmail.com>
2018-06-01 14:57:19 -0400

    Add structure of dummy data for tests

---
+++ test/mocha-node/fixtures.js
@@ -0,0 +1,55 @@
+/**
+  * # Test Fixtures
+  * @description This is the mock data to be used in REST API test
+**/
+
+module.exports = {
+  answer: {
+    validAnswer: {
+      studentName: '',
+      answer: '',
+      explanation: '',
+      problemId: '',
+      sectionId: '',
+    },
+    invalidAnswer: {
+      id: '',
+      studentName: '',
+      answer: '',
+      explanation: '',
+      problemId: '',
+      sectionId: '',
+    }
+  },
+  category: {
+    name: ''
+  },
+  problem: {
+    validProblem: {
+      title: '',
+      puzzleId: '', // if this is coming from PoWs otherwise null
+      categories: []
+    },
+    invalidProblem: {
+      title: '',
+      puzzleId: '', // if this is coming from PoWs otherwise null
+      categories: []
+    }
+  },
+  section: {
+    validSection: {
+      sectionId: '',
+      name: '',
+      problems: [],
+      students: [],
+      teachers: [],
+    },
+    invalidSection: {
+      sectionId: '',
+      name: '',
+      problems: [],
+      students: [],
+      teachers: [],
+    }
+  },
+}
