d40f96b6c222dd88769fd4f7b8c914d14634b673
okputadora <okputadora@gmail.com>
2018-06-04 16:55:49 -0400

    Add unit tests for problem api

--- test/mocha/sectionTest.js
+++ test/mocha/sectionTest.js
@@ -42,7 +42,6 @@ describe('/POST section', () => {
 describe('/PUT update section name', () => {
   it('should change the section name to phils class', done => {
     let url = baseUrl + fixtures.section.validSection._id;
-    console.log("URL: ", url);
     chai.request('http://localhost:8080')
     .put(url)
     .set('Cookie', userCredentials)
