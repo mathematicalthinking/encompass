b1fcee90f3445fa2451f560d865637494fb4677f
Daniel Kelly <djk8552@gmail.com>
2018-06-05 13:20:57 -0400

    Refactor base tests from nightmare to selenium

--- test/selenium/workspaces.js
+++ test/selenium/workspaces.js
@@ -1,30 +1,30 @@
-const selenium = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver')
 const chai = require('chai');
-
 const expect = chai.expect;
 const assert = chai.assert;
 describe('test', function() {
   this.timeout('10s');
   let driver = null;
   before(() => {
-    driver = new selenium.Builder()
-      .withCapabilities(selenium.Capabilities.chrome())
+    driver = new Builder()
+      .forBrowser('chrome')
       .build();
-    driver.getWindowHandle();
   });
   
   after(() => {
     driver.quit();
   });
-  it('should work', (done) => {
+  it('should work', async function() {
+    try {
+    await driver.get('http://localhost:8080/devonly/fakelogin/casper');
+    await driver.findElement(By.css('a[href="#/workspaces"]')).sendKeys('webdriver', Key.RETURN);
+    await driver.sleep(3000);
+    await driver.findElement(By.css('a[href="#/workspaces/53df8c4c3491b46d73000211/work"]')).sendKeys('webdriver', Key.RETURN);
+    await driver.sleep(3000);
+    }catch(err) {
+      console.log(err);
+    }
     
-    driver.get('http://localhost:8080/devonly/fakelogin/casper')
-    .wait(elementLocated(By.class('menu.workspaces')),5000)
-    .then((res) => {
-      console.log(res);
-      done();
-    })
-    .catch(done);
   });
 });
 
