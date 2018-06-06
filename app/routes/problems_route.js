0076d2d6c12df1f5a24a9a642f08239cf4085cc0
Philip Wisner <philipwisner@gmail.com>
2018-06-06 10:34:57 -0400

    updates

--- app/routes/problems_route.js
+++ app/routes/problems_route.js
@@ -1,7 +1,6 @@
 Encompass.ProblemsRoute = Ember.Route.extend({
   model: function (params) {
     var store = this.get('store');
-    // var user = this.modelFor('users').filterBy('username', params.username).get('firstObject');
     var problems = store.findAll('problem');
     // Filter only problems by current logged in user
     return problems;
