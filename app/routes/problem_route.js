4fd1d3d61ce5c3252e11d1c3faebd5728fff9ab6
Philip Wisner <philipwisner@gmail.com>
2018-06-06 09:49:52 -0400

    can display individual problem on a page now

--- app/routes/problem_route.js
+++ app/routes/problem_route.js
@@ -1,6 +1,6 @@
 Encompass.ProblemRoute = Ember.Route.extend({
   model: function (params) {
-    var problem = this.modelFor('problems').filterBy('title', params.title).get('firstObject');
+    var problem = this.modelFor('problems').filterBy('id', params.id).get('firstObject');
     return problem;
   },
 
