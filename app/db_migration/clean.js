a67b246b1125cbbbb2c46a347fcbf49f933c2793
Daniel Kelly <djk8552@gmail.com>
2018-06-09 19:17:10 -0400

    Add comments

--- app/db_migration/clean.js
+++ app/db_migration/clean.js
@@ -14,8 +14,8 @@ const collections = ["Tagging",
 "Section",
 "Category"
 ];
-
-const targetWorkspaces = ['']
+// Criteria for deciding which workspaces to keep
+// Current buildCriteria function uses greater than these values 
 const workspaceMinimum = {
   submissions: 3, 
   comments: 0, 
@@ -24,6 +24,7 @@ const workspaceMinimum = {
   selections: 0
 }
 
+// builds the criteria that will be passed to the find operation
 function buildCriteria(options) {
   let $andArray = [];
   const keys = Object.keys(options);
@@ -36,6 +37,8 @@ function buildCriteria(options) {
 }
 console.log(buildCriteria(workspaceMinimum));
 
+// finds matching workspaces based on criteria and then deletes
+// all other workspaces that don't match
 async function pruneWorkspaces() {
   let workspaces;
   let ids;
@@ -44,14 +47,13 @@ async function pruneWorkspaces() {
     workspaces = await models.Workspace.find(buildCriteria(workspaceMinimum));
     console.log('matching workspaces', workspaces.length);
     ids = workspaces.map(ws => ws._id);
-
     deleted = await models.Workspace.deleteMany({_id: {$nin: ids}});
   }catch(err) {
     console.log(err)
   }
   return deleted;
 }
-//pruneWorkspaces();
+// removes all documents from specified collectiond
 async function removeTrashedDocuments() {
   let trashed;
   try {
@@ -65,7 +67,12 @@ async function removeTrashedDocuments() {
   return trashed;
 }
 
+// collections where the schema has a workspace field which contains a single 
+// reference to a workspace ObjectId
 const belongsToWs = ['Selection', 'Comment', 'Response', 'Folder']
+
+// removes all documents whose workspace field references a workspace
+// ObjectId that no longer exists in database 
 async function removeOrphanedFromWs(collection) {
   let deleted = 0;
   let model = models[collection];
@@ -85,15 +92,76 @@ async function removeOrphanedFromWs(collection) {
        if (shouldDelete) {
           await model.deleteOne({_id: doc._id});
           deleted++;
-       }
+        }
       }
     }catch(err) {
       console.log(err);
   }
   
   console.log(`Deleted ${deleted} ${collection}s`);
+  return deleted;
 }
 
+async function removeAllOrphanedFromWs() {
+  for (let collection of belongsToWs) {
+    await removeOrphanedFromWs(collection);
+  }
+  return;
+}
+
+
+// Removes all taggings that contian a reference to a selection or folder
+// that no longer exist
+async function removeOrphanedTaggings() {
+  let deleted = 0;
+  let model = models.Tagging;
+  try {
+    let taggings = await model.find({});
+    console.log(`there are ${taggings.length} taggings`);
+
+    for (tagging of taggings) {
+      let shouldDelete = true;
+      let selectionId = tagging.selection;
+      let folderId = tagging.folder;
+
+      if (selectionId && folderId) {
+        let selection = await models.Selection.findById(selectionId);
+        let folder = await models.Folder.findById(folderId);
+
+        if (selection !== null && folder !== null) {
+          shouldDelete = false;
+        }
+
+        if(shouldDelete) {
+          await model.deleteOne({_id: tagging._id});
+          deleted++;
+        }
+      }
+    }
+  }catch(err) {
+    console.log(err);
+  }
+  console.log(`deleted ${deleted} taggings`);
+  return deleted;
+}
+// removes all orphaned workspaceIds from submissions
+async function removeOrphanedWorkspaces() {
+  let results;
+  try {
+    let workspaces = await models.Workspace.find({});
+    let workspaceIds = workspaces.map(ws => ws._id);
+    results = await models.Submission.updateMany({}, {$pull: {workspaces: {$nin: workspaceIds}}});
+  }catch(err) {
+    console.log(err);
+  }
+  console.log('results for removing Orphaned workspaces',results);
+  return results;
+}
+
+// submissions contain a workspaces field which is an array 
+// of workspace ObjectId references
+// deletes submissions that contain a reference to deleted workspaces
+// otherwise app throws an error when navigating in the workspace
 async function removeOrphanedSubs() {
   let deleted = 0;
   let model = models.Submission;
@@ -125,60 +193,8 @@ async function removeOrphanedSubs() {
   console.log(`Deleted ${deleted} submissionss`);
 }
 
-async function removeOrphanedTaggings() {
-  let deleted = 0;
-  let model = models.Tagging;
-  try {
-    let taggings = await model.find({});
-    console.log(`there are ${taggings.length} taggings`);
-
-    for (tagging of taggings) {
-      let shouldDelete = true;
-      let selectionId = tagging.selection;
-      let folderId = tagging.folder;
-
-      if (selectionId && folderId) {
-        let selection = await models.Selection.findById(selectionId);
-        let folder = await models.Folder.findById(folderId);
-
-        if (selection !== null && folder !== null) {
-          shouldDelete = false;
-        }
-
-        if(shouldDelete) {
-          await model.deleteOne({_id: tagging._id});
-          deleted++;
-        }
-      }
-    }
-  }catch(err) {
-    console.log(err);
-  }
-  console.log(`deleted ${deleted} taggings`);
-}
-
-async function removeAllOrphanedFromWs() {
-  for (let c of belongsToWs) {
-    await removeOrphanedFromWs(c);
-  }
-  return;
-}
-
-async function removeOrphanedWorkspaces() {
-  let results;
-  try {
-    //let submissions = await models.Submission.find({});
-    let workspaces = await models.Workspace.find({});
-    let workspaceIds = workspaces.map(ws => ws._id);
-    console.log(workspaceIds);
-    results = await models.Submission.updateMany({}, {$pull: {workspaces: {$nin: workspaceIds}}});
-  }catch(err) {
-    console.log(err);
-  }
-  console.log('results for removing Orphaned workspaces',results);
-  return results;
-}
-
+// remove all users that are not admins or that are not owners or editors
+// of existing workspaces
 async function removeIrrelevantUsers() {
   let deleted;
   let model = models.User;
