6ee56a4cc09eb8c7e0fa713512f8109a0201c3f6
Daniel Kelly <djk8552@gmail.com>
2018-06-11 11:14:50 -0400

    Use --drop option in restoreDb instead of having separate drop function

--- app/db_migration/restore.js
+++ app/db_migration/restore.js
@@ -1,61 +1,28 @@
 const spawn = require('child_process').spawn;
-const exec = require('child_process').exec 
+const exec = require('child_process').exec
 const path = require('path');
 const pathToBackup = path.resolve(__dirname, 'data/encompass_test');
 const testDb = 'encompass_test';
 
-const restoreDb = function(dbName, backupPath) {
+const restoreDb = function (dbName, backupPath) {
   return new Promise(function (resolve, reject) {
-    const args = ['--db', dbName, backupPath];
+    const args = ['--db', testDb, '--drop', backupPath];
     const mongorestore = spawn('/usr/local/bin/mongorestore', args);
     mongorestore.stdout.on('data', function (data) {
-      restoreOutput = data;
       console.log('stdout: ' + data);
     });
 
     mongorestore.on('error', function (err) {
-        reject(err);
+      reject(err);
     });
 
-    mongorestore.on('exit', function () { 
-      resolve('Mongorestore completed succesfully: ');
+    mongorestore.on('exit', function () {
+      resolve(`Restored ${dbName} database successfully!`);
     });
   });
 };
 
-// const dropDb = function() {
-//   return new Promise(function (resolve, reject) {
-//     const args = ['--db', testDb, pathToBackup];
-//     const mongor = spawn('/usr/local/bin/mongorestore', args);
-
-//     child.stdout.on('data', function (data) {
-//       console.log('stdout: ' + data);
-//     });
-
-//     child.on('error', function (data) {
-//         reject('Mongorestore errored');
-//     });
-
-//     child.on('exit', function () {
-//         resolve('Mongorestore completed succesfully');
-//     });
-// });
-// };
-// const mongorestore = spawn('/usr/local/bin/mongorestore', args);
-
-// mongorestore.stdout.on('data', function(data) {
-//   console.log('stdout: ' + data);
-// });
-
-// mongorestore.stderr.on('data', function(data) {
-//   console.log('stderr: ' + data);
-// });
-
-// mongorestore.on('exit', function (code) {
-//   console.log('mongorestore exited with code ' + code);
-// });
-
-const dropTestdb = function() {
+const dropTestdb = function () {
   return new Promise((resolve, reject) => {
     exec('npm run drop-testdb', (err, stdout, stderr) => {
       if (err) {
@@ -63,32 +30,21 @@ const dropTestdb = function() {
       }
       console.log('db drop results: ', stdout);
       //console.log('db drop errors: ', stderr);
-      resolve('Dropped successfully');
+      resolve('Dropped encompass_test database successfully!');
     });
   });
 }
-// const restoreTestdb = function() {
-//   return new Promise((resolve, reject) => {
-//     exec('npm run restore-testdb', (err, stdout, stderr) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       console.log('Testdb restore results: ', stdout);
-//       resolve('Restored successfully');
-//     });
-//   });
-// }
 
-const prepTestDb = async function() {
+const prepTestDb = async function () {
   try {
-    let dropped = await dropTestdb();
-    console.log(dropped);
-    
+    // let dropped = await dropTestdb();
+    // console.log(dropped);
+
     let restored = await restoreDb(testDb, pathToBackup);
     console.log(restored);
-  }catch(err) {
+  } catch (err) {
     console.log(err);
   }
 };
-
+//prepTestDb();
 module.exports.prepTestDb = prepTestDb;
