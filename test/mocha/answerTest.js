03219ae22d11e129a073b6a440fd92fa30a14397
Daniel Kelly <djk8552@gmail.com>
2018-06-11 11:13:38 -0400

    Refactor api tests to use users from testdb

--- test/mocha/answerTest.js
+++ test/mocha/answerTest.js
@@ -1,17 +1,22 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const dbSetup = require('../../app/db_migration/restore');
+
 const fixtures = require('./fixtures.js');
-const userCredentials = 'loginSessionUser=steve; EncAuth=ff6d8301-dd2f-4a83-9e3e-1ff4a459a292'; // this is always changing
+const userCredentials = 'loginSessionUser=superuser; EncAuth=test-admin-key'; // this is always changing
 const baseUrl = "/api/answers/";
 const answerId = '';
+const host = 'http://localhost:8088';
+
 
 chai.use(chaiHttp);
 
 /** GET **/
 describe('/GET answers', () => {
+  
   it('should get all answers', done => {
-    chai.request('http://localhost:8080')
+    chai.request(host)
     .get(baseUrl)
     .set('Cookie', userCredentials) // what to do about this?
     .end((err, res) => {
@@ -24,35 +29,35 @@ describe('/GET answers', () => {
   });
 });
 
-// /** POST **/
-// describe('/POST answer', () => {
-//   it('should post a new answer', done => {
-//     chai.request('http://localhost:8080')
-//     .post(baseUrl)
-//     .set('Cookie', userCredentials)
-//     .send({answer: fixtures.answer.validAnswer})
-//     .end((err, res) => {
-//       expect(res).to.have.status(200);
-//       expect(res.body.answer).to.have.any.keys('puzzleId', 'categories', 'title');
-//       expect(res.body.answer.title).to.eql('test math answer');
-//       done();
-//     });
-//   });
-// });
-//
-// /** PUT name**/
-// describe('/PUT update answer name', () => {
-//   it('should change the title to test science answer', done => {
-//     let url = baseUrl + answerId;
-//     chai.request('http://localhost:8080')
-//     .put(url)
-//     .set('Cookie', userCredentials)
-//     .send({answer: {title: 'test science answer'}})
-//     .end((err, res) => {
-//       expect(res).to.have.status(200);
-//       expect(res.body.answer).to.have.any.keys('puzzleId', 'title', 'categories');
-//       expect(res.body.answer.title).to.eql('test science answer');
-//       done();
-//     });
-//   });
-// });
+/** POST **/
+describe('/POST answer', () => {
+  it('should post a new answer', done => {
+    chai.request(host)
+    .post(baseUrl)
+    .set('Cookie', userCredentials)
+    .send({answer: fixtures.answer.validAnswer})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body.answer).to.have.any.keys('puzzleId', 'categories', 'title');
+      expect(res.body.answer.title).to.eql('test math answer');
+      done();
+    });
+  });
+});
+
+/** PUT name**/
+describe('/PUT update answer name', () => {
+  it('should change the title to test science answer', done => {
+    let url = baseUrl + answerId;
+    chai.request(host)
+    .put(url)
+    .set('Cookie', userCredentials)
+    .send({answer: {title: 'test science answer'}})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body.answer).to.have.any.keys('puzzleId', 'title', 'categories');
+      expect(res.body.answer.title).to.eql('test science answer');
+      done();
+    });
+  });
+});
