9a61be284aae54d2128a589c46fdb5e8f0dde353
Daniel Kelly <djk8552@gmail.com>
2018-06-15 22:03:20 -0400

    Add css selectors for testing

---
+++ test/selenium/selectors.js
@@ -0,0 +1,17 @@
+module.exports = {
+  topBar: {
+    login: 'a.menu.login',
+    signup: 'a.menu.signup',
+    workspaces: 'a.menu.workspaces',
+    responses: 'a.menu.responses',
+    users: 'a.menu.users',
+    logout: 'a.menu.logout',
+    problem: 'a.menu.problem',
+
+  },
+  login: {
+    username: 'input[name=username]',
+    password: 'input[name=password]',
+    submit: 'button[type=submit]'
+  }
+};
