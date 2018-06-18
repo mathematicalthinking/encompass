482d37a3cf1ca580532813fe53848a40c29830db
okputadora <okputadora@gmail.com>
2018-06-18 12:17:41 -0400

    Added selenium tests back to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt) {
           clearCacheFilter: (key) => true, // Optionally defines which files should keep in cache
           noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
         },
-        src: ['test/mocha/*.js']
+        src: ['test/selenium/**/*.js', 'test/mocha/*.js']
       }
     },
 
