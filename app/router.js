f15b65071c05a8bcb789e87507200b593880a705
philipwisner <philipwisner@gmail.com>
2018-06-20 09:27:21 -0400

    removed old packages, moved router file to main directory and changed the name of the app from mytodo to encompass

--- app/router.js
+++ app/router.js
@@ -1,66 +1,20 @@
 /*
-  The route matching behavior of this
-  application.
-
-  The string argument will be upcased,
-  and have 'Route' appended. A matching
-  constant will be looked up on your
-  application's namespace. For child
-  routes of a resource, the resource
-  name is upcased and prepended. See
-  http://emberjs.com/guides/routing/defining-your-routes/
-  for more details.
-
-  So, for the routes below to work we
-  have to define
-    Encompass.EncompassRoute
-    Encompass.EncompassAllWorkspacesRoute
-    Encompass.EncompassActiveWorkspacesRoute
-
-  Loading the application with any of these
-  urls will result in the correct data
-  being fetched from the store and displayed.
-
-  Entering into these states later will
-  cause the correct data to be fetched and displayed.
-
-  Child routes are entered by passing through
-  their parents. This means parent routes can be used
-  to fetch data and build parent view hierarchies
-  and you can be sure this structure will occur whether
-  you are starting the application directly at a particular
-  route or entering that route at a later point.
+  This is the map of all the routes and their child routes for EnCoMPASS
 */
+
 Encompass.Router.map(function() {
-  //this.route("login");
-  // this.route("signup");
+  // AUTH PARENT ROUTE
   this.route("auth", function(){
     this.route("login");
     this.route("signup");
   });
-
-  this.route("unauthorized");
-
-  /*
-  this.resource("comments", function(){
-    this.route("index", {path: "/"}); //this might show the list of comments, but is here for testing for now
-  });
-  this.resource("folders", function(){
-    this.route("index", {path: "/"}); //this might show the list of top level folders only, but is here for testing for now
-    this.route("edit", {path: '/:folder_id/edit'}, function() {
-    });
-  });
-  this.resource("selections", function(){
-    this.route("index", {path: "/"}); //this might show the list of selections, but is here for testing for now
-  });
-  */
-
-  this.route("users", function(){
-    this.route("user", {resetNamespace: true, path: ':username'}, function(){
-      //this.route("edit");
+  // PROBLEMS PARENT ROUTE
+  this.route("problems", function(){
+    this.route("problem", {resetNamespace: true, path: '/:problem_id'}, function() {
     });
     this.route("new");
   });
+  // WORKSPACES PARENT ROUTE
   this.route("workspaces", function(){
     this.route("index", {path: "/"});
     this.route("mine");
@@ -87,7 +41,7 @@ Encompass.Router.map(function() {
       });
     });
   });
-  this.route("problem");
+  // RESPONSES PARENT ROUTE
   this.route("responses", function(){
     this.route("responses.new", {resetNamespace: true, path: '/new'}, function() {
       this.route("submission", {path: '/submission/:submission_id'});
@@ -98,5 +52,28 @@ Encompass.Router.map(function() {
       //this.route("print");
     });
   });
+  // USERS PARENT ROUTE
+  this.route("users", function(){
+    this.route("user", {resetNamespace: true, path: ':username'}, function(){
+      //this.route("edit");
+    });
+    this.route("new");
+  });
   this.route("logout");
+  this.route("unauthorized");
 });
+
+
+/*
+  this.resource("comments", function(){
+    this.route("index", {path: "/"}); //this might show the list of comments, but is here for testing for now
+  });
+  this.resource("folders", function(){
+    this.route("index", {path: "/"}); //this might show the list of top level folders only, but is here for testing for now
+    this.route("edit", {path: '/:folder_id/edit'}, function() {
+    });
+  });
+  this.resource("selections", function(){
+    this.route("index", {path: "/"}); //this might show the list of selections, but is here for testing for now
+  });
+  */
