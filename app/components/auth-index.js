afb7e6236de525ec179f1c2a8b4feb942c2297ca
philipwisner <philipwisner@gmail.com>
2018-06-25 15:30:07 -0400

    the auth page now renders both login and signup and displays one at a time

--- app/components/auth-index.js
+++ app/components/auth-index.js
@@ -1,29 +1,21 @@
 Encompass.AuthIndexComponent = Ember.Component.extend({
-  content: null,
+  shouldShowLogin: null,
 
-  init() {
-    this.set('content', true);
+
+  init: function () {
+    this._super(...arguments);
+    this.set('shouldShowLogin', true);
+  },
+
+  actions: {
+    changeComponent: function () {
+      console.log('shouldShowLogin:', this.shouldShowLogin);
+      if (this.shouldShowLogin) {
+        this.set('shouldShowLogin', false);
+      } else {
+        this.set('shouldShowLogin', true);
+      }
+    }
   }
-
-  // actions: {
-  //   changeComponent: function () {
-  //     if (login) {
-  //       return !login;
-  //     } else if (this.changeComponent = 'signup') {
-  //       return this.currentComponent = 'login';
-  //     }
-  //   },
-
-  //   selectChange: function () {
-  //     var changeAction = this.get('action');
-  //     var selectedEl = this.$('select')[0];
-  //     var selectedIndex = selectedEl.selectedIndex;
-  //     var content = this.get('content');
-  //     var selectedValue = content[selectedIndex];
-
-  //     this.set('selectedValue', selectedValue);
-  //     changeAction(selectedValue);
-  //   }
-  // }
 });
 
