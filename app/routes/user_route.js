5196866b7373f9e6e8ef710a6781a9afddf27379
SAMUEL HAILE <samishken@gmail.com>
2018-06-25 15:23:20 -0400

    user component rendering

--- app/routes/user_route.js
+++ app/routes/user_route.js
@@ -1,8 +1,10 @@
-Encompass.UserRoute = Ember.Route.extend({
-
-  model: function(params) {
-    var user = this.modelFor('users').filterBy('username', params.username).get('firstObject');
+Encompass.UserRoute = Encompass.AuthenticatedRoute.extend({
+  model: function (params) {
+    var user = this.modelFor('users').filterBy('username', params.id).get('firstObject');
     return user;
   },
 
+  renderTemplate: function () {
+    this.render('users/user');
+  }
 });
