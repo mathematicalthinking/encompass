779bd473e45e24848a724e363233d17a9fe7f975
philipwisner <philipwisner@gmail.com>
2018-07-03 12:41:26 -0400

    added comments to passport

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -1,6 +1,15 @@
+/**
+ * # Passport Authentication Middleware & Logic
+ * @description This file hosts all the logic needed to:
+ *   Serialize and deserialize users
+ *   Login & Signup using a local strategy
+ *   Signup & Login using Google OAuth
+ * @author Philip Wisner & Daniel Kelly
+ * @since 2.0.0
+ */
+
 //REQUIRE MODULES
 const passport = require('passport');
-const flash = require('connect-flash');
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
@@ -12,13 +21,9 @@ const models = require('../datasource/schemas');
 const User = models.User;
 
 module.exports = (passport) => {
-
-  // =========================================================================
-  // passport session setup ==================================================
-  // =========================================================================
+  // passport session setup
   // required for persistent login sessions
   // passport needs ability to serialize and unserialize users out of session
-
   // used to serialize the user for the session
   passport.serializeUser((user, next) => {
     next(null, user.id);
@@ -33,24 +38,25 @@ module.exports = (passport) => {
     });
   });
 
-
+ /* This is the local strategy for login
+  * It checks to see if the username matches a username in the database
+  * If it doesn't it sends an error of incorrect username
+  * It then checks to see if the hashed passwords match
+  * If they don't then it returns incorrect password
+  */
   passport.use('local-login', new LocalStrategy((username, password, next) => {
     User.findOne({
       username: username
     }, (err, user) => {
-      console.log('user', user);
       if (err) {
-        console.log('err from db', err);
         return next(err);
       }
       if (!user) {
-        console.log('incorrect username');
         return next(null, false, {
           message: "Incorrect username"
         });
       }
       if (!bcrypt.compareSync(password, user.password)) {
-        console.log('incorrect password');
         return next(null, false, {
           message: "Incorrect password"
         });
@@ -60,6 +66,12 @@ module.exports = (passport) => {
     });
   }));
 
+  /* This is the local strategy for signup
+  * It checks to see if the username already exists
+  * If it does it returns an error of username already exists
+  * It then creates a new user with the name, email, organziation,
+  *   location, password and request reason
+  */
   passport.use('local-signup', new LocalStrategy({
       passReqToCallback: true
     },
@@ -73,7 +85,6 @@ module.exports = (passport) => {
           }
 
           if (user) {
-            console.log('username already exists');
             return next(null, false, {
               message: "Username already exists"
             });
@@ -111,22 +122,25 @@ module.exports = (passport) => {
     }));
 
 
-  //GOOGLE STRATEGY
+ /* This is the Google Strategy for login and signup
+  * It first checks to see if the user has already loggedin with Google
+  * If this is the first time it then creates a new User with:
+  *   googleId, name, username, email & isAuthorized
+  * This allows you to signup with google and then when you return
+  *   we already have you in our database
+  */
   passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "/auth/google/callback"
   }, (accessToken, refreshToken, profile, done) => {
-    console.log('profileid', profile.id);
     User.findOne({
       googleId: profile.id
     }, (err, user) => {
       if (err) {
-        console.log('in err block', err);
         return done(err);
       }
       if (user) {
-        console.log('found google user', user);
         return done(null, user);
       }
 
@@ -137,10 +151,8 @@ module.exports = (passport) => {
         email: profile.emails[0].value,
         isAuthorized: true
       });
-      console.log('new user', newUser);
       newUser.save((err) => {
         if (err) {
-          console.log('error saving', err);
           return done(err);
         }
         done(null, newUser);
