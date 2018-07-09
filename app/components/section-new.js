9236611cab0c2b4091df5f62bd1dc4e05733a9e1
SAMUEL HAILE <samishken@gmail.com>
2018-07-09 16:29:01 -0400

    admin feature updated

--- app/components/section-new.js
+++ app/components/section-new.js
@@ -6,11 +6,10 @@ Encompass.SectionNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
   createSectionError: null,
   teacher: null,
 
-
   didInsertElement: function () {
     var user = this.get('user');
     console.log('user in didinsert', user);
-    if(!user.get('isAdmin')) {
+    if (!user.get('isAdmin')) {
       console.log('in if');
       this.set('teacher', user);
       console.log(this.get('teacher'));
@@ -18,15 +17,29 @@ Encompass.SectionNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
     console.log('teacher', this.get('teacher'));
   },
 
-actions: {
+  actions: {
   createSection: function () {
     var newSectionName = this.get('newSectionName');
     var sectionId = this.get('sectionId');
     var schoolName = this.get('schoolName');
-    var teacher = this.get('teacher');
     var user = this.get('user');
+    var teacher = this.get('teacher');
     console.log('teacher username', teacher);
+    if (user.get('isAdmin')) {
+      var teachersUsername = this.store.createRecord('section', {
+        name: newSectionName,
+        sectionId: sectionId,
+        schoolId: schoolName,
+        teachers: [teacher]
+      });
 
+      teachersUsername.save()
+        .then((res) => {
+          this.sendAction('toSectionList');
+      });
+    }
+
+    console.log('teacher username', teacher);
     this.set('newSectionName', '');
     console.debug('creating new section ' + newSectionName);
 
@@ -41,12 +54,11 @@ actions: {
       teachers: [teacher]
     });
 
-
 //save entry
     sectionData.save()
       .then((res) => {
         this.sendAction('toSectionList');
       });
-    },
+    }
 }
 });
