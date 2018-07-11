f5433b7f6d86c83b84852a31f26138ff319888fd
Daniel Kelly <djk8552@gmail.com>
2018-07-11 14:22:09 -0400

    Display error message and block submission creation if not all students matched
    Also add info about import process

--- app/components/student-matching.js
+++ app/components/student-matching.js
@@ -1,7 +1,14 @@
 Encompass.StudentMatchingComponent = Ember.Component.extend({
-
+  matchingStudentsError: null,
   actions: {
     formatAnswers: function() {
+      let studentDropdowns = this.$('#student-matching select');
+      for (let select of studentDropdowns) {
+        if (Ember.isEmpty(select.value)) {
+          this.set('matchingStudentsError', true);
+          return;
+        }
+      }
       this.get('uploadAnswers')();
     }
   }
