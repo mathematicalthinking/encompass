a8a3df629204c1fb0902a834f270b0ba92b435d8
Daniel Kelly <djk8552@gmail.com>
2018-07-11 12:33:00 -0400

    Add isPdf condition to properly render image if it is a pdf

--- app/components/student-matching-answer.js
+++ app/components/student-matching-answer.js
@@ -2,10 +2,15 @@ Encompass.StudentMatchingAnswerComponent = Ember.Component.extend({
   assignedStudent: null,
   section: null,
   submission: null,
+  isPdf: null,
 
   didInsertElement: function() {
     var section = this.get('selectedSection');
     var answer = this.get('answer');
+    if (answer.explanation.data) {
+      var isPdf = answer.explanation.data.includes('application/pdf');
+      this.set('isPdf', isPdf);
+    }
     this.set('section', section);
     this.set('submission', answer);
 
