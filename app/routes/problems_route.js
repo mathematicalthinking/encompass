7a074c04f0672fd3b28b79146764f6bbbaa5b796
philipwisner <philipwisner@gmail.com>
2018-07-12 18:06:50 -0400

    added currentuser model to problems route

--- app/routes/problems_route.js
+++ app/routes/problems_route.js
@@ -1,8 +1,12 @@
 Encompass.ProblemsRoute = Encompass.AuthenticatedRoute.extend({
   model: function (params) {
-    var store = this.get('store');
-    var problems = store.findAll('problem');
-    // Filter only problems by current logged in user
-    return problems;
+    return Ember.RSVP.hash({
+      problems: this.get('store').findAll('problem'),
+      user: this.modelFor('application')
+    });
+    // var store = this.get('store');
+    // var problems = store.findAll('problem');
+    // // Filter only problems by current logged in user
+    // return problems;
   }
 });
