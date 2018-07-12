cb8a9d06beff31c3c2cb3d4dbc5441a4194e481f
Daniel Kelly <djk8552@gmail.com>
2018-07-12 16:35:24 -0400

    Remove console.log /comments

--- app/components/my-select.js
+++ app/components/my-select.js
@@ -16,11 +16,8 @@ Encompass.MySelectComponent = Ember.Component.extend({
   actions: {
     selectChange: function() {
       var changeAction = this.get('action');
-      console.log('changeAction', changeAction);
       var selectedEl = this.$('select')[0];
       var selectedIndex = selectedEl.selectedIndex - 1;
-      // the content that is being passed in is an ember object with a content property
-      // that is an array of objects -- not sure if this is the best fix
       var content = this.get('content');
       var selectedValue = content.objectAt(selectedIndex);
       this.set('selectedValue', selectedValue);
