ea4e38c82731d12c4535f0adb6f4216c3511c7ff
philipwisner <philipwisner@gmail.com>
2018-07-12 18:17:40 -0400

    added filter to display user's problems

--- app/components/problem-list.js
+++ app/components/problem-list.js
@@ -1,3 +1,17 @@
 Encompass.ProblemListComponent = Ember.Component.extend({
-  content: null,
+  myProblemsOnly: [],
+  showMyProblems: true,
+
+  // filter problem list by username
+  filteredComments: function () {
+      var filtered = this.problems.filterBy('username');
+
+      if (this.showMyProblems) {
+        filtered = filtered.filterBy('createdBy', this.get('currentUser'));
+      }
+      console.log(filtered);
+      return filtered;
+  }
 });
+
+
