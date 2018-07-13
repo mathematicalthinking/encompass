1333ec390c92ed075334775ffe79c1063b614264
philipwisner <philipwisner@gmail.com>
2018-07-13 09:41:28 -0400

    filtering in the route

--- app/routes/problems_route.js
+++ app/routes/problems_route.js
@@ -1,8 +1,9 @@
 Encompass.ProblemsRoute = Encompass.AuthenticatedRoute.extend({
   model: function (params) {
     return Ember.RSVP.hash({
-      problems: this.get('store').findAll('problem'),
-      currentUser: this.modelFor('application')
+      // problems: this.get('store').findAll('problem'),
+      publicProblems: this.get('store').findAll('problem').filterBy('isPublic', true),
+      currentUser: this.modelFor('application'),
     });
     // var store = this.get('store');
     // var problems = store.findAll('problem');
