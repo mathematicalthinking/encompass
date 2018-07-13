1bdc8a3ac9478174fd0b463cead32f2f08c18dbd
philipwisner <philipwisner@gmail.com>
2018-07-13 12:14:48 -0400

    reveerted route

--- app/routes/problems_route.js
+++ app/routes/problems_route.js
@@ -1,9 +1,8 @@
 Encompass.ProblemsRoute = Encompass.AuthenticatedRoute.extend({
   model: function (params) {
     return Ember.RSVP.hash({
-      // problems: this.get('store').findAll('problem'),
-      publicProblems: this.get('store').findAll('problem').filterBy('isPublic', true),
-      currentUser: this.modelFor('application'),
+      problems: this.get('store').findAll('problem'),
+      currentUser: this.modelFor('application')
     });
     // var store = this.get('store');
     // var problems = store.findAll('problem');
