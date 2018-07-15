464e97910f6e189f9d7c38c0a78c220cd0ee62d3
philipwisner <philipwisner@gmail.com>
2018-07-15 18:40:28 -0400

    added is Public

--- app/components/problem-info.js
+++ app/components/problem-info.js
@@ -12,14 +12,20 @@ Encompass.ProblemInfoComponent = Ember.Component.extend({
       this.set('isEditing', true);
     },
 
+    radioSelect: function (value) {
+      this.set('isPublic', value);
+    },
+
     updateProblem: function () {
       let problem = this.get('problem');
-      let name = this.get('title');
-      let question = this.get('text');
+      let title = this.get('title');
+      console.log('title', title);
+      let text = this.get('text');
+      console.log('tex', text);
       let isPublic = this.get('isPublic');
 
-      problem.set('title', name);
-      problem.set('text', question);
+      problem.set('title', title);
+      problem.set('text', text);
       problem.set('isPublic', isPublic);
       problem.save();
       this.set('isEditing', false);
