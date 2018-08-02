05bedfec8997a49d6bf61d1dfe45e5f8f14e5f15
Philip Wisner <philipwisner@gmail.com>
2018-08-02 10:25:11 -0400

    Update problems_new_route.js

--- app/routes/problems_new_route.js
+++ app/routes/problems_new_route.js
@@ -2,8 +2,7 @@ Encompass.ProblemsNewRoute = Encompass.AuthenticatedRoute.extend({
   model: function (params) {
     return Ember.RSVP.hash({
       users: this.get('store').findAll('user'),
-      problems: this.get('store').findAll('problem'),
-      user: this.modelFor('application')
+      problems: this.get('store').findAll('problem')
     });
   },
   renderTemplate: function () {
