4cb55467a8a3209eebbaa97575f846776f47f516
philipwisner <philipwisner@gmail.com>
2018-08-03 14:52:38 -0400

    problem list updates only on edit

--- app/routes/problem_route.js
+++ app/routes/problem_route.js
@@ -14,3 +14,4 @@ Encompass.ProblemRoute = Encompass.AuthenticatedRoute.extend({
     },
   }
 });
+
--- app/routes/problems_route.js
+++ app/routes/problems_route.js
@@ -1,11 +1,13 @@
 Encompass.ProblemsRoute = Encompass.AuthenticatedRoute.extend({
   model: function (params) {
     let problems = this.get('store').findAll('problem');
+    console.log('problems route model called');
     return problems;
-
   },
 
   renderTemplate: function () {
     this.render('problems/problems');
-  }
+  },
+
+
 });
