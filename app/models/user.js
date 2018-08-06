b53659bd95dbdcb4d9b92db71c4ea8d691ca03f2
Daniel Kelly <djk8552@gmail.com>
2018-08-06 13:29:35 -0400

    Add sections to user model

--- app/models/user.js
+++ app/models/user.js
@@ -20,6 +20,7 @@ Encompass.User = DS.Model.extend(Encompass.Auditable, {
   sections: DS.attr(),
   assignments: DS.hasMany('assignment', {async: true, inverse: null}),
   answers: DS.hasMany('answer', {async: true}),
+  sections: DS.attr(),
   isAuthenticated: function() {
     return !this.get('isGuest');
   }.property('isGuest'),
