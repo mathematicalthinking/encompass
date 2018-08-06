8ac2db01798a24a173a5de0522d4cb36932af41b
Daniel Kelly <djk8552@gmail.com>
2018-08-06 13:30:06 -0400

    Get section and problem for assignment in assignment-info component

--- app/components/assignment-info.js
+++ app/components/assignment-info.js
@@ -1,3 +1,24 @@
 Encompass.AssignmentInfoComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
+  didReceiveAttrs: function() {
+    if (this.assignment) {
+      const assignment = this.assignment;
+      return assignment.get('section')
+      .then((section) => {
+        this.set('section', section);
+        return assignment.get('problem');
+      })
+      .then((problem) => {
+        this.set('problem', problem);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  },
 
+  actions: {
+    toAssignments: function() {
+      this.sendAction('toAssignments');
+    },
+  }
 });
