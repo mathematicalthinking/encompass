7208664eb514f10f4b6c0449d83c2acc8eec109a
Daniel Kelly <djk8552@gmail.com>
2018-08-06 20:03:14 -0400

    Make answers, section, problem async

--- app/models/assignment.js
+++ app/models/assignment.js
@@ -1,9 +1,9 @@
 Encompass.Assignment = DS.Model.extend(Encompass.Auditable, {
   assignmentId: Ember.computed.alias('id'),
-  answers: DS.hasMany('answer'),
+  answers: DS.hasMany('answer', {async: true}),
   students: DS.hasMany('user'),
-  section: DS.belongsTo('section'),
-  problem: DS.belongsTo('problem'),
+  section: DS.belongsTo('section', {async: true}),
+  problem: DS.belongsTo('problem', {async: true}),
   assignedDate: DS.attr('date'),
   dueDate: DS.attr('date')
 });
