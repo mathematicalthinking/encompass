12ab9f4478051317bf1759a417f4cbbcb6e57b03
Daniel Kelly <djk8552@gmail.com>
2018-08-06 23:41:48 -0400

    Using assignmentList property to display list of assignments
    List updates to filter out any assignments that were set to isTrashed: true

--- app/components/assignment-list.js
+++ app/components/assignment-list.js
@@ -1,3 +1,7 @@
 Encompass.AssignmentListComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
+  assignmentList: function() {
+    console.log('recalculating assignmentList');
+    return this.assignments.filterBy('isTrashed', false);
+  }.property('assignments.@each.isTrashed'),
 
 });
