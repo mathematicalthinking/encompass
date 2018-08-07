3f36dbfa3648165a2a404a5370eb6bbd95d80ea5
Daniel Kelly <djk8552@gmail.com>
2018-08-07 14:11:14 -0400

    Automatically update past submissions list after answer created

--- app/components/assignment-info-student.js
+++ app/components/assignment-info-student.js
@@ -3,45 +3,56 @@ Encompass.AssignmentInfoStudentComponent = Ember.Component.extend(Encompass.Curr
   formattedAssignedDate: null,
   isResponding: false,
   displayedAnswer: null,
+  answerList: [],
+
+  init: function() {
+    this._super(...arguments);
+    return this.getAnswers().then((answers) => {
+      this.set('answerList', answers);
+    });
+  },
 
   getAnswers: function() {
-    return this.store.findAll('answer');
-},
+    const student = this.get('currentUser');
+    return student.get('answers')
+    .catch((err) => {
+      this.set('errorLoadingAnswers', true);
+    });
+  },
 
-  sortedAnswers: function() {
-    if (this.get('answers')) {
-      return this.get('answers').sortBy('createDate').reverse();
-    }
-    return [];
-  }.property('answers.[]'),
+  filteredList: function() {
+      const answers = this.get('answerList');
+      return answers.filterBy('assignment.id', this.assignment.id);
+    }.property('answerList.[]', 'assignment.id'),
+
+  sortedList: function() {
+    const filtered = this.get('filteredList');
+    return filtered.sortBy('createDate').reverse();
+  }.property('filteredList.[]'),
 
   priorAnswer: function() {
-    return this.get('sortedAnswers').get('firstObject');
-  }.property('sortedAnswers.[]'),
+    return this.get('sortedList').get('firstObject');
+  }.property('sortedList.[]'),
 
   didReceiveAttrs: function() {
     if (this.assignment) {
       console.log('assignment in route', this.assignment);
-      let dateTime = 'YYYY-MM-DD';
+    let dateTime = 'YYYY-MM-DD';
     let dueDate = this.assignment.get('dueDate');
     let assignedDate = this.assignment.get('assignedDate');
-    console.log('dueDate', dueDate);
-    console.log('assignedDate', assignedDate);
     this.set('formattedDueDate', moment(dueDate).format(dateTime));
     this.set('formattedAssignedDate', moment(assignedDate).format(dateTime));
-    return this.getAnswers()
-    .then((answers) => {
-      let filtered = answers.filterBy('assignment.id', this.assignment.id);
-      console.log('filtered', filtered);
-      this.set('answers', filtered);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-    }
+  }
+},
+toggleResponse: function() {
+  if (this.get('isResponding')) {
+    this.set('isResponding', false);
+  } else if (this.get('isRevising')) {
+    this.set('isRevising', false);
+  }
+},
 
 
-  },
 
   actions: {
     beginAssignmentResponse: function() {
@@ -52,22 +63,26 @@ Encompass.AssignmentInfoStudentComponent = Ember.Component.extend(Encompass.Curr
       this.set('isRevising', true);
     },
 
-    cancelResponse: function() {
-      console.log('cancelling response');
-      if (this.get('isResponding')) {
-        this.set('isResponding', false);
-      } else if (this.get('isRevising')) {
-        this.set('isRevising', false);
-      }
-    },
-
     toAnswerInfo: function(answer) {
       this.sendAction('toAnswerInfo', answer);
     },
     displayAnswer: function(answer) {
       //this.set('isDisplayingAnswer', true);
       this.set('displayedAnswer', answer);
-    }
+    },
+
+    handleCreatedAnswer: function(answer) {
+      console.log('handling created Answer');
+      this.set('answerCreated', true);
+      this.toggleResponse();
+      this.get('answerList').pushObject(answer);
+    },
+
+    cancelResponse: function() {
+      console.log('cancelling response');
+      this.toggleResponse();
+
+    },
   }
 
 });
