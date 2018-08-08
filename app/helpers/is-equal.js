38c76c41dec7f486e8d7aea70c47bff174988ae3
Daniel Kelly <djk8552@gmail.com>
2018-08-07 22:57:14 -0400

    Fix is-equal helper

--- app/helpers/is-equal.js
+++ app/helpers/is-equal.js
@@ -1,6 +1,7 @@
 // is-equal helper is necessary to determine which option is currently selected.
 // app/helpers/is-equal.js
 
-Encompass.IsEqualHelper = Ember.Helper.helper( function(leftSide, rightSide){
+Encompass.IsEqualHelper = Ember.Helper.helper( function(args){
+  let [leftSide, rightSide] = args;
   return leftSide === rightSide;
 });
