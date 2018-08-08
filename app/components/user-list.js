cc89de15f9257f8378aa7f49fd19c8f963c9ad98
philipwisner <philipwisner@gmail.com>
2018-08-08 15:49:16 -0400

    deleted duplicate

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -1,5 +1,17 @@
 Encompass.UserListComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
-  test: null,
+  elementId: 'user-list',
+
+  unauthUsers: function () {
+    var users = this.users.filterBy('isTrashed', false);
+    var unauthUsers = users.filterBy('isAuthorized', false);
+    return unauthUsers;
+  }.property('users.@each.isAuthorized'),
+
+  adminUsers: function () {
+    var users = this.users.filterBy('isTrashed', false);
+    var adminUsers = users.filterBy('isAdmin', true);
+    return adminUsers;
+  }.property('users.@each.isTrashed'),
 
   actions: {
   }
