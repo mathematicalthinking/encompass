a8e2646abc426dca60e8120e7d03ff7a41111110
philipwisner <philipwisner@gmail.com>
2018-08-08 16:17:54 -0400

    4 lists of users for admin now

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -2,16 +2,36 @@ Encompass.UserListComponent = Ember.Component.extend(Encompass.CurrentUserMixin,
   elementId: 'user-list',
 
   unauthUsers: function () {
-    var users = this.users.filterBy('isTrashed', false);
-    var unauthUsers = users.filterBy('isAuthorized', false);
+    let users = this.users.filterBy('isTrashed', false);
+    let unauthUsers = users.filterBy('isAuthorized', false);
     return unauthUsers;
   }.property('users.@each.isAuthorized'),
 
   adminUsers: function () {
-    var users = this.users.filterBy('isTrashed', false);
-    var adminUsers = users.filterBy('isAdmin', true);
+    let users = this.users.filterBy('isTrashed', false);
+    let adminUsers = users.filterBy('isAdmin', true);
     return adminUsers;
-  }.property('users.@each.isTrashed'),
+  }.property('users.@each.isAdmin'),
+
+  studentUsers: function () {
+    let users = this.users.filterBy('isTrashed', false);
+    let students = users.filterBy('isStudent', true);
+    return students;
+  }.property('users.@each.isStudent'),
+
+  authUsers: function () {
+    let users = this.users.filterBy('isTrashed', false);
+    let authUsers = users.filterBy('isAuthorized', true);
+    let notStudent = authUsers.filter((user) => {
+      let student = user.get('isStudent');
+      return student !== true;
+    });
+    let notAdmin = notStudent.filter((user) => {
+      let admin = user.get('isAdmin');
+      return admin !== true;
+    });
+    return notAdmin;
+  }.property('users.@each.isAuthorized'),
 
   actions: {
   }
