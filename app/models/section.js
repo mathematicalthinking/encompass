1f9c3b51861f4a80b4d01ccfa265119fcf370a62
Daniel Kelly <djk8552@gmail.com>
2018-08-09 13:08:10 -0400

    Make students and teachers fields inverse:null on section model

--- app/models/section.js
+++ app/models/section.js
@@ -2,9 +2,9 @@ Encompass.Section = DS.Model.extend(Encompass.Auditable, {
   sectionId: Ember.computed.alias('id'),
   name: DS.attr('string'),
   organization: DS.belongsTo('organization', {inverse: null}),
-  teachers: DS.hasMany('user'),
+  teachers: DS.hasMany('user', { inverse: null}),
   sectionPassword: DS.attr('string'),
-  students: DS.hasMany('user'),
+  students: DS.hasMany('user', { inverse: null }),
   problems: DS.hasMany('problem'),
   assignments: DS.hasMany('assignment')
 });
