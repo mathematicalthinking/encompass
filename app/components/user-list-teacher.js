8c26870924faf05af3633a188d814b29db8abaa3
philipwisner <philipwisner@gmail.com>
2018-08-09 15:36:38 -0400

    added created users list

--- app/components/user-list-teacher.js
+++ app/components/user-list-teacher.js
@@ -1,42 +1,26 @@
 Encompass.UserListTeacherComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'user-list-teacher',
 
-  orgUsers: function () {
+  yourStudents: function () {
     let users = this.users.filterBy('isTrashed', false);
+    let adminUsers = users.filterBy('isAdmin', true);
+    return adminUsers;
+  }.property('users.@each.isTrashed'),
+
+  yourUsers: function () {
+    let yourId = this.get('currentUser').get('id');
+    console.log('yourId', yourId);
+    let yourUsers = this.users.filterBy('createdBy.id', yourId);
+    return yourUsers;
+  }.property('users.@each.isTrashed'),
+
+  orgUsers: function () {
     let usersWithOrgs = this.users.filter((user) => {
       return !user.get('isTrashed') && user.get('organization.id') && !user.get('isStudent');
     });
     let yourOrgId = this.get('currentUser').get('organization').get('id');
-    //console.log('your Org is', yourOrg);
-
     return usersWithOrgs.filterBy('organization.id', yourOrgId);
   }.property('users.@each.isTrashed'),
-
-  // adminUsers: function () {
-  //   let users = this.users.filterBy('isTrashed', false);
-  //   let adminUsers = users.filterBy('isAdmin', true);
-  //   return adminUsers;
-  // }.property('users.@each.isAdmin'),
-
-  // studentUsers: function () {
-  //   let users = this.users.filterBy('isTrashed', false);
-  //   let students = users.filterBy('isStudent', true);
-  //   return students;
-  // }.property('users.@each.isStudent'),
-
-  // authUsers: function () {
-  //   let users = this.users.filterBy('isTrashed', false);
-  //   let authUsers = users.filterBy('isAuthorized', true);
-  //   let notStudent = authUsers.filter((user) => {
-  //     let student = user.get('isStudent');
-  //     return student !== true;
-  //   });
-  //   let notAdmin = notStudent.filter((user) => {
-  //     let admin = user.get('isAdmin');
-  //     return admin !== true;
-  //   });
-  //   return notAdmin;
-  // }.property('users.@each.isAuthorized'),
 
   actions: {}
 
