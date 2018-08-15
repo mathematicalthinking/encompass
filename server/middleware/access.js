1656fbc71356a75e76b4fa50880980a1da11748d
Daniel Kelly <djk8552@gmail.com>
2018-08-15 17:32:36 -0400

    Add middleware file for functions dedicated to determining what records can be accessed

---
+++ server/middleware/access.js
@@ -0,0 +1,80 @@
+/* jshint ignore:start */
+//REQUIRE MODULES
+const _ = require('underscore');
+//const logger = require('log4js').getLogger('server');
+
+//REQUIRE FILES
+const models = require('../datasource/schemas');
+
+module.exports.get = {};
+module.exports.post = {};
+module.exports.put = {};
+
+async function getOrgUsers(orgId) {
+  try {
+    const orgMembers = await models.User.find({organization: orgId}, {_id: 1});
+    return orgMembers.map(obj => obj._id);
+  }catch(err) {
+    console.log('error getting orgusers', err);
+  }
+}
+
+async function getTeacherAssignments(userId) {
+  try {
+    const ownAssignmentIds = await models.Assignment.find({createdBy: userId}, {_id: 1});
+    return ownAssignmentIds.map(obj => obj._id);
+  }catch(err) {
+    console.log('error getting teacher assignments', err);
+  }
+}
+
+const accessibleAnswersQuery = async function(user, ids) {
+  const accountType = user.accountType;
+  const actingRole = user.actingRole;
+
+  let filter = {
+    isTrashed: false
+  };
+
+  if (ids) {
+    filter._id = {$in : ids};
+  }
+
+  if (actingRole === 'student' || accountType === 'S') {
+    filter.createdBy = user.id;
+    return filter;
+  }
+  // will only reach here if admins/pdadmins are in actingRole teacher
+  if (accountType === 'A') {
+    return filter;
+  }
+
+  if (accountType === 'P') {
+    // only answers tied to organization
+    //get users from org and then ch
+    const userOrg = user.organization;
+    const userIds = await getOrgUsers(userOrg);
+    console.log('userIds', userIds);
+
+    filter.createdBy = {$in : userIds};
+    return filter;
+  }
+
+  if (accountType === 'T') {
+    // only answers from either a teacher's assignments or from a section where they are in the teachers array
+
+    const ownSections = user.sections.map((section) => {
+      if (section.role === 'teacher') {
+        return section.sectionId;
+      }
+     });
+     const ownAssignmentIds = await getTeacherAssignments(user.id);
+     filter.assignment = { $in: ownAssignmentIds };
+
+    filter.section = { $in: ownSections };
+    return filter;
+  }
+};
+
+module.exports.get.answers = accessibleAnswersQuery;
+/* jshint ignore:end */
