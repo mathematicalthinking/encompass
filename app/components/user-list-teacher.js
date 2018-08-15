98fb39e08fdf2319afbfa11f83834b5a0d9d9012
philipwisner <philipwisner@gmail.com>
2018-08-15 15:55:14 -0400

    student listing for teachers later

--- app/components/user-list-teacher.js
+++ app/components/user-list-teacher.js
@@ -6,17 +6,37 @@ Encompass.UserListTeacherComponent = Ember.Component.extend(Encompass.CurrentUse
   // These are all the students that are in sections you are a teacher of
   yourStudents: function () {
     let yourSections = this.get('currentUser').get('sections');
-    console.log('your sections are', yourSections);
     let yourTeacherSections = yourSections.filterBy('role', 'teacher');
     console.log('yourTeacher Sections is', yourTeacherSections);
+    let yourSectionIds = [];
+    yourTeacherSections.forEach((section) => {
+      yourSectionIds.push(section.sectionId);
+    });
+    console.log('your sections ids', yourSectionIds);
+
+    // return Promise.all(yourSectionIds.map((id) => {
+    //   return this.store.findRecord('section', id)
+    //     .then((section) => {
+    //       return section.get('students');
+    //     });
+    // }))
+    //   .then((students) => {
+    //     console.log('studnets', students);
+    //   })
+    //   .catch(console.log);
+
     let firstId = yourTeacherSections[0].sectionId;
     console.log('first sectionid is', firstId);
 
-    let section1 = this.store.findRecord('section', firstId).then((section) => {
-      console.log('section is', section);
-      let sectionStudents = section.get('students');
-      console.log('section students', sectionStudents);
-    });
+    let section1 = this.store.findRecord('section', firstId)
+      .then((section) => {
+        console.log('section is', section);
+        let sectionStudents = section.get('students');
+        sectionStudents.forEach((student) => {
+          console.log('student name is', student.get('username'));
+        });
+        console.log('section students', sectionStudents);
+      });
     // console.log('section 1 is', section1);
     // let section1Students = section1.get('name');
     // console.log('section1 name is', section1Students);
