c40aec8ffee1a7185b673956dbb6680efd24fcaa
Daniel Kelly <djk8552@gmail.com>
2018-08-16 19:31:24 -0400

    Comments

--- server/middleware/access/workspaces.js
+++ server/middleware/access/workspaces.js
@@ -1,13 +1,17 @@
 const utils = require('./utils');
+
 module.exports.get = {};
 
 const canLoadWorkspace = function(user, ws) {
   console.log('ws.editors', ws.editors);
   const accountType = user.accountType;
 
+  // As of now, students cannot get any workspaces
   if (accountType === 'S' || user.actingRole === 'student') {
     return false;
   }
+
+  // Admins can get any workspace
   if (accountType === 'A') {
     return true;
   }
@@ -15,22 +19,26 @@ const canLoadWorkspace = function(user, ws) {
   const ownerOrg = ws.owner.organization.toString();
   const userOrg = user.organization.toString();
 
+  // PdAdmins can get any workspace that is owned by a member of their org
   if (accountType === 'P') {
     if (ownerOrg === userOrg) {
       return true;
     }
   }
+
   const wsId = ws._id.toString();
   const userId = user._id.toString();
 
-
   const isOwner = userId === wsId;
+
 
   const wsEditors = ws.editors.map(ws => ws._id.toString());
   const isEditor = wsEditors.includes(userId);
-  console.log('isEditor', isEditor);
+  console.log('isEditor canLoadWorkspace? ', isEditor);
   const isPublic = ws.mode === 'public';
 
+  // Any teacher or PdAdmin can view a workspace if they are the owner, editor, or ws is public
+  // Should we allow students be added as an editor to a WS? Or view as read-only public workspaces?
   return isOwner || isEditor || isPublic;
 };
 
@@ -44,36 +52,40 @@ const accessibleWorkspacesQuery = async function(user, ids) {
   };
 
   if (ids) {
-    filter._id = {$in : ids};
+    filter._id = {$in: ids};
   }
 
+  // should be stopped earlier in the middleware chain
   if (actingRole === 'student' || accountType === 'S') {
-    //filter.createdBy = user.id;
+    filter.createdBy = user.id;
     return filter;
   }
   // will only reach here if admins/pdadmins are in actingRole teacher
   if (accountType === 'A') {
     return filter;
   }
+  // Teachers and PdAdmins
   filter.$or = [];
-  filter.$or.push({mode: 'public'});
-  filter.$or.push({editors: user._id});
+  filter.$or.push({ mode: 'public' });
+  filter.$or.push({ editors: user._id });
+  filter.$or.push({ owner: user._id });
 
   if (accountType === 'P') {
-    // only answers tied to organization
-    //get users from org and then ch
+
     const userOrg = user.organization;
-    const userIds = await utils.getModelIds('Organization', {_id: userOrg});
+    const userIds = await utils.getModelIds('User', { organization: userOrg, accountType: {$ne: 'S'} });
 
     filter.$or.push({owner: {$in : userIds}});
+
     return filter;
   }
 
   if (accountType === 'T') {
-    // only answers from either a teacher's assignments or from a section where they are in the teachers array
+    // Workspaces where a teacher is the primary teacher or in the teachers array
 
     filter.$or.push({'teacher.id': user.id});
     filter.$or.push({'teachers': user.id});
+
     return filter;
   }
 };
