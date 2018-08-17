6f872cd424ebf44d8ad1ae113c0ebadafd3df736
Daniel Kelly <djk8552@gmail.com>
2018-08-17 13:44:04 -0400

    Remove unused code from pows import

--- app/components/workspace-new-enc.js
+++ app/components/workspace-new-enc.js
@@ -1,22 +1,12 @@
 Encompass.WorkspaceNewEncComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   ElementId: 'workspace-new-enc',
 
-  canEdit: Ember.computed.not('currentUser.isAdmin'),
-  teacher: Ember.computed.oneWay('currentUser.username'),
-
   selectedPdSetId: null,
   selectedFolderSetId: null,
-
-  hasId: Ember.computed.or('pubId', 'puzzId', 'subs'),
-  hasDate: Ember.computed.or('startDate', 'endDate'),
-
-  importMode: 0,
-  isPdImport: Ember.computed.equal('importMode', 0),
-  isPowImport: Ember.computed.equal('importMode', 1),
-
-  didReceiveAttrs: function() {
-
-  },
+  selectedAssignment: null,
+  selectedSection: null,
+  selectedProblem: null,
+  teacher: null,
 
   teacherPool: function() {
     const currentUser = this.get('currentUser');
@@ -60,13 +50,14 @@ Encompass.WorkspaceNewEncComponent = Ember.Component.extend(Encompass.CurrentUse
   },
 
   isAnswerCriteriaValid: function() {
-    const params = ['teacher', 'assignment', 'problem', 'section'];
+    const params = ['teacher', 'selectedAssignment', 'selectedProblem', 'selectedSection'];
     for (let param of params) {
-      if (!Ember.isEmpty(this.get(param))) {
+      console.log(param);
+      if (this.get(param)) {
         return true;
       }
     }
-  }.property('teacher', 'assignment', 'problem', 'section'),
+  }.property('teacher', 'selectedAssignment', 'selectedProblem', 'selectedSection'),
 
   isFormValid: Ember.computed.or('isDateRangeValid', 'isAnswerCriteriaValid'),
 
@@ -89,7 +80,7 @@ Encompass.WorkspaceNewEncComponent = Ember.Component.extend(Encompass.CurrentUse
       const endDate = this.get('endDate');
       console.log(startDate, endDate);
       const criteria = {
-        teacher: this.get('selectedTeacher'),
+        //teacher: this.get('selectedTeacher'),
         assignment: this.get('selectedAssignment'),
         problem: this.get('selectedProblem'),
         section: this.get('selectedSection'),
@@ -98,102 +89,27 @@ Encompass.WorkspaceNewEncComponent = Ember.Component.extend(Encompass.CurrentUse
       };
 
       const encWorkspaceRequest = this.store.createRecord('encWorkspaceRequest', criteria);
-      encWorkspaceRequest.save();
-
-
-
-    },
-
-    createWorkspace: function() {
-      var controller = this;
-      var doPoWImport = this.get('isPowImport');
-
-      Ember.run(function() {
-        if(doPoWImport) { controller.send('importWorkspace'); }
-        else { controller.send('newWorkspace'); }
-      });
-    },
-
-    newWorkspace: function() {
-      var controller    = this;
-      var pdSetName     = this.get('selectedPdSetId');
-      var folderSetName = this.get('selectedFolderSetId');
-
-      if(!pdSetName) {
-        pdSetName = 'default';
-      }
-
-      if(!folderSetName) {
-        folderSetName = 'none';
-      }
-
-      var request = this.store.createRecord('newWorkspaceRequest', {
-        pdSetName: pdSetName,
-        folderSetName: folderSetName
-      });
-
-      request.save().then(function(obj){
-        controller.transitionToRoute('workspaces.index');
-      });
-    },
-
-    importWorkspace: function() {
-      var controller = this;
-      var importData = { /*jshint camelcase: false */
-        teacher: this.get('teacher'),
-        submitter: this.get('submitter'),
-        publication: this.get('pubId'),
-        puzzle: this.get('puzzId'),
-        class_id: this.get('course'),
-        collection: this.get('newPdSet'),
-        folders: this.get('selectedFolderSet.id'),
-        since_date: Date.parse(this.get('startDate') ),
-        max_date: Date.parse( this.get('endDate') ),
-      };
-
-      if(this.get('pd')) {
-        //PD set name cannot be blank
-        if(Ember.isNone(importData.collection)) {
-          window.alert('PD set name required');
+      encWorkspaceRequest.save().then((res) => {
+        console.log('res ws', res);
+        if (res.get('isEmptyAnswerSet')) {
+          this.set('isEmptyAnswerSet', true);
+          return;
+        }
+        if (res.get('createWorkspaceError')) {
+          this.set('createWorkspaceError', res.get('createWorkspaceError'));
           return;
         }
 
-        //PD set name must be unique
-        if(this.get('pdSets').isAny('id', importData.collection)) {
-          window.alert('There is already a PD set by that name');
-          return;
-        }
-      }
-
-      if(!!this.get('subs')) {
-        importData.submissions = Ember.String.w( this.get('subs') );
-      }
-
-      var request = this.store.createRecord('importRequest', importData);
-      var output;
-
-      request.save().then(function(obj) {
-        var result = obj.get('results');
-        //var output = "Imported %@1 submissions!".fmt(result.imported);
-        var output = `Imported ${result.imported} submissions!`;
-
-        if(result.updatedExisting) {
-          output += "\nYou have workspace(s) for these!";
-        }
-
-        /*
-        if(result.addedNew) {
-          output += "\n\nAdded %@1 new submissions to your workspace".fmt(result.addedNew);
-        }
-        */
-
-        if( window.confirm(output) ) {
-          if(result.imported > 0) {
-            window.location.reload();
-          }
-        }
+        // currently redirecting to workspaces list if successful
+        // should we try to redirect to the individual workspace page?
+        this.sendAction('toWorkspaces');
+      })
+      .catch((err) => {
+        this.set('createWorkspaceError', err);
+        return;
       });
-    }
+    },
   }
-
 });
+
+
