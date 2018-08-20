14ff4017a171b627141ef8f962acf0edb67f10bd
philipwisner <philipwisner@gmail.com>
2018-08-20 11:05:14 -0400

    added sam's code

--- app/components/section-info.js
+++ app/components/section-info.js
@@ -41,21 +41,6 @@ Encompass.SectionInfoComponent = Ember.Component.extend(Encompass.CurrentUserMix
     });
   },
 
-  searchResults: function () {
-    var searchText = this.get('studentUsername');
-    console.log('search text is', searchText);
-    searchText = searchText.replace(/\W+/g, "");
-    if (searchText.length < 1) {
-      return;
-    }
-
-    let people = this.get('store').query('user', {
-      username: searchText
-    });
-    return people;
-  }.property('studentUsername'),
-
-
   willDestroyElement: function () {
     this.set('createdStudents', null);
   },
@@ -121,16 +106,12 @@ Encompass.SectionInfoComponent = Ember.Component.extend(Encompass.CurrentUserMix
       } else if (!section.get('students').contains(student)){
         section.get('students').pushObject(student);
         section.save();  //adds the new student (username) to database (backend)
-    }
+      }
 
-    // addStudent: function (student)  {
-    //   let section = this.get('section');
-    //   let students = section.get('students');
-
-    //   let sectionObj = {
-    //     sectionId: section.id,
-    //     role: 'student'
-    //   };
+      let sectionObj = {
+        sectionId: section.id,
+        role: 'student'
+      };
 
       return this.store.findRecord('user', student.id).then((student) => {
         console.log('student rec', student);
