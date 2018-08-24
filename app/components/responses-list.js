3b23caa5e0316c6787f267c1b4853967f6af7474
philipwisner <philipwisner@gmail.com>
2018-08-24 12:43:53 -0400

    students can now see responses from teachers

--- app/components/responses-list.js
+++ app/components/responses-list.js
@@ -7,20 +7,14 @@ Encompass.ResponsesListComponent = Ember.Component.extend(Encompass.CurrentUserM
     let currentUser = this.get('currentUser');
     let responses = this.get('responses');
     let yourResponses = responses.filterBy('createdBy.content', currentUser);
-    return yourResponses;
+    return yourResponses.sortBy('createDate').reverse();
   }),
 
-  // yourResponsesStudent: Ember.computed(function () {
-  //   console.log('your responses list running');
-  //   let currentUser = this.get('currentUser');
-  //   let userType = currentUser.get('accountType');
-  //   let isStudent = userType === 'S';
-  //   console.log('is the user a student?', isStudent);
-
-  //   let responses = this.store.findAll('response');
-  //   let yourResponses = responses.filterBy('recipient', currentUser.id);
-  //   console.log('your Responses', yourResponses);
-  //   return yourResponses;
-  // }),
+  responsesStudent: Ember.computed(function () {
+    let currentUser = this.get('currentUser');
+    let responses = this.get('responses');
+    let responsesStudent = responses.filterBy('recipient.content', currentUser);
+    return responsesStudent.sortBy('createDate').reverse();
+  }),
 
 });
