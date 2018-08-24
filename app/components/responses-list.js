329cd127d031afbea0300dbd54c5ca17080204ee
philipwisner <philipwisner@gmail.com>
2018-08-24 12:36:46 -0400

    responses now shows just the responses you created

--- app/components/responses-list.js
+++ app/components/responses-list.js
@@ -1,5 +1,26 @@
 Encompass.ResponsesListComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'responses-list',
   sortProperties: ['createDate'],
-  sortAscending: false
+  sortAscending: false,
+
+  yourResponses: Ember.computed(function () {
+    let currentUser = this.get('currentUser');
+    let responses = this.get('responses');
+    let yourResponses = responses.filterBy('createdBy.content', currentUser);
+    return yourResponses;
+  }),
+
+  // yourResponsesStudent: Ember.computed(function () {
+  //   console.log('your responses list running');
+  //   let currentUser = this.get('currentUser');
+  //   let userType = currentUser.get('accountType');
+  //   let isStudent = userType === 'S';
+  //   console.log('is the user a student?', isStudent);
+
+  //   let responses = this.store.findAll('response');
+  //   let yourResponses = responses.filterBy('recipient', currentUser.id);
+  //   console.log('your Responses', yourResponses);
+  //   return yourResponses;
+  // }),
+
 });
