c85cddc2a81e40562a04bac1dc423e2eab3222db
Daniel Kelly <djk8552@gmail.com>
2018-08-27 17:31:13 -0400

    Update model

--- app/models/enc_workspace_request.js
+++ app/models/enc_workspace_request.js
@@ -1,13 +1,16 @@
 Encompass.EncWorkspaceRequest = DS.Model.extend(Encompass.Auditable, {
-  teacher: DS.belongsTo('user', {inverse: null}),
-  assignment: DS.belongsTo('assignment', {inverse: null}),
-  problem: DS.belongsTo('problem', {inverse: null}),
-  section: DS.belongsTo('section', {inverse: null}),
+  teacher: DS.belongsTo('user', { inverse: null }),
+  assignment: DS.belongsTo('assignment', { inverse: null }),
+  problem: DS.belongsTo('problem', { inverse: null }),
+  section: DS.belongsTo('section', { inverse: null }),
   startDate: DS.attr('date'),
   endDate: DS.attr('date'),
   pdSetName: DS.attr('string'),
   folderSetName: DS.attr('string'),
+  requestedName: DS.attr('string'),
   createdWorkspace: DS.belongsTo('workspace'),
-  isEmptyAnswerSet: DS.attr('boolean', {default: null}),
-  createWorkspaceError: DS.attr('string')
+  isEmptyAnswerSet: DS.attr('boolean', { default: null }),
+  createWorkspaceError: DS.attr('string'),
+  owner: DS.belongsTo('user', { inverse: null }),
+  mode: DS.attr('string')
 });
