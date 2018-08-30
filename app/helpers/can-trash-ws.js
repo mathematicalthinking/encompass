437c54dde61d48cf2d7828141f3dc3fc1f79c281
Daniel Kelly <djk8552@gmail.com>
2018-08-30 09:24:27 -0400

    Validate args

--- app/helpers/can-trash-ws.js
+++ app/helpers/can-trash-ws.js
@@ -1,6 +1,9 @@
 Encompass.CanTrashWsHelper = Ember.Helper.helper( function(args) {
   let [ user, ws ] = args;
-  console.log(user, ws);
+
+  if (!user || !ws) {
+    return false;
+  }
   const isAdmin = user.get('accountType') === 'A';
   const isOwner = ws.get('owner.id') === user.id;
   return isAdmin || isOwner;
