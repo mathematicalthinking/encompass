0fa625475443c791a7be14d3a7c8708d45981017
Daniel Kelly <djk8552@gmail.com>
2018-08-29 23:22:32 -0400

    All, Mine, Public filters now work for workspace list
    Can delete a workspace if admin or owner of ws
    User is prompted to confirm if they want to delete workspace

--- app/controllers/workspaces_list_controller.js
+++ app/controllers/workspaces_list_controller.js
@@ -4,11 +4,83 @@
   * @author Amir Tahvildaran <amir@mathforum.org>
   * @since 1.0.0
 */
-Encompass.WorkspacesListController = Ember.Controller.extend({
+Encompass.WorkspacesListController = Ember.Controller.extend(Encompass.CurrentUserMixin, {
   sortProperties: ['name'],
+  workspaceToDelete: null,
+
   currentAsOf: function() {
     return moment(this.get('since')).format('H:mm');
-  }.property()
+  }.property(),
+
+  listFilter: 'all',
+
+  setDisplayList: function() {
+    console.log('running l');
+    const key = {
+      all: 'allWorkspaces',
+      mine: 'ownWorkspaces',
+      public: 'publicWorkspaces'
+    };
+
+    const filter = this.get('listFilter');
+
+    if (Ember.isEmpty(filter) || Ember.isEmpty(key[filter])) {
+      return this.get('model').rejectBy('isTrashed');
+    }
+
+    const listName = key[filter];
+    this.set('displayList', this.get(listName));
+  }.observes('listFilter', 'model.@each.isTrashed'),
+
+  setOwnWorkspaces: function() {
+    const currentUser = this.get('currentUser');
+    const workspaces = this.get('model').rejectBy('isTrashed');
+
+    this.set('ownWorkspaces', workspaces.filterBy('owner.id', currentUser.id));
+  }.observes('model.@each.isTrashed'),
+
+  setAllWorkspaces: function() {
+    this.set('allWorkspaces', this.get('model').rejectBy('isTrashed'));
+  }.observes('model.@each.isTrashed'),
+
+  test: function() {
+    console.log('something was trashed');
+
+  }.observes('model.@each.isTrashed'),
+
+  setPublicWorkspaces: function() {
+    console.log('public');
+    const workspaces = this.get('model').rejectBy('isTrashed');
+    this.set('publicWorkspaces', workspaces.filterBy('mode', 'public'));
+  }.observes('model.@each.isTrashed'),
+
+  actions: {
+    showModal: function(ws) {
+      this.set('workspaceToDelete', ws);
+    },
+    trashWorkspace: function(ws) {
+      const id = ws.id;
+      const workspaces = this.get('model');
+      const filtered = workspaces.filterBy('id', id);
+      console.log('filtered', filtered);
+      if (!Ember.isEmpty(filtered)) {
+        const ws = filtered.objectAt(0);
+        console.log('ws to trash', ws);
+        ws.set('isTrashed', true);
+        ws.save().then((rec) => {
+          console.log('rec', rec);
+          this.set('workspaceToDelete', null);
+        });
+      }
+    },
+
+    toggleFilter: function(key) {
+      if (key === this.get('listFilter')) {
+        return;
+      }
+      this.set('listFilter', key);
+    }
+  }
 });
 
 
