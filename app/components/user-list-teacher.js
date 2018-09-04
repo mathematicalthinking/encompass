18698938aea6202175e83c83643c47ccf108906e
philipwisner <philipwisner@gmail.com>
2018-09-04 13:09:13 -0400

    updating teacher user list

--- app/components/user-list-teacher.js
+++ app/components/user-list-teacher.js
@@ -14,36 +14,22 @@ Encompass.UserListTeacherComponent = Ember.Component.extend(Encompass.CurrentUse
     });
     console.log('your sections ids', yourSectionIds);
 
-    // return Promise.all(yourSectionIds.map((id) => {
-    //   return this.store.findRecord('section', id)
-    //     .then((section) => {
-    //       return section.get('students');
-    //     });
-    // }))
-    //   .then((students) => {
-    //     console.log('studnets', students);
-    //   })
-    //   .catch(console.log);
-
     let firstId = yourTeacherSections[0].sectionId;
     console.log('first sectionid is', firstId);
 
-    let section1 = this.store.findRecord('section', firstId)
+    let studentListing = [];
+    return this.store.findRecord('section', firstId)
       .then((section) => {
-        console.log('section is', section);
         let sectionStudents = section.get('students');
         sectionStudents.forEach((student) => {
           console.log('student name is', student.get('username'));
+          studentListing.push(student);
         });
-        console.log('section students', sectionStudents);
+        console.log('studentListing is', studentListing);
       });
-    // console.log('section 1 is', section1);
-    // let section1Students = section1.get('name');
-    // console.log('section1 name is', section1Students);
 
-    let users = this.users.filterBy('isTrashed', false);
-    let students = users.filterBy('isStudent', true);
-    return students.sortBy('createDate').reverse();
+
+    // return studentListing.sortBy('createDate').reverse();
   }.property('users.@each.accountType'),
 
 
@@ -66,16 +52,5 @@ Encompass.UserListTeacherComponent = Ember.Component.extend(Encompass.CurrentUse
     return orgUsersNotYou.sortBy('createDate').reverse();
   }.property('users.@each.isTrashed'),
 
-  actions: {
-    consoleInfo: function () {
-      let yourSections = this.get('currentUser').get('sections');
-      console.log('your sections', yourSections);
-      let sectionIdArray = [];
-      yourSections.forEach((section) => {
-        sectionIdArray.push(section.sectionId);
-      });
-      console.log('your sections ids are', sectionIdArray);
-    }
-  }
 
 });
