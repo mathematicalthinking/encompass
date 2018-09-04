201e11a10dad0bdefe37145f42f38257724107b2
Daniel Kelly <djk8552@gmail.com>
2018-09-04 16:28:15 -0400

    Styling

--- app/components/modal-delete.js
+++ app/components/modal-delete.js
@@ -1,4 +1,5 @@
 Encompass.ModalDeleteComponent = Ember.Component.extend({
+  elementId: 'modal-delete',
   actions: {
     delete: function() {
       this.sendAction('onConfirm', this.get('itemToDelete'));
