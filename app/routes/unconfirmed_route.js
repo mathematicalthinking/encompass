8997ee0697052c20665edf4822bb9ccb94d6ada4
Daniel Kelly <djk8552@gmail.com>
2018-09-05 10:46:50 -0400

    Redirect to home if unconfirmed page is reloaded and user's email has been confirmed

--- app/routes/unconfirmed_route.js
+++ app/routes/unconfirmed_route.js
@@ -1,3 +1,9 @@
 Encompass.UnconfirmedRoute = Ember.Route.extend({
-
+  beforeModel: function() {
+    // redirect to home if user is confirmed or if no user logged in
+    const user = this.modelFor('application');
+    if ((user && user.get('isEmailConfirmed') || !user.get('isAuthenticated'))){
+      this.transitionTo('/');
+    }
+  }
 });
