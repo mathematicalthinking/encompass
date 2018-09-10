f9b8308cd12bcb893da85bf11926447340129411
Daniel Kelly <djk8552@gmail.com>
2018-09-10 09:26:07 -0400

    Add pdadmin and teacher tests

--- test/mocha/userTest.js
+++ test/mocha/userTest.js
@@ -6,6 +6,7 @@ const _ = require('underscore');
 // REQUIRE FILES
 const fixtures = require('./fixtures.js');
 const helpers = require('./helpers');
+const userFixtures = require('./userFixtures');
 
 const expect = chai.expect;
 const host = helpers.host;
@@ -178,15 +179,17 @@ xdescribe('User CRUD operations as admin', function() {
   // });
 });
 
-describe('User CRUD operations as PdAdmin', function() {
+describe('User CRUD operations as Teacher with actingRole teacher', function() {
   this.timeout('10s');
   const agent = chai.request.agent(host);
+  const { username, password, organization, unaccessibleUser, accessibleUser, accessibleUserCount } = userFixtures.teacherMT;
 
-  const orgId = '5b4a64a028e4b75919c28512';
+  console.log('accessibleUser', accessibleUser);
+  console.log('type', username );
 
   before(async function(){
     try {
-      await helpers.setup(agent, helpers.pdAdmin.username, helpers.pdAdmin.password);
+      await helpers.setup(agent, username, password);
     }catch(err) {
       console.log(err);
     }
@@ -206,12 +209,12 @@ describe('User CRUD operations as PdAdmin', function() {
         expect(res.body.user).to.be.a('array');
 
         const users = res.body.user;
-        const orgUsers = users.filter(user => user.organization === orgId);
+        // const orgUsers = users.filter(user => user.organization === organization);
 
-        // checking that all users returned are from PdAdmins org
-        expect(users.length).to.eql(orgUsers.length);
+        // // checking that all users returned are from teachers org
+        // expect(users.length).to.eql(orgUsers.length);
 
-        expect(res.body.user.length).to.eql(14);
+        expect(res.body.user.length).to.eql(accessibleUserCount);
         done();
       });
     });
@@ -222,7 +225,7 @@ describe('User CRUD operations as PdAdmin', function() {
       it('should return 403 error', done => {
         agent
         .get(baseUrl)
-        .query('username=ssmith')
+        .query(`username=${unaccessibleUser.username}`)
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.have.all.keys('user');
@@ -233,16 +236,16 @@ describe('User CRUD operations as PdAdmin', function() {
       });
     });
 
-    describe('/GET user by username', () => {
+    describe('/GET accessible user by username', () => {
       it('should return user with the username "rick"', done => {
         agent
         .get(baseUrl)
-        .query('username=rick')
+        .query(`username=${accessibleUser.username}`)
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.have.all.keys('user');
           expect(res.body.user).to.be.a('array');
-          expect(res.body.user[0].username).to.eql('rick');
+          expect(res.body.user[0].username).to.eql(accessibleUser.username);
           done();
         });
       });
@@ -251,7 +254,7 @@ describe('User CRUD operations as PdAdmin', function() {
     /** GET **/
     describe('/GET unaccessible user by id', () => {
       it('should return 403 error', done => {
-        const url = baseUrl + '5b914a802ecaf7c30dd47493';
+        const url = baseUrl + unaccessibleUser._id;
         agent
         .get(url)
         .end((err, res) => {
@@ -262,15 +265,30 @@ describe('User CRUD operations as PdAdmin', function() {
     });
 
     describe('/GET user by id', () => {
-      it('should return the user "sam3"', done => {
-        const url = baseUrl + '5b913eaf3add43b868ae9806';
+      it('should return the user "teachertaylor"', done => {
+        const url = baseUrl + accessibleUser._id;
         agent
         .get(url)
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body).to.have.all.keys('user');
           expect(res.body.user).to.be.a('object');
-          expect(res.body.user.username).to.eql('sam3');
+          expect(res.body.user.username).to.eql(accessibleUser.username);
+          done();
+        });
+      });
+    });
+
+    describe('/GET createdBy from an accessible user', () => {
+      it('should return the user "rick"', done => {
+        const url = baseUrl + accessibleUser.createdBy;
+        agent
+        .get(url)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.all.keys('user');
+          expect(res.body.user).to.be.a('object');
+          expect(res.body.user.username).to.eql('rick');
           done();
         });
       });
@@ -294,14 +312,14 @@ describe('User CRUD operations as PdAdmin', function() {
 
     describe('/PUT update unaccessible user name', () => {
       it('should return 403 error', done => {
-        const url = baseUrl + fixtures.user._id;
+        const url = baseUrl + unaccessibleUser._id;
         agent
         .put(url)
         .send({
           user: {
             'name': 'test name',
-            'username': fixtures.user.validUser.username,
-            'accountType': fixtures.user.validUser.accountType,
+            'username': unaccessibleUser.username,
+            'accountType': unaccessibleUser.accountType,
           }
         })
         .end((err, res) => {
@@ -313,19 +331,192 @@ describe('User CRUD operations as PdAdmin', function() {
 
     describe('/PUT update user name', () => {
       it('should change steves name from null to test name', done => {
-        const url = baseUrl + fixtures.user._id;
+        const url = baseUrl + accessibleUser._id;
         agent
         .put(url)
         .send({
           user: {
             'name': 'test name',
-            'username': fixtures.user.validUser.username,
-            'accountType': fixtures.user.validUser.accountType,
+            'username': accessibleUser.username,
+            'accountType': accessibleUser.accountType,
           }
         })
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body.user.username).to.eql('steve');
+          expect(res.body.user.username).to.eql(accessibleUser.username);
+          expect(res.body.user.name).to.eql('test name');
+          done();
+        });
+      });
+    });
+});
+
+describe('User CRUD operations as PdAdmin', function() {
+  this.timeout('10s');
+  const agent = chai.request.agent(host);
+
+  const { username, password, organization, unaccessibleUser, accessibleUser, accessibleUserCount } = userFixtures.pdAdminDrexel;
+
+  before(async function(){
+    try {
+      await helpers.setup(agent, username, password);
+    }catch(err) {
+      console.log(err);
+    }
+  });
+
+  after(() => {
+    agent.close();
+  });
+
+  describe('/GET users', () => {
+    it('should get all users', done => {
+      agent
+      .get(baseUrl)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.all.keys('user');
+        expect(res.body.user).to.be.a('array');
+
+        const users = res.body.user;
+        const orgUsers = users.filter(user => user.organization === organization);
+
+        // checking that all users returned are from PdAdmins org
+        expect(users.length).to.eql(orgUsers.length);
+
+        expect(res.body.user.length).to.eql(accessibleUserCount);
+        done();
+      });
+    });
+  });
+
+    /** GET **/
+    describe('/GET unaccessible user by username', () => {
+
+      it('should return 403 error', done => {
+        agent
+        .get(baseUrl)
+        .query(`username=${unaccessibleUser.username}`)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.all.keys('user');
+          expect(res.body.user).to.be.a('array');
+          expect(res.body.user.length).to.eql(0);
+          done();
+        });
+      });
+    });
+
+    // admin from same org, also creator
+    describe('/GET accessible user by username', () => {
+      it('should return user with the username "rick"', done => {
+        agent
+        .get(baseUrl)
+        .query(`username=${accessibleUser.username}`)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.all.keys('user');
+          expect(res.body.user).to.be.a('array');
+          expect(res.body.user[0].username).to.eql(accessibleUser.username);
+          done();
+        });
+      });
+    });
+
+    /** GET **/
+    describe('/GET unaccessible user by id', () => {
+      it('should return 403 error', done => {
+        const url = baseUrl + unaccessibleUser._id;
+        agent
+        .get(url)
+        .end((err, res) => {
+          expect(res).to.have.status(403);
+          done();
+        });
+      });
+    });
+
+    // student from same org
+    describe('/GET user by id', () => {
+      it('should return the user "sam3"', done => {
+        const url = baseUrl + accessibleUser._id;
+        agent
+        .get(url)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.all.keys('user');
+          expect(res.body.user).to.be.a('object');
+          expect(res.body.user.username).to.eql(accessibleUser.username);
+          done();
+        });
+      });
+    });
+
+    // describe('/GET createdBy from an accessible user', () => {
+    //   it('should return the user "rick"', done => {
+    //     const url = baseUrl + '5b245760ac75842be3189525';
+    //     agent
+    //     .get(url)
+    //     .end((err, res) => {
+    //       expect(res).to.have.status(200);
+    //       expect(res.body).to.have.all.keys('user');
+    //       expect(res.body.user).to.be.a('object');
+    //       expect(res.body.user.username).to.eql('rick');
+    //       done();
+    //     });
+    //   });
+    // });
+
+
+    /** POST **/
+    xdescribe('/POST user', () => {
+      it('should post a new user', done => {
+        agent
+        .post(baseUrl)
+        .send({user: fixtures.user.validUser})
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.user).to.have.any.keys('username');
+          expect(res.body.user.username).to.eql('testUser3');
+          done();
+        });
+      });
+    });
+
+    describe('/PUT update unaccessible user name', () => {
+      it('should return 403 error', done => {
+        const url = baseUrl + unaccessibleUser._id;
+        agent
+        .put(url)
+        .send({
+          user: {
+            'name': 'test name',
+            'username': unaccessibleUser.username,
+            'accountType': unaccessibleUser.accountType,
+          }
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(403);
+          done();
+        });
+      });
+    });
+
+    describe('/PUT update accessible user name', () => {
+      it('should change sam3s name from null to test name', done => {
+        const url = baseUrl + accessibleUser._id;
+        agent
+        .put(url)
+        .send({
+          user: {
+            'name': 'test name',
+            'username': accessibleUser.username,
+            'accountType': accessibleUser.accountType,
+          }
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.user.username).to.eql('sam3');
           expect(res.body.user.name).to.eql('test name');
           done();
         });
