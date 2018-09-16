dd3a8e8de3b8b6526e1d67972feb45159fa9e41f
Daniel Kelly <djk8552@gmail.com>
2018-09-16 17:17:42 -0400

    Refactor to be more general

--- app/components/twitter-typeahead.js
+++ app/components/twitter-typeahead.js
@@ -40,7 +40,25 @@ Encompass.TwitterTypeaheadComponent = Ember.Component.extend({
 
       that.set('selectedValue', suggestion);
       that.sendAction('onSelect', suggestion);
-      that.$('.typeahead').typeahead('val', '');
+      if (that.get('allowMultiple')) {
+        that.$('.typeahead').typeahead('val', '');
+      }
+    });
+
+    this.$('.typeahead').on('typeahead:change', function(ev, val) {
+      console.log('on change event: ', ev);
+      console.log('Val on change: ' + val);
+
+      let inputValue = that.$('.typeahead').typeahead('val');
+      if (inputValue === that.get('selectedValue')) {
+        return;
+      }
+
+      that.set('selectedValue', inputValue);
+      that.sendAction('onSelect', inputValue);
+      if (that.get('allowMultiple')) {
+        that.$('.typeahead').typeahead('val', '');
+      }
     });
   },
 
@@ -63,18 +81,29 @@ Encompass.TwitterTypeaheadComponent = Ember.Component.extend({
     const that = this;
 
     return function findMatches(q, cb) {
-      var matches, substrRegex;
+      var matches, substrRegex, filtered, pool;
 
-      let selectedItems = that.get('creators');
-      if (!selectedItems) {
-        selectedItems = [];
+
+      if (that.get('allowMultiple')) {
+        // filter out already selected items
+
+        let selectedItems = that.get('selectedItems');
+        if (!selectedItems) {
+          selectedItems = [];
+        }
+        selectedItems = selectedItems.mapBy(path);
+
+        filtered = suggestions.filter((item) => {
+          let alreadySelected = selectedItems.includes(item);
+          return !alreadySelected;
+        });
       }
-      selectedItems = selectedItems.mapBy('username');
 
-    let filtered = suggestions.filter((item) => {
-      let alreadySelected = selectedItems.includes(item);
-      return !alreadySelected;
-    });
+      if (filtered) {
+        pool = filtered;
+      } else {
+        pool = suggestions;
+      }
 
       // an array that will be populated with substring matches
       matches = [];
@@ -84,7 +113,7 @@ Encompass.TwitterTypeaheadComponent = Ember.Component.extend({
 
       // iterate through the pool of strings and for any string that
       // contains the substring `q`, add it to the `matches` array
-      $.each(filtered, function(i, str) {
+      $.each(pool, function(i, str) {
         if (substrRegex.test(str)) {
           matches.push(str);
         }
