228f9d1dcdd1aa27445d80f40a9b0c778673027a
Daniel Kelly <djk8552@gmail.com>
2018-09-16 14:04:36 -0400

    Twitter-typeahead component

---
+++ app/components/twitter-typeahead.js
@@ -0,0 +1,87 @@
+Encompass.TwitterTypeaheadComponent = Ember.Component.extend({
+  classNames: ['twitter-typeahead'],
+  alreadySelected: Ember.A(),
+
+  init() {
+    this._super(...arguments);
+  },
+
+  didReceiveAttrs(){
+    this._super(...arguments);
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    const that = this;
+    let dataList = this.get('dataList');
+
+    const name = this.get('listName');
+    const containerId = this.get('containerId');
+
+    $(`#${containerId} .typeahead-${containerId}`).typeahead({
+      hint: false,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      name: name,
+      source: that.substringMatcher(dataList),
+    });
+
+    $(`.typeahead-${containerId}`).on('typeahead:select', function(ev, suggestion) {
+      console.log('Selection: ' + suggestion);
+
+      that.set('selectedValue', suggestion);
+      that.sendAction('onSelect', suggestion);
+      $(`.typeahead-${containerId}`).typeahead('val', '');
+    });
+  },
+
+  substringMatcher: function(data) {
+    // data should be array of ember objects
+    const that = this;
+    let path = this.get('optionLabelPath');
+    if (!path) {
+      path = 'id';
+    }
+
+    if (!data) {
+      data = [];
+    }
+    let suggestions = data.map((obj) => {
+      return obj.get(path);
+    });
+
+    return function findMatches(q, cb) {
+      var matches, substrRegex;
+
+      let selectedItems = that.get('creators');
+      if (!selectedItems) {
+        selectedItems = [];
+      }
+      selectedItems = selectedItems.mapBy('username');
+
+    let filtered = suggestions.filter((item) => {
+      let alreadySelected = selectedItems.includes(item);
+      return !alreadySelected;
+    });
+
+      // an array that will be populated with substring matches
+      matches = [];
+
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
+
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(filtered, function(i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+
+      cb(matches);
+    };
+  }
+
+});
