b42381832eae300a31b44681209f19ecb296f33a
Daniel Kelly <djk8552@gmail.com>
2018-09-17 17:54:33 -0400

    Add unauthorized route and un-authorized component/template

---
+++ app/routes/unauthorized_route.js
@@ -0,0 +1,26 @@
+Encompass.UnauthorizedRoute = Ember.Route.extend({
+  beforeModel: function() {
+    // redirect to home if user is authorized or if no user logged in
+    const user = this.modelFor('application');
+    if ((user && user.get('isAuthz') || !user.get('isAuthenticated'))){
+      this.transitionTo('/');
+    }
+  },
+
+  model: function() {
+    let user = this.modelFor('application');
+    console.log('user', user);
+    console.log('needs info', user.get('needAdditionalInfo'));
+    if (user.get('needAdditionalInfo')) {
+      return this.store.findAll('organization');
+    }
+    return;
+
+  },
+
+  actions: {
+    toHome: function () {
+      window.location.href = '/';
+    }
+  }
+});
