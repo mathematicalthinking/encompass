666d2266d7dd5b3df531eb59497379c19686efe9
philipwisner <philipwisner@gmail.com>
2018-09-19 14:12:56 -0400

    updated assignment list filtering

--- app/components/assignment-list.js
+++ app/components/assignment-list.js
@@ -1,8 +1,8 @@
 Encompass.AssignmentListComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   didReceiveAttrs: function() {
     this.filterAssignments();
-
   },
+
   filterAssignments: function() {
     let filtered = this.assignments.filter((assignment) => {
       return assignment.id && !assignment.get('isTrashed');
@@ -12,4 +12,37 @@ Encompass.AssignmentListComponent = Ember.Component.extend(Encompass.CurrentUser
   }.observes('assignments.@each.isTrashed', 'currentUser.isStudent'),
 
 
+  yourList: function() {
+    let currentUser = this.get('currentUser');
+    let yourList = this.assignments.filter((assignment) => {
+      let userId = currentUser.get('id');
+      let assigmentCreator = assignment.get('createdBy');
+      return userId === assigmentCreator.get('id');
+    });
+    return yourList.sortBy('createDate').reverse();
+  }.property('assignments.@each.isTrashed', 'currentUser.isStudent'),
+
+
+  adminList: function() {
+    let currentUser = this.get('currentUser');
+    let adminList = this.get('assignmentList').filter((assignment) => {
+      let userId = currentUser.get('id');
+      let assigmentCreator = assignment.get('createdBy');
+      return userId !== assigmentCreator.get('id');
+    });
+    return adminList.sortBy('createDate').reverse();
+  }.property('assignments.@each.isTrashed', 'currentUser.isStudent'),
+
+  pdList: function () {
+    let currentUser = this.get('currentUser');
+    let pdList = this.get('assignmentList').filter((assignment) => {
+      let userId = currentUser.get('id');
+      let assigmentCreator = assignment.get('createdBy');
+      return userId !== assigmentCreator.get('id');
+    });
+    return pdList.sortBy('createDate').reverse();
+  }.property('assignments.@each.isTrashed', 'currentUser.isStudent'),
+
 });
+
+
