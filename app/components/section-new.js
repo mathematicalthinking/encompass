afeca1be0806906ff8d7f8b316e3714001121a9c
Daniel Kelly <djk8552@gmail.com>
2018-09-21 13:41:30 -0400

    Update seciton-new to use typeahead-twitter for selecting teacher

--- app/components/section-new.js
+++ app/components/section-new.js
@@ -17,51 +17,66 @@ Encompass.SectionNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
     var user = this.get('user');
     var teachers = this.get('teachers');
     if (!user.get('isAdmin')) {
-      this.set('leader', user);
-      teachers.pushObject(user);
+      this.set('teacher', user);
+      // this.set('leader', user);
+      // teachers.pushObject(user);
     }
   },
+
+  setTeacher: function() {
+    let teacher = this.get('teacher');
+    if (!teacher) {
+      return;
+    }
+
+    let organization = teacher.get('organization');
+
+    if (organization) {
+      this.set('organization', organization);
+    } else {
+      this.set('organization', this.get('currentUser.organization'));
+    }
+  }.observes('teacher'),
 
   actions: {
     createSection: function () {
       var that = this;
       var newSectionName = this.get('newSectionName');
-      if (!newSectionName) {
+      var organization = this.get('organization');
+      var teacher = this.get('teacher');
+
+      if (!newSectionName || !teacher || !organization) {
         this.set('missingFieldsError', true);
         return;
       }
-      //var organization = this.get('selectedOrganization');
-      var user = this.get('user');
-      var organization = user.get('organization');
-      this.set('userOrg', organization);
-      var teacher = this.get('teacher');
       var currentUser = this.get('currentUser');
       //var leader = this.get('leader');
-      var teachers = this.get('teachers');
-      if (user.get('isAdmin')) {
-        //check if user exists
-        let users = this.users.filterBy('username', teacher);
-        if (!Ember.isEmpty(users)) {
-          let user = users.get('firstObject');
-          teachers.pushObject(user);
-          let userOrg = user.get('organization');
-          this.set('userOrg', userOrg);
-        } else {
-          this.set('invalidTeacherUsername', true);
-          return;
-        }
-      }
+      // var teachers = this.get('teachers');
+      // if (user.get('isAdmin')) {
+      //   //check if user exists
+      //   let users = this.users.filterBy('username', teacher);
+      //   if (!Ember.isEmpty(users)) {
+      //     let user = users.get('firstObject');
+      //     teachers.pushObject(user);
+      //     let userOrg = user.get('organization');
+      //     this.set('userOrg', userOrg);
+      //   } else {
+      //     this.set('invalidTeacherUsername', true);
+      //     return;
+      //   }
+      // }
 
       var sectionData = this.store.createRecord('section', {
         name: newSectionName,
-        organization: this.get('userOrg'),
+        organization: this.get('organization'),
         createdBy: currentUser,
       });
 
+      sectionData.get('teachers').addObject(teacher);
 
-      for (let teacher of teachers) {
-        sectionData.get('teachers').addObject(teacher);
-      }
+      // for (let teacher of teachers) {
+      //   sectionData.get('teachers').addObject(teacher);
+      // }
 
       sectionData.save()
       .then((section) => {
