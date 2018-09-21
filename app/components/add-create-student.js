28b94ec3a46e69aa9f4a56ab6f5e5c86c97901f9
Daniel Kelly <djk8552@gmail.com>
2018-09-21 11:27:40 -0400

    Add update sectionPassword functionality

--- app/components/add-create-student.js
+++ app/components/add-create-student.js
@@ -3,44 +3,38 @@ Encompass.AddCreateStudentComponent = Ember.Component.extend({
   isUsingDefaultPassword: false,
   fieldType: 'password',
 
-  clearFields: function(doInputs, doErrors, doExistingUser) {
-    let fields = [];
+  clearCreateInputs: function() {
+    let fields = ['username', 'name', 'password'];
 
-    if (doInputs) {
-      fields = fields.concat(['username', 'name', 'password']);
+    for (let field of fields) {
+      this.set(field, null);
+    }
+  },
+  clearAddExistingUser: function() {
+  let fields = ['canAddExistingUser', 'canAddExistingUser', 'canAddExistingUser'];
+
+    for (let field of fields) {
+      this.set(field, null);
+    }
+  },
+
+  addStudent: function() {
+    let student = this.get('studentToAdd');
+    if (!student) {
+      return;
     }
 
-  if (doErrors) {
-    let errors = ['userAlreadyInSection', 'isMissingCredentials'];
-    fields = fields.concat(errors);
-  }
+    let students = this.get('students');
 
-  if (doExistingUser) {
-    fields = fields.concat(['canAddExistingUser', 'existingUser']);
-  }
+    if (students.includes(student)) {
+      this.set('userAlreadyInSection', true);
+      this.set('studentToAdd', null);
+      return;
+    }
 
-  for (let field of fields) {
-    this.set(field, null);
-  }
-},
-
-addStudent: function() {
-  let student = this.get('studentToAdd');
-  if (!student) {
-    return;
-  }
-
-  let students = this.get('students');
-
-  if (students.includes(student)) {
-    this.set('userAlreadyInSection', true);
+    students.pushObject(student);
     this.set('studentToAdd', null);
-    return;
-  }
-
-  students.pushObject(student);
-  this.set('studentToAdd', null);
-}.observes('studentToAdd'),
+  }.observes('studentToAdd'),
 
 
 
@@ -55,9 +49,6 @@ addStudent: function() {
     let sectionId = this.get('section').id;
     let sectionRole = 'student';
     let currentUser = that.get('currentUser');
-    let isUsingDefaultPassword = this.get('isUsingDefaultPassword');
-    //let students = section.get('students');
-
 
     let createUserData = {
       name,
@@ -83,20 +74,17 @@ addStudent: function() {
           that.set('usernameAlreadyExists', true);
           return;
         } else if (res.message === 'Can add existing user') {
-          console.log('this inside', this);
           this.set('canAddExistingUser', true);
           this.set('existingUser', res.user);
         } else {
           let userId = res._id;
           let section = this.get('section');
           let students = section.get('students');
-          let studentPassword = section.get('studentPassword');
-          console.log('section password is currently', studentPassword);
           return this.store.findRecord('user', userId)
             .then((user) => {
               students.pushObject(user);   //add student to students aray
               section.save();  //save section
-              that.clearFields(true, true, true);
+              that.clearCreateInputs();
             })
             .catch((err) => {
               console.log(err);
@@ -110,9 +98,8 @@ addStudent: function() {
 
   actions: {
     showPassword: function () {
-      var isShowingPassword = this.get('showingPassword');
-      console.log('isShowingPassword =', isShowingPassword);
-      if (isShowingPassword === false) {
+      let isShowingPassword = this.get('showingPassword');
+      if (!isShowingPassword) {
         this.set('showingPassword', true);
         this.set('fieldType', 'text');
       } else {
@@ -130,11 +117,17 @@ addStudent: function() {
       this.store.findRecord('user', student._id).then((user) => {
         if (!students.includes(user)) {
           students.pushObject(user);
-          this.clearFields(true, false, true);
+          this.clearAddExistingUser();
+          this.clearCreateInputs();
         } else {
           this.set('userAlreadyInSection', true);
         }
       });
+    },
+
+    exitAddExistingUsername: function () {
+      this.clearAddExistingUser();
+      this.clearCreateInputs();
     },
 
     validateCreateStudent: function() {
@@ -144,7 +137,7 @@ addStudent: function() {
       const isUsingDefaultPassword = this.get('isUsingDefaultPassword');
 
       if (isUsingDefaultPassword) {
-        password = this.get('defaultPassword');
+        password = this.get('sectionPassword');
       } else {
         password = this.get('password');
       }
@@ -161,6 +154,10 @@ addStudent: function() {
         return;
       }
 
+      if (this.get('incorrectUsername')) {
+        return;
+      }
+
       const name = this.get('name');
 
       const ret = {
@@ -171,7 +168,44 @@ addStudent: function() {
       this.createStudent(ret);
     },
 
+    usernameValidate() {
+      var username = this.get('username');
+      if (username) {
+        var usernamePattern = new RegExp(/^[a-z0-9.\-_@]{3,64}$/);
+        var usernameTest = usernamePattern.test(username);
+
+        if (usernameTest === false) {
+          this.set('incorrectUsername', true);
+          return;
+        }
+
+        if (usernameTest === true) {
+          this.set('incorrectUsername', false);
+          this.set('isMissingCredentials', false);
+          return;
+        }
+      }
+    },
+
+    checkError: function () {
+      let errors = ['usernameAlreadyExists', 'userAlreadyInSection', 'isMissingCredentials'];
+
+      for (let error of errors) {
+        if (this.get(error)) {
+          this.set(error, null);
+        }
+      }
+    },
+
+    updateSectionPassword: function() {
+      this.set('isEditingSectionPassword', false);
+      let section = this.get('section');
+      if (section.get('dirtyType') === 'updated') {
+        section.save().then(() => {
+          console.log('section password updated!');
+        });
+      }
+
+    }
   }
-
-
 });
