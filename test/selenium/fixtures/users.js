b6162a99277b13879c76252f8a6856acd430113e
Daniel Kelly <djk8552@gmail.com>
2018-09-29 17:25:11 -0400

    User fixtures for selenium tests

---
+++ test/selenium/fixtures/users.js
@@ -0,0 +1,40 @@
+module.exports = {
+  pdAdminDrexel: {
+    testDescriptionTitle: 'PD Admin',
+    username: 'pdadmin',
+    password: 'pdadmin',
+    accountType: 'P',
+
+  },
+
+  teacherMT: {
+    testDescriptionTitle: 'Teacher',
+    username: 'ssmith',
+    password: 'ssmith',
+    accountType: 'T',
+  },
+
+  admin: {
+    testDescriptionTitle: 'Admin',
+    username: 'rick',
+    password: 'sanchez',
+    accountType: 'A',
+  },
+
+  studentMT: {
+    testDescriptionTitle: 'Student',
+    username: 'tracyc',
+    password: 'tracyc',
+    organization: '5b4e4d5f808c7eebc9f9e82c',
+    accountType: 'S',
+  },
+
+  teacherActingStudent: {
+    testDescriptionTitle: 'Teacher acting as Student',
+    username: 'actingstudent',
+    password: 'allison',
+    accountType: 'T',
+    actingRole: 'student',
+  }
+
+};
