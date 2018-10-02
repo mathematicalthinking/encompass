1458cc5c5483fbf63d462c3826ee3797852c4dcb
Daniel Kelly <djk8552@gmail.com>
2018-10-02 13:19:08 -0400

    Pagination/search functionality/layout for public problem list

--- app/components/problem-list.js
+++ app/components/problem-list.js
@@ -2,6 +2,35 @@ Encompass.ProblemListComponent = Ember.Component.extend(Encompass.CurrentUserMix
   elementId: 'problem-list',
   classNames: ['problem-list', 'left-list'],
   yourProblemList: null,
+  publicFilter: {
+    privacySetting: 'E'
+  },
+  dataLoadErrors: [],
+  publicCurrentPage: null,
+  publicGoToPage: null,
+
+  // showTopPageNav: function() {
+  //   let count = this.get('publicProblems.length');
+  //   let searchCount = this.get('publicSearchResults.length');
+  //   return count > 20 || searchCount > 20;
+  // }.property('publicProblems.[]', 'publicSearchResults.[]'),
+
+  didReceiveAttrs: function() {
+    this.store.query('problem', {
+      filterBy: {
+        privacySetting: 'E'
+      }
+    }).then((res) => {
+      this.set('publicQueryResults', res);
+      this.set('publicProblemsMetadata', res.get('meta'));
+    }).catch((err) => {
+      this.handleErrors(err, 'dataLoadErrors');
+    });
+  },
+
+  init: function() {
+    this._super(...arguments);
+  },
 
 
   // This displays only the problems beloging to the current user
@@ -27,7 +56,11 @@ Encompass.ProblemListComponent = Ember.Component.extend(Encompass.CurrentUserMix
 
   // This sorts all the problems that are visible to everyone
   publicProblems: function () {
-    var problems = this.problems.filterBy('isTrashed', false);
+    var queryResults = this.get('publicQueryResults');
+    if (!queryResults) {
+      return [];
+    }
+    var problems = queryResults.filterBy('isTrashed', false);
     var currentUser = this.get('currentUser');
     var publicProblems = problems.filterBy('privacySetting', 'E');
     var yourPublic = publicProblems.filter((el) => {
@@ -35,7 +68,50 @@ Encompass.ProblemListComponent = Ember.Component.extend(Encompass.CurrentUserMix
       return content.id !== currentUser.id;
     });
     return yourPublic.sortBy('createDate').reverse();
-  }.property('problems.@each.isTrashed', 'currentUser.isStudent'),
+  }.property('publicQueryResults.@each.isTrashed', 'currentUser.isStudent'),
+
+  actions: {
+    searchPublic: function() {
+      let searchText = this.get('publicSearchText');
+      if (!searchText) {
+        return;
+      }
+      this.set('searchQuery', searchText);
+      let filter = this.get('publicFilter');
+      if (filter) {
+        filter.title = searchText;
+      } else {
+        filter = {
+          title: searchText
+        };
+      }
+
+      this.store.query('problem', {
+       filterBy: filter
+      }).then((results) => {
+        this.set('publicSearchResults', results);
+        this.set('publicSearchMetadata', results.get('meta'));
+      });
+    },
+    clearPublicResults: function() {
+      this.set('publicSearchText', null);
+      this.set('searchQuery', null);
+      this.set('publicSearchResults', null);
+
+      let publicFilter = this.get('publicFilter');
+      if (publicFilter) {
+        delete publicFilter.title;
+      }
+      this.store.query('problem', {}).then((results) => {
+        this.set('publicList', results);
+        this.set('metadata', results.get('meta'));
+      }).catch((err) => {
+        this.handleErrors(err, 'dataLoadErrors');
+      });
+    },
+  },
+
+
 
 });
 
