3ecf31e5ab0ca42876d67273d9b7e0f359b8e6b4
Daniel Kelly <djk8552@gmail.com>
2018-10-14 22:09:42 -0400

    Add class bindings to problem-list-item for display icons

--- app/components/problem-list-item.js
+++ app/components/problem-list-item.js
@@ -1,3 +1,23 @@
 Encompass.ProblemListItemComponent = Ember.Component.extend({
-  classNames: ['problem-list-item']
+  classNames: ['problem-list-item'],
+  classNameBindings: ['isPublic', 'isPrivate', 'isOrg', 'isPows'],
+  privacySetting: Ember.computed.alias('problem.privacySetting'),
+  puzzleId: Ember.computed.alias('problem.puzzleId'),
+
+  isPublic: function() {
+    return this.get('privacySetting') === 'E';
+  }.property('problem.privacySetting'),
+
+  isOrg: function() {
+    return this.get('privacySetting') === 'O';
+  }.property('problem.privacySetting'),
+
+  isPrivate: function() {
+    return this.get('privacySetting') === 'M';
+  }.property('problem.privacySetting'),
+
+  isPows: function() {
+    return this.get('puzzleId') !== null && this.get('puzzleId') !== undefined;
+  }.property('problem.puzzleId')
+
 });
