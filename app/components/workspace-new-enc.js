22d4e978c4b84282c874314aa2af2651d7172b8f
Daniel Kelly <djk8552@gmail.com>
2018-10-15 09:46:57 -0400

    Only display org users in teacher/owner dropdowns for pdadmins on workspace-new form

--- app/components/workspace-new-enc.js
+++ app/components/workspace-new-enc.js
@@ -59,27 +59,17 @@ Encompass.WorkspaceNewEncComponent = Ember.Component.extend(Encompass.CurrentUse
 
     let teachers = this.get('userList').rejectBy('accountType', 'S');
     let authTeachers = teachers.filterBy('isAuthorized', true);
-    return authTeachers;
 
-    // if (accountType === 'P') {
-    //   let yourUsers = this.get('userList');
-    //   let yourUserList = [];
-    //   return yourUsers.filterBy('isAuthorized');
-    //   // yourUsers.forEach((user) => {
-    //   //   let yourOrg = currentUser.get('organization').get('id');
-    //   //   let userType = user.get('accountType');
-    //   //   let userOrg = user.get('organization').get('id');
-    //   //   let isAuth = user.get('isAuthorized');
 
-    //   //   if (yourOrg === userOrg && userType === 'T' && isAuth) {
-    //   //     yourUserList.push(user);
-    //   //   }
-    //   // });
-    //   return yourUserList;
-    // }
-    // if (accountType === 'A') {
-    //   return users.filterBy('isAuthorized', true);
-    // }
+    if (accountType === 'P') {
+    let pdOrg = currentUser.get('organization');
+      let orgTeachers = authTeachers.filterBy('organization', pdOrg);
+      return orgTeachers;
+
+    }
+    if (accountType === 'A') {
+      return authTeachers;
+    }
   },
 
   isDateRangeValid: function() {
