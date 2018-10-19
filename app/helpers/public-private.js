664be1dbb61327155867934ff844a20cefa0d1f9
philipwisner <philipwisner@gmail.com>
2018-10-19 16:23:17 -0400

    added height

--- app/helpers/public-private.js
+++ app/helpers/public-private.js
@@ -1,9 +1,7 @@
 Encompass.PublicPrivateHelper = Ember.Helper.helper(function (setting) {
   if (setting[0] === "O" || setting[0] === "E") {
-    // return 'public';
     return '<i class="fas fa-globe-americas"></i>';
   } else {
-    // return 'private';
     return '<i class="fas fa-lock"></i>';
   }
 
