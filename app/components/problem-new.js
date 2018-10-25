120b49b7cea6fa7c9297c50a6fbe57c41a48beee
philipwisner <philipwisner@gmail.com>
2018-10-25 11:37:20 -0400

    you can create a new problem with just basics

--- app/components/problem-new.js
+++ app/components/problem-new.js
@@ -18,13 +18,13 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
   init: function () {
     this._super(...arguments);
     let tooltips = {
-        name: 'Please try and give all your problems a unique name',
+        name: 'Please try and give all your problems a unique title',
         statement: 'Content of the problem to be completed',
         categories: 'Use category menu to select appropriate common core categories',
         keywords: 'Add keywords to help other people find this problem',
         additionalInfo: 'Any additional information desired for the problem',
         additionalImage: 'You can upload a JPG, PNG or PDF (only the first page is saved)',
-        privacySettings: 'Just Me makes your problem private, My Organization allows your problem to be seen by all members in your organization, and public means every user can see your problem',
+        privacySettings: 'Just Me makes your problem private, My Organization allows your problem to be seen by all members in your organization, and Public means every user can see your problem',
         copyrightNotice: 'Add notice if problem contains copyrighted material',
         sharingAuth: 'If you are posting copyrighted material please note your permission',
         legalNotice: 'Please verify that the material you are posting is either your own or properly authorized to share',
@@ -39,21 +39,6 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
 
     let isMissing = this.checkMissing.bind(this);
     this.get('validator').initialize(formId, isMissing);
-
-    var options = {
-      debug: 'false',
-      modules: {
-        toolbar: [
-        ['bold', 'italic', 'underline'],
-        ['image'],
-        ]
-      },
-      placeholder: 'Problem Statement',
-      theme: 'snow'
-    };
-    var quill = new window.Quill('#editor', options); // eslint-disable-line no-unused-vars
-
-    $('.list-outlet').removeClass('hidden');
   },
 
   checkMissing: function() {
@@ -90,41 +75,40 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
   },
 
   createProblem: function() {
-    var that = this;
-    const quillContent = this.$('.ql-editor').html();
-    const problemStatement = quillContent.replace(/["]/g, "'");
-    var createdBy = that.get('currentUser');
-    var title = that.get('title');
-    //var categories = [];
-    var additionalInfo = that.get('additionalInfo');
-    var privacySetting = that.get('privacySetting');
-    var currentUser = that.get('currentUser');
-    var organization = currentUser.get('organization');
-    var categories = this.get('selectedCategories');
-    var copyrightNotice = that.get('copyrightNotice');
-    var sharingAuth = that.get('sharingAuth');
+    let that = this;
+    const problemStatement = this.get('problemStatement');
+    let createdBy = that.get('currentUser');
+    let title = that.get('problemTitle');
+    let additionalInfo = that.get('additionalInfo');
+    let privacySetting = that.get('privacySetting');
+    let currentUser = that.get('currentUser');
+    let organization = currentUser.get('organization');
+    let categories = this.get('selectedCategories');
+    let copyrightNotice = that.get('copyrightNotice');
+    let sharingAuth = that.get('sharingAuth');
 
     if (!this.get('approvedProblem')) {
       this.set('noLegalNotice', true);
       return;
     }
 
-    var createProblemData = that.store.createRecord('problem', {
+    let createProblemData = that.store.createRecord('problem', {
       createdBy: createdBy,
       createDate: new Date(),
       title: title,
       text: problemStatement,
-      categories: categories,
-      additionalInfo: additionalInfo,
+      // categories: categories,
+      // additionalInfo: additionalInfo,
       privacySetting: privacySetting,
       organization: organization,
-      copyrightNotice: copyrightNotice,
-      sharingAuth: sharingAuth
+      status: 'approved',
+      // copyrightNotice: copyrightNotice,
+      // sharingAuth: sharingAuth
     });
 
     if (that.filesToBeUploaded) {
-      var uploadData = that.get('filesToBeUploaded');
-      var formData = new FormData();
+      let uploadData = that.get('filesToBeUploaded');
+      let formData = new FormData();
       for(let f of uploadData) {
         formData.append('photo', f);
       }
@@ -190,14 +174,6 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
       }
     },
 
-  confirmCreatePublic: function() {
-    this.get('alert').showModal('question', 'Are you sure you want to create a public problem?', 'Creating a public problem means it will be accessible to all EnCoMPASS users. You will not be able to make any changes once this problem has been used', 'Yes')
-    .then((result) => {
-      if (result.value) {
-        this.createProblem();
-      }
-    });
-  },
 
   actions: {
     radioSelect: function(value) {
@@ -205,24 +181,12 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
     },
 
     validate: function() {
-      let title = this.get('title');
-      let privacySetting = this.get('privacySetting');
-
-      let isQuillValid = this.isQuillValid();
-      if (!isQuillValid || !title || !privacySetting) {
-        this.set('isMissingRequiredFields', true);
-        return;
-      }
-
       if (!this.get('approvedProblem')) {
         this.set('noLegalNotice', true);
         return;
       }
       if (this.get('isMissingRequiredFields')) {
         this.set('isMissingRequiredFields', null);
-      }
-      if (privacySetting === "E") {
-        this.confirmCreatePublic();
       } else {
         this.createProblem();
       }
@@ -272,33 +236,72 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
       this.sendAction('toProblemList');
     },
 
+    insertQuillContent: function (selector, options) {
+      $('#editor').ready(() => {
+        let options = {
+          debug: 'false',
+          modules: {
+            toolbar: [
+              ['bold', 'italic', 'underline'],
+              ['image'],
+            ]
+          },
+          placeholder: 'Problem Statement',
+          theme: 'snow'
+        };
+        let quill = new window.Quill(selector, options); // eslint-disable-line no-unused-vars
+        let statement = this.get('problemStatement');
+        this.$('.ql-editor').html(statement);
+      });
+    },
+
+    confirmCreatePublic: function () {
+      this.get('alert').showModal('question', 'Are you sure you want to create a public problem?', 'Creating a public problem means it will be accessible to all EnCoMPASS users. You will not be able to make any changes once this problem has been used', 'Yes')
+        .then((result) => {
+          if (result.value) {
+            this.set('showCats', true);
+            this.set('showGeneral', false);
+            this.set('showAdditional', false);
+            this.set('showLegal', false);
+          }
+        });
+    },
+
     showGeneral: function () {
       this.set('showGeneral', true);
       this.set('showCats', false);
       this.set('showAdditional', false);
       this.set('showLegal', false);
-      var options = {
-        debug: 'false',
-        modules: {
-          toolbar: [
-            ['bold', 'italic', 'underline'],
-            ['image'],
-          ]
-        },
-        placeholder: 'Problem Statement',
-        theme: 'snow'
-      };
-      $('#editor').ready(() => {
-        var quill = new window.Quill('#editor', options); // eslint-disable-line no-unused-vars
-      });
     },
 
     showCats: function () {
-      this.set('showCats', true);
-      this.set('showGeneral', false);
-      this.set('showAdditional', false);
-      this.set('showLegal', false);
+      console.log('clicked on showCats or next to showCats');
+      this.set('problemTitle', this.get('title'));
+      let quillContent = this.$('.ql-editor').html();
+      let problemStatement = quillContent.replace(/["]/g, "'");
+      this.set('problemStatement', problemStatement);
+      this.set('privacySetting', this.get('privacySetting'));
+      console.log('title is', this.get('problemTitle'));
+      console.log('statment is', this.get('problemStatement'));
+      console.log('privacy Setting is', this.get('privacySetting'));
+
+      let isQuillValid = this.isQuillValid();
+      if (!isQuillValid || !this.get('problemTitle') || !this.get('problemStatement')) {
+        console.log('missing required fields');
+        this.set('isMissingRequiredFields', true);
+        return;
+      }
+      if (this.get('privacySetting') === "E") {
+        this.send('confirmCreatePublic');
+      } else {
+        this.set('showCats', true);
+        this.set('showGeneral', false);
+        this.set('showAdditional', false);
+        this.set('showLegal', false);
+      }
     },
+
+
 
     showAdditional: function () {
       this.set('showAdditional', true);
@@ -312,6 +315,28 @@ Encompass.ProblemNewComponent = Ember.Component.extend(Encompass.CurrentUserMixi
       this.set('showCats', false);
       this.set('showAdditional', false);
       this.set('showGeneral', false);
+    },
+
+    nextStep: function () {
+      console.log('nextStep clicked');
+      if (this.get('showGeneral')) {
+        this.send('showCats');
+      } else if (this.get('showCats')) {
+        this.send('showAdditional');
+      } else if (this.get('showAdditional')) {
+        this.send('showLegal');
+      }
+    },
+
+    backStep: function () {
+      console.log('backStep clicked');
+      if (this.get('showCats')) {
+        this.send('showGeneral');
+      } else if (this.get('showAdditional')) {
+        this.send('showCats');
+      } else if (this.get('showLegal')) {
+        this.send('showAdditional');
+      }
     },
   }
 });
