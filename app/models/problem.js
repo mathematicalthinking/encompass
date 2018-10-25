47afc43b3c3aa363ff65c94693eba50b551d0a38
Daniel Kelly <djk8552@gmail.com>
2018-10-25 12:55:59 -0400

    Add isForEdit and isForAssignment props to problem model

--- app/models/problem.js
+++ app/models/problem.js
@@ -18,5 +18,7 @@ Encompass.Problem = DS.Model.extend(Encompass.Auditable, {
   error: DS.attr('string'),
   isUsed: DS.attr('boolean'),
   status: DS.attr('string'),
-  flagReason: DS.attr('string')
+  flagReason: DS.attr('string'),
+  isForEdit: DS.attr('boolean', { default: false }),
+  isForAssignment: DS.attr('boolean', {default: false})
 });
