70539e0f81ddde26cd43429ed445a445b791fce7
philipwisner <philipwisner@gmail.com>
2018-10-31 11:49:51 -0400

    added service and guider

--- app/controllers/workspace_submission_controller.js
+++ app/controllers/workspace_submission_controller.js
@@ -15,6 +15,8 @@ Encompass.WorkspaceSubmissionController = Ember.Controller.extend(Encompass.Curr
   currentSelection: Ember.computed.alias('workspace.currentSelection'),
   workspaceOwner: Ember.computed.alias('currentWorkspace.owner'),
   permissions: Ember.inject.service('workspace-permissions'),
+  guider: Ember.inject.service('guiders-create'),
+
 
   inWorkspace: function() {
     var controller = this;
@@ -63,6 +65,167 @@ Encompass.WorkspaceSubmissionController = Ember.Controller.extend(Encompass.Curr
   }.property('currentUser.username', 'currentWorkspace.owner.username'),
 
   actions: {
+    startTour: function () {
+      this.get('guider').createGuider(
+        'workspace',
+        'submissions',
+        'Welcome to your Workspace!',
+        "Your workspace is a place where you can browse, organize, and comment on submissions.  Let's get started!",null, null, null,
+        [{name: "Next"}],
+        true,
+        null
+      ).show();
+
+      this.get('guider').createGuider(
+        'submissions',
+        'submissions.nav',
+        'Submissions Area',
+        "Your submissions show up here",
+        "#al_center",
+        "#al_center",
+        12,
+        [{name: "Next"}],
+        true,
+        null
+      );
+
+      this.get('guider').createGuider(
+        'submissions.nav',
+        'submissions.text',
+        'Submission Navigation',
+        "You can navigate between submissions using the dropdown or the arrows",
+        "#submission-nav",
+        "#submission-nav",
+        9,
+        [{name: "Next"}],
+        true,
+        '200px',
+        null
+      );
+      window.guiders.createGuider({
+        buttons: [{
+          name: "Next"
+        }, {
+          name: "Close"
+        }],
+        description: "Here is the short and long answer of the submission",
+        id: "submissions.text",
+        next: "submissions.selection",
+        attachTo: "#al_submission>.submission-short",
+        overlay: true,
+        width: '200px',
+        highlight: "#al_submission>#submission_container",
+        position: 9,
+        title: "Submission Text"
+      });
+
+      window.guiders.createGuider({
+        buttons: [],
+        description: "Click the 'Make selection' button to begin making a selection. Then highlight the part of the submission you want to select. <strong>Go ahead and make a selection now.</strong>",
+        id: "submissions.selection",
+        next: "submissions.selections",
+        attachTo: "#al_center",
+        overlay: true,
+        width: '200px',
+        highlight: "#al_center",
+        position: 9,
+        title: "Make Selection"
+      });
+
+      window.guiders.createGuider({
+        buttons: [],
+        description: "Great! Now click on your selection and let's see what we can do with it.",
+        id: "submissions.selections",
+        next: "comments",
+        attachTo: "#submission_selections", //really wanted to attach to this but not working
+        overlay: true,
+        width: '200px',
+        highlight: "#al_center",
+        position: 9,
+        title: "You've made a selection!"
+      });
+
+      window.guiders.createGuider({
+        buttons: [{
+          name: "Next"
+        }, {
+          name: "Close"
+        }],
+        description: "Now that you have chosen a selection you can add comments.  Select one of the comment types: Notice, Wonder, Feedback and type a comment.",
+        id: "comments",
+        next: "comments.comment",
+        attachTo: "#al_right", //really wanted to attach to this but not working
+        overlay: true,
+        highlight: "#al_right",
+        position: 12,
+        title: "Commenting on a Selection"
+      });
+
+      window.guiders.createGuider({
+        buttons: [{
+          name: "Next"
+        }],
+        description: "The most relevant comments show up at the top and have a darker highlighting.  You can check the box next to the comment to indicate you want to include this comment in your reply.  You'll notice that if there are comments selected you will have another button next to 'Make Selection' called 'Draft Reply'.",
+        id: "comments.comment",
+        next: "folders",
+        attachTo: "#al_feedback_display",
+        overlay: true,
+        highlight: "#al_feedback_display",
+        position: 9,
+        title: "Your comments show up here."
+      });
+
+      window.guiders.createGuider({
+        buttons: [{
+          name: "Next"
+        }],
+        description: "You can organize your selections by filing them in folders.  Folders contain other folders and can be expanded by clicking on the folder icon.  You can reorganize folders by dragging them into sub-folders.  You can also add and delete folders using the buttons at the bottom.",
+        id: "folders",
+        next: "folders.counts",
+        attachTo: "#al_left",
+        overlay: true,
+        highlight: "#al_left",
+        position: 2,
+        title: "Organizing Selections"
+      });
+      window.guiders.createGuider({
+        buttons: [{
+          name: "Next"
+        }],
+        description: "The count shows how many distinct submissions (left) and selections (right) have been filed in this folder (including all of the sub-folders).",
+        id: "folders.counts",
+        next: "fileSelection",
+        attachTo: "#al_folders>li.folderItem:eq(2) aside",
+        overlay: true,
+        highlight: "#al_folders>li.folderItem:eq(2) aside",
+        position: 12,
+        title: "Folder Counts"
+      });
+
+       window.guiders.createGuider({
+        buttons: [{
+          name: "Next"
+        }],
+        description: "To file a selection, drag it to a folder.",
+        id: "fileSelection",
+        next: "done",
+        title: "File Selections in Folders"
+      });
+
+      window.guiders.createGuider({
+        buttons: [{
+          name: "Close",
+        }],
+        id: "done",
+        title: "Done!",
+        overlay: true,
+        highlight: "#takeTour",
+        position: 9,
+        attachTo: "#takeTour",
+        description: "You're all done.  If you want to re-take the tour, just hit this button."
+      });
+    },
+
     cancelComment: function(){
       console.log("Cancelling comment!");
       this.transitionToRoute('workspace.submission');
