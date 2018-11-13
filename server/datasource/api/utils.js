c7f978991791ed087ce98c3eb58a218ac796caa4
philipwisner <philipwisner@gmail.com>
2018-11-13 15:55:49 -0500

    added sort workspaces to utils

--- server/datasource/api/utils.js
+++ server/datasource/api/utils.js
@@ -237,6 +237,45 @@ function getSafeName(str, doRemoveExtraSpaces, doCapitalize) {
   return firstName;
 }
 
+const sortWorkspaces = function(model, sortParam, req, criteria) {
+  // Limit and skip are passed in with the req
+  let limit = req.query.limit;
+  let skip = req.skip;
+
+  // Determine which field should be sorted and what value
+  let sortField = Object.keys(sortParam)[0];
+  let value = parseInt(sortParam[sortField], 0);
+
+  let aggregateArray = [];
+
+  // Creating objects to add to the aggregate pipeline
+  let sortObj = { "$sort" : { "length": value } };
+  let limitObj = { "$limit": limit };
+  let skipObj = { "$skip": skip };
+
+  // Match Obj takes the passed in criteria, as well as checking sortable field exists
+  let matchObj = { "$match" : criteria };
+  let matchNest = matchObj.$match;
+  matchNest[sortField] = { $exists: true, $ne: null };
+
+  // Project object iterates of keys of schema and then adds a length field to selected sortField
+  let projectObj = { "$project" : { } };
+  let projNest = projectObj.$project;
+  let schema = require('mongoose').model(model).schema;
+  let schemaObj = schema.obj;
+  let ObjKeys = Object.keys(schemaObj);
+  ObjKeys.forEach((key) => {
+    projNest[key] = 1;
+  });
+  projNest.length = { "$size": '$' + sortField };
+
+  // All objects are pushed into the aggregate array
+  aggregateArray.push(matchObj, projectObj, sortObj, skipObj, limitObj);
+
+  // Return results of aggregate by provied model
+  return models[model].aggregate(aggregateArray).exec();
+};
+
 module.exports.filterByForeignRef = filterByForeignRef;
 module.exports.filterByForeignRefArray = filterByForeignRefArray;
 module.exports.findAndReturnIds = findAndReturnIds;
@@ -247,3 +286,4 @@ module.exports.capitalizeString = capitalizeString;
 module.exports.capitalizeWord = capitalizeWord;
 module.exports.getSafeName = getSafeName;
 module.exports.isNonEmptyString = isNonEmptyString;
+module.exports.sortWorkspaces = sortWorkspaces;
