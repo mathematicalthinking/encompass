82439ef0334a7b3ce59b5e8e6938496bf1545738
Daniel Kelly <djk8552@gmail.com>
2018-11-14 15:30:02 -0500

    Copy workspace tests

--- test/mocha/copyWorkspaceTest.js
+++ test/mocha/copyWorkspaceTest.js
@@ -78,28 +78,64 @@ describe(`Copy Workspace operations by account type`, async function() {
       let newWsName = "New Cloned Workspace";
       let newWsOwner = "5b9149552ecaf7c30dd4748e";
 
-      function isOriginalRecordUnaltered(model, originalId, originalJSON, callback) {
-        it('should return 403 error', done => {
-          const url = `/api/${model}s/${originalId}`;
-          agent
-          .get(url)
-          .end((err, res) => {
-            if (err) {
-              console.log(err);
-            }
-            expect(res).to.have.status(200);
-            expect(res).to.have.all.keys(model);
-            expect(res.body[model]).to.be.a('object');
+      const basicShallowRequest = {
+        originalWsId: baseWsId,
 
-            const fetchedRecord = res.body[model];
-            let isEqual = _.isEqual(fetchedRecord, originalJSON);
-            console.log('isEqual', isEqual);
-            done();
-          });
-          callback();
-      });
-      }
+        optionsHash: {
+          owner: newWsOwner,
+          name: newWsName,
+          answerOptions: {
+            all: true
+          },
+          folderOptions: {
+            includeStructureOnly: true,
+            folderSetOptions: {
+              doCreateFolderSet: false,
+            },
+            "all": true
+          },
+          selectionOptions: {
+            none: true
+          },
+          commentOptions: {
+            none: true,
 
+          },
+          responseOptions: {
+           none: true
+          }
+        }
+
+      };
+      const deepCloneRequest = {
+        originalWsId: baseWsId,
+
+        optionsHash: {
+          owner: newWsOwner,
+          name: newWsName,
+          answerOptions: {
+            all: true
+          },
+          folderOptions: {
+            includeStructureOnly: false,
+            folderSetOptions: {
+              doCreateFolderSet: false,
+            },
+            "all": true
+          },
+          selectionOptions: {
+            all: true
+          },
+          commentOptions: {
+            all: true,
+
+          },
+          responseOptions: {
+           all: true
+          }
+        }
+
+      };
       before(async function(){
         try {
           await helpers.setup(agent, username, password);
@@ -112,6 +148,79 @@ describe(`Copy Workspace operations by account type`, async function() {
         agent.close();
       });
         describe('Basic shallow request', function() {
+          const request = basicShallowRequest;
+            it('should post request successfully', done => {
+              agent
+              .post(baseUrl)
+              .send(request)
+              .end((err, res) => {
+                if (err) {
+                  console.log('err', err);
+                } else if (isStudent) {
+                  expect(res).to.have.status(403);
+                  done();
+                } else {
+                  expect(res).to.have.status(200);
+                  expect(res.body).to.have.all.keys('workspace');
+                  const workspace = res.body.workspace;
+
+                  const { owner, name, mode, submissions, folders, taggings, selections, comments, responses } = workspace;
+
+                  const tuples = [
+                    [submissions, originalWs.submissions.length ],
+                    [folders, originalWs.folders.length ],
+                    [taggings, 0], [selections, 0], [comments, 0], [responses, 0]
+                  ];
+                  checkLengths(tuples);
+                  expect(owner).to.eql(newWsOwner);
+                  expect(name).to.eql(newWsName);
+                  expect(mode).to.eql(originalWs.mode);
+
+                  done();
+                }
+              });
+            });
+          });
+
+        describe('Basic Deep Clone Workspace', function() {
+          const request = deepCloneRequest;
+            it('should post request successfully', done => {
+              agent
+              .post(baseUrl)
+              .send(request)
+              .end((err, res) => {
+                if (err) {
+                  console.log('err', err);
+                } else if (isStudent) {
+                  expect(res).to.have.status(403);
+                  done();
+                } else {
+                  expect(res).to.have.status(200);
+                  expect(res.body).to.have.all.keys('workspace');
+                  const workspace = res.body.workspace;
+
+                  const { owner, name, mode, submissions, folders, taggings, selections, comments, responses } = workspace;
+
+                  const tuples = [
+                    [submissions, originalWs.submissions.length ],
+                    [folders, originalWs.folders.length ],
+                    [taggings, originalWs.taggings.length], [selections, originalWs.selections.length], [comments, originalWs.comments.length], [responses, originalWs.responses.length]
+                  ];
+
+                  checkLengths(tuples);
+
+                  expect(owner).to.eql(newWsOwner);
+                  expect(name).to.eql(newWsName);
+                  expect(mode).to.eql(originalWs.mode);
+                  done();
+                }
+              });
+            });
+        });
+
+        describe('Shallow By AnswerId', function() {
+          // ashleyc
+          let requestedAnswerIds = ["5bec35898c73047613e2f34b"];
           const request = {
             originalWsId: baseWsId,
 
@@ -119,7 +228,7 @@ describe(`Copy Workspace operations by account type`, async function() {
               owner: newWsOwner,
               name: newWsName,
               answerOptions: {
-                all: true
+                answerIds: requestedAnswerIds
               },
               folderOptions: {
                 includeStructureOnly: true,
@@ -141,70 +250,74 @@ describe(`Copy Workspace operations by account type`, async function() {
             }
 
           };
-            it('should post request successfully', done => {
-              agent
-              .post(baseUrl)
-              .send(request)
-              .end((err, res) => {
-                if (err) {
-                  console.log('err', err);
-                } else if (isStudent) {
-                  expect(res).to.have.status(403);
-                  done();
-                } else {
-                  expect(res).to.have.status(200);
-                  expect(res.body).to.have.all.keys('workspace');
-                  const workspace = res.body.workspace;
-                  const { owner, name, mode, submissions, folders, taggings, selections, comments, responses } = workspace;
-                  const tuples = [
-                    [submissions, originalWs.submissions.length ],
-                    [folders, originalWs.folders.length ],
-                    [taggings, 0], [selections, 0], [comments, 0], [responses, 0]
-                  ];
-                  checkLengths(tuples);
-                  expect(owner).to.eql(newWsOwner);
-                  expect(name).to.eql(newWsName);
-                  done();
-                }
+          let submission = fixtures.byAnswerIdSubmission;
 
-              });
-            });
-
-
-
-        });
-
-        describe('Basic Deep Clone Workspace', function() {
-          const request = {
-            originalWsId: baseWsId,
-
-            optionsHash: {
-              owner: newWsOwner,
-              name: newWsName,
-              answerOptions: {
-                all: true
-              },
-              folderOptions: {
-                includeStructureOnly: false,
-                folderSetOptions: {
-                  doCreateFolderSet: false,
-                },
-                "all": true
-              },
-              selectionOptions: {
-                all: true
-              },
-              commentOptions: {
-                all: true,
-
-              },
-              responseOptions: {
-               none: true
+          it('should only copy records related to requestedAnswer', done => {
+            agent
+            .post(baseUrl)
+            .send(request)
+            .end((err, res) => {
+              if (err) {
+                console.log('err', err);
+              } else if (isStudent) {
+                expect(res).to.have.status(403);
+                done();
+              } else {
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.all.keys('workspace');
+                const workspace = res.body.workspace;
+                const { owner, name, mode, submissions, folders, taggings, selections, comments, responses } = workspace;
+                const tuples = [
+                  [submissions, requestedAnswerIds.length ],
+                  [folders, originalWs.folders.length ], [taggings, 0],
+                  [selections, 0], [comments, 0], [responses, 0]
+                ];
+                checkLengths(tuples);
+                expect(owner).to.eql(newWsOwner);
+                expect(name).to.eql(newWsName);
+                expect(mode).to.eql(originalWs.mode);
+                done();
               }
-            }
 
-          };
-            it('should post request successfully', done => {
+            });
+          });
+        });
+
+        describe('Deep Clone By AnswerId', function() {
+            // tracyc
+            let requestedAnswerIds = ["5bb813fc9885323f6d894972"];
+            const request = {
+              originalWsId: baseWsId,
+
+              optionsHash: {
+                owner: newWsOwner,
+                name: newWsName,
+                answerOptions: {
+                  answerIds: requestedAnswerIds
+                },
+                folderOptions: {
+                  includeStructureOnly: false,
+                  folderSetOptions: {
+                    doCreateFolderSet: false,
+                  },
+                  "all": true
+                },
+                selectionOptions: {
+                  all: true
+                },
+                commentOptions: {
+                  all: true,
+
+                },
+                responseOptions: {
+                 all: true
+                }
+              }
+
+            };
+            let submission = fixtures.byAnswerIdSubmission;
+
+            it('should only copy records related to requestedAnswer', done => {
               agent
               .post(baseUrl)
               .send(request)
@@ -218,26 +331,23 @@ describe(`Copy Workspace operations by account type`, async function() {
                   expect(res).to.have.status(200);
                   expect(res.body).to.have.all.keys('workspace');
                   const workspace = res.body.workspace;
-                  console.log('res ws', workspace);
                   const { owner, name, mode, submissions, folders, taggings, selections, comments, responses } = workspace;
                   const tuples = [
-                    [submissions, originalWs.submissions.length ],
-                    [folders, originalWs.folders.length ],
-                    [taggings, originalWs.taggings.length], [selections, originalWs.selections.length], [comments, originalWs.comments.length], [responses, 0]
+                    [submissions, requestedAnswerIds.length ],
+                    [folders, originalWs.folders.length ], [taggings, 2],
+                    [selections, 2], [comments, 2], [responses, 1]
                   ];
                   checkLengths(tuples);
                   expect(owner).to.eql(newWsOwner);
                   expect(name).to.eql(newWsName);
+                  expect(mode).to.eql(originalWs.mode);
                   done();
                 }
 
               });
             });
+          });
         });
-
-
-
-    });
   }
   for (let user of Object.keys(testUsers)) {
     let testUser = testUsers[user];
