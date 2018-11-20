7d1cbc1e2fedfdd0fbf784644d36d3e12f2e9070
Daniel Kelly <djk8552@gmail.com>
2018-11-20 16:34:54 -0500

    Handle permissions for ws-copy request

--- app/components/ws-copy-permissions.js
+++ app/components/ws-copy-permissions.js
@@ -1,9 +1,32 @@
 Encompass.WsCopyPermissionsComponent = Ember.Component.extend({
   elementId: 'ws-copy-permissions',
+  utils: Ember.inject.service('utility-methods'),
+  permissions: [],
 
   actions: {
-    submit() {
-      this.get('createCopyRequest')();
+    setCollaborator(val, $item) {
+      if (!val) {
+        return;
+      }
+      const user = this.get('store').peekRecord('user', val);
+      this.set('selectedCollaborator', user);
+    },
+    savePermissions(permissionsObject) {
+      if (!this.get('utils').isNonEmptyObject) {
+        return;
+      }
+
+      this.get('permissions').addObject(permissionsObject);
+
+      // clear selectedCollaborator
+      // clear selectize input
+
+      this.set('selectedCollaborator', null);
+      this.$('select#collab-select')[0].selectize.clear();
+
+    },
+    next() {
+      this.get('onProceed')(this.get('permissions'));
     }
   }
 });
---
+++ app/components/ws-permissions-new.js
@@ -0,0 +1,128 @@
+Encompass.WsPermissionsNewComponent = Ember.Component.extend({
+  elementId: 'ws-permissions-new',
+  utils: Ember.inject.service('utility-methods'),
+
+  showCustom: Ember.computed.equal('global', 'custom'),
+
+  global: 'viewOnly',
+  answerItems: {
+    groupName: 'answers',
+    groupLabel: 'Answer Permissions',
+    required: true,
+    inputs: [
+      { label: 'All', value: 'all' },
+      { label: 'Own Only', value: 'mine' }
+    ]
+  },
+
+  folderItems: {
+    groupName: 'folders',
+    groupLabel: 'Folder Permissions',
+    required: true,
+    inputs: [
+      { label: 'None', value: 0 },
+      { label: 'View Only', value: 1 },
+      { label: 'Create', value: 2 },
+      { label: 'Edit', value: 3 },
+      { label: 'Delete', value: 4 }
+    ]
+  },
+  selectionItems: {
+    groupName: 'selections',
+    groupLabel: 'Selection Permissions',
+    required: true,
+    inputs: [
+      { label: 'None', value: 0 },
+      { label: 'View Only', value: 1 },
+      { label: 'Create', value: 2 },
+      { label: 'Edit', value: 3 },
+      { label: 'Delete', value: 4 }
+    ]
+  },
+  commentItems: {
+    groupName: 'comments',
+    groupLabel: 'Comment Permissions',
+    required: true,
+    inputs: [
+      { label: 'None', value: 0 },
+      { label: 'View Only', value: 1 },
+      { label: 'Create', value: 2 },
+      { label: 'Edit', value: 3 },
+      { label: 'Delete', value: 4 }
+    ]
+  },
+
+  feedbackItems: {
+    groupName: 'feedback',
+    groupLabel: 'Feedback Permissions',
+    required: true,
+    inputs: [
+      { label: 'None', value: 'none' },
+      { label: 'Authorization Required', value: 'authReq' },
+      { label: 'PreAuthorized', value: 'preAuth' }
+    ]
+  },
+  globalItems: {
+    groupName: 'global',
+    groupLabel: 'Global Permissions',
+    required: true,
+    inputs: [
+      { label: 'View Only', value: 'viewOnly' },
+      { label: 'Editor', value: 'editor' },
+      { label: 'Custom', value: 'custom' }
+    ]
+  },
+
+  folders: 1,
+  answers: 'all',
+  comments: 1,
+  selections: 1,
+  feedback: 'authReq',
+
+  buildPermissionsObject() {
+    const user = this.get('selectedUser');
+    const global = this.get('global');
+    const answers = this.get('answers');
+
+    const results = {
+      user,
+      answers,
+      global
+    };
+
+    if (global === 'viewOnly') {
+      results.folders = 1;
+      results.selections = 1;
+      results.comments = 1;
+      results.feedback = 'none';
+
+      return results;
+    }
+    if (global === 'editor') {
+      results.folders = 4;
+      results.selections = 4;
+      results.comments = 4;
+      results.feedback = 'preAuth';
+
+      return results;
+    }
+    results.folders = this.get('folders');
+    results.selections = this.get('selections');
+    results.comments = this.get('comments');
+    results.feedback = this.get('feedback');
+
+    return results;
+  },
+
+  actions: {
+    savePermissions() {
+      const permissions = this.buildPermissionsObject();
+      if (this.get('utils').isNonEmptyObject(permissions)) {
+        this.get('onSave')(permissions);
+        return;
+      }
+      this.set('saveError', true);
+    }
+  }
+
+});
