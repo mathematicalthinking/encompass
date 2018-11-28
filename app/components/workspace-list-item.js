2661021ca233a57ab012610dd74092750af9e83e
philipwisner <philipwisner@gmail.com>
2018-11-28 15:47:03 -0500

    added ability to delete workspace from more menu

--- app/components/workspace-list-item.js
+++ app/components/workspace-list-item.js
@@ -2,21 +2,10 @@
 Encompass.WorkspaceListItemComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   classNames: ['workspace-list-item'],
   alert: Ember.inject.service('sweet-alert'),
-
-  permissions: Ember.inject.service('problem-permissions'),
-  canEdit: Ember.computed.alias('writePermissions.canEdit'),
-  canDelete: Ember.computed.alias('writePermissions.canDelete'),
-  canAssign: Ember.computed.alias('writePermissions.canAssign'),
+  permissions: Ember.inject.service('workspace-permissions'),
   menuOptions: Ember.computed.alias('parentView.moreMenuOptions'),
   parentData: Ember.computed.alias('parentView.containerData'),
   parentActions: Ember.computed.alias("parentView.containerActions"),
-
-
-
-  didReceiveAttrs: function () {
-    let problem = this.get('problem');
-    this.set('writePermissions', this.get('permissions').writePermissions(problem));
-  },
 
   isPublic: function () {
     return this.get('privacySetting') === 'E';
@@ -32,23 +21,28 @@ Encompass.WorkspaceListItemComponent = Ember.Component.extend(Encompass.CurrentU
 
 
   ellipsisMenuOptions: function () {
-    let canDelete = this.get('canDelete');
-    let canAssign = this.get('canAssign');
+    let ws = this.get('workspace');
+    let canDelete = this.get('permissions').canDelete(ws);
+    console.log('canDelete is', canDelete);
+    // let canAssign = this.get('canAssign');
+    // let canCopy = this.get('canAssign');
+    // let canHide = this.get('canAssign');
+    // let deleted = this.get('workspace.isTrashed');
     let moreMenuOptions = this.get('menuOptions');
-    let isAdmin = this.get('currentUser.isAdmin');
+    // let isAdmin = this.get('currentUser.isAdmin');
     let options = moreMenuOptions.slice();
-    let deleted = this.get('workspace.isTrashed');
+    console.log('options are', options);
 
-    if (!canDelete) {
-      // dont show delete or edit option
-      options = _.filter(moreMenuOptions, (option) => {
-        return option.value !== 'edit' && option.value !== 'delete';
-      });
-    }
+    // if (!canDelete) {
+    //   // dont show delete or edit option
+    //   options = _.filter(moreMenuOptions, (option) => {
+    //     return option.value !== 'edit' && option.value !== 'delete';
+    //   });
+    // }
 
-    if (isAdmin) {
-      // if problem is approved, admins will have exposed Flag button so no need in more menu
-    }
+    // if (isAdmin) {
+    //   if problem is approved, admins will have exposed Flag button so no need in more menu
+    // }
 
     // if (!isAdmin) {
     //   // remove any admin only options for non admins
@@ -75,11 +69,11 @@ Encompass.WorkspaceListItemComponent = Ember.Component.extend(Encompass.CurrentU
     //   });
     // }
 
-    if (deleted) {
-      options = _.filter(options, (option) => {
-        return !_.contains(['delete'], option.value);
-      });
-    }
+    // if (deleted) {
+    //   options = _.filter(options, (option) => {
+    //     return !_.contains(['delete'], option.value);
+    //   });
+    // }
 
     return options;
   }.property('workspace.id', 'workspace.isTrashed'),
@@ -106,29 +100,29 @@ Encompass.WorkspaceListItemComponent = Ember.Component.extend(Encompass.CurrentU
         });
     },
 
-    deleteProblem: function () {
-      let problem = this.get('problem');
-      this.get('alert').showModal('warning', 'Are you sure you want to delete this problem?', null, 'Yes, delete it')
+    deleteWorkspace: function () {
+      console.log('deleteWorkspace Called');
+      let workspace = this.get('workspace');
+      this.get('alert').showModal('warning', 'Are you sure you want to delete this workspace?', null, 'Yes, delete it')
         .then((result) => {
           if (result.value) {
-            problem.set('isTrashed', true);
-            // this.sendAction('toProblemList');
-            problem.save().then((problem) => {
+            workspace.set('isTrashed', true);
+            workspace.save().then((workspace) => {
               if (this.get('showMoreMenu')) {
                 this.set('showMoreMenu', false);
               }
-              this.get('alert').showToast('success', 'Problem Deleted', 'bottom-end', 5000, true, 'Undo')
+              this.get('alert').showToast('success', 'Workspace Deleted', 'bottom-end', 5000, true, 'Undo')
                 .then((result) => {
                   if (result.value) {
-                    problem.set('isTrashed', false);
-                    problem.save().then(() => {
-                      this.get('alert').showToast('success', 'Problem Restored', 'bottom-end', 3000, false, null);
+                    workspace.set('isTrashed', false);
+                    workspace.save().then(() => {
+                      this.get('alert').showToast('success', 'Workspace Restored', 'bottom-end', 3000, false, null);
                       // window.history.back();
                     });
                   }
                 });
             }).catch((err) => {
-              this.handleErrors(err, 'updateProblemErrors', problem);
+              console.log('error', err);
             });
           }
         });
