910027908fa6ec449bef1b5f219924a27651ddfb
philipwisner <philipwisner@gmail.com>
2018-11-29 11:33:29 -0500

    passed workspace Id to workspace copy route and set on model

--- app/routes/workspaces_copy_route.js
+++ app/routes/workspaces_copy_route.js
@@ -1,9 +1,21 @@
 Encompass.WorkspacesCopyRoute = Encompass.AuthenticatedRoute.extend({
+  beforeModel: function (transition) {
+    this._super.apply(this, arguments);
+
+    let workspace = transition.queryParams.workspace;
+    this.set('workspaceId', workspace);
+    console.log('workspaceId', this.get('workspaceId'));
+
+    //set on the route then return it on the model
+
+  },
 
   model: function() {
     const store = this.get('store');
+    this.set('workspaceToCopy', this.get('workspaceId'));
     return Ember.RSVP.hash({
-      folderSets: store.findAll('folderSet')
+      folderSets: store.findAll('folderSet'),
+      workspacetoCopy: this.get('workspaceId'),
     });
 
   },
