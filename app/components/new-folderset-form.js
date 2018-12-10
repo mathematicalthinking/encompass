a44924a25ac3b00aef0b8521a89e7ac95a79cc24
philipwisner <philipwisner@gmail.com>
2018-12-10 10:49:28 -0500

    added tooltips

--- app/components/new-folderset-form.js
+++ app/components/new-folderset-form.js
@@ -17,17 +17,22 @@ Encompass.NewFoldersetFormComponent = Ember.Component.extend({
       {
         value: 'M',
         label: 'Private',
-        isChecked: true
+        isChecked: true,
+        moreInfo: 'Only you will be able to see and reuse this folder set',
+
       },
       {
         value: 'O',
         label: 'My Org',
-        isChecked: false
+        isChecked: false,
+        moreInfo: 'All members of your org will be able to see and reuse this folder set',
+
       },
       {
         value: 'E',
         label: 'Public',
-        isChecked: false
+        isChecked: false,
+        moreInfo: 'All Encompass users will be able to see and reuse this folder set',
       },
     ]
   },
