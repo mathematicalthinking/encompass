a80d5a61a4ac72fb92550ea9e3bb89d879eb17fe
Daniel Kelly <djk8552@gmail.com>
2018-12-10 18:34:23 -0500

    Remove pagination from /answers for now

--- server/server.js
+++ server/server.js
@@ -193,7 +193,7 @@ server.get('/api/taggings', api.get.taggings);
 server.get('/api/taggings/:id', path.validateId(), api.get.tagging);
 server.get('/api/problems', paginate.middleware(20,100), api.get.problems);
 server.get('/api/problems/:id', path.validateId(), api.get.problem);
-server.get('/api/answers', paginate.middleware(25,200), api.get.answers);
+server.get('/api/answers', api.get.answers);
 server.get('/api/answers/:id', path.validateId(), api.get.answer);
 server.get('/api/sections', api.get.sections);
 server.get('/api/sections/:id', path.validateId(), api.get.section);
