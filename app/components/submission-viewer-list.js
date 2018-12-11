f4bd181dfbd48d66d9b52d47059c1c4ff538a9c5
philipwisner <philipwisner@gmail.com>
2018-12-11 11:21:04 -0500

    added info bar to submission viewer

--- app/components/submission-viewer-list.js
+++ app/components/submission-viewer-list.js
@@ -1,9 +1,20 @@
 Encompass.SubmissionViewerListComponent = Ember.Component.extend({
   elementId: 'submission-viewer-list',
+  isChecked: false,
 
   actions: {
     onSelect: function(submissionId) {
       this.get('onSelect')(submissionId);
+    },
+    toggleSelect: function() {
+      console.log('clicked toggle Select');
+      this.set('isChecked', !this.get('isChecked'));
+      console.log('is Checked is', this.get('isChecked'));
+      if (this.get('isChecked')) {
+        this.send('selectAll');
+      } else {
+        this.send('unselectAll');
+      }
     },
     selectAll: function() {
       this.get('onSelectAll')();
