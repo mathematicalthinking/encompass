7045da4afdb72864db537af67cc0a2848812d29d
Daniel Kelly <djk8552@gmail.com>
2018-12-11 20:03:24 -0500

    Cleanup

--- app/components/submissions-filter.js
+++ app/components/submissions-filter.js
@@ -599,38 +599,6 @@ Encompass.SubmissionsFilterComponent = Ember.Component.extend(Encompass.CurrentU
       });
       this.get('onSearch')(criteria);
 
-      // const encWorkspaceRequest = this.store.createRecord('encWorkspaceRequest', criteria);
-      // this.set('isRequestInProgress', true);
-      // encWorkspaceRequest.save().then((res) => {
-      //   this.set('isRequestInProgress', false);
-      //   if (res.get('isEmptyAnswerSet')) {
-      //     this.set('isEmptyAnswerSet', true);
-      //     $('.error-box').show();
-      //     return;
-      //   }
-      //   if (res.get('createWorkspaceError')) {
-      //     this.set('createWorkspaceError', res.get('createWorkspaceError'));
-      //     return;
-      //   }
-      //   this.get('alert').showToast('success', 'Workspace Created', 'bottom-end', 3000, false, null);
-      //   //Get the created workspaceId from the res
-      //   let workspaceId = res.get('createdWorkspace').get('id');
-      //   //Then find the first SubmissionID, this is sent to route in order to redirect
-      //   this.store.findRecord('workspace', workspaceId).then((workspace) => {
-      //     let submission = workspace.get('submissions').get('firstObject');
-      //     let submissionId = submission.get('id');
-      //     this.sendAction('toWorkspaces', workspaceId, submissionId);
-      //   }).catch((err) => {
-      //     this.handleErrors(err, 'findRecordErrors');
-      //   });
-
-      // })
-      // .catch((err) => {
-      //   this.set('isRequestInProgress', false);
-
-      //   this.handleErrors(err, 'wsRequestErrors', encWorkspaceRequest);
-      //   return;
-      // });
     },
     closeError: function (error) {
       $('.error-box').addClass('fadeOutRight');
