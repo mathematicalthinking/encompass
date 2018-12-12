057b80216464d54d7157987bb8e4f52248664edf
Daniel Kelly <djk8552@gmail.com>
2018-12-12 10:26:12 -0500

    Readd submission-viewer-list-item component
    Fix custom-submission-viewer-list-item component

--- app/components/custom-submission-viewer-list-item.js
+++ app/components/custom-submission-viewer-list-item.js
@@ -2,25 +2,12 @@ Encompass.CustomSubmissionViewerListItemComponent = Ember.Component.extend({
   elementId: ['custom-submission-viewer-list-item'],
 
   isChecked: function() {
-   let id = this.get('answer.id');
-   let prop = `selectedMap.${id}`;
-   return this.get(prop);
-  }.property('answer.id', 'selectedMap'),
-  revisionCount: function() {
-    let student = this.get('student');
-    let threads = this.get('threads');
-    if (threads) {
-      let work = threads.get(student);
-      if (work) {
-        return work.length;
-      }
-    }
-    return  0;
-  }.property('threads', 'student'),
+    return this.get('selectedSubmissionIds').includes(this.get('submission.id'));
+  }.property('selectedSubmissionIds.[]'),
 
   actions: {
     onSelect: function() {
-      this.get('onSelect')(this.get('answer'), this.get('isChecked'));
+      this.get('onSelect')(this.get('submission.id'));
     }
   }
 
---
+++ app/components/submission-viewer-list-item.js
@@ -0,0 +1,27 @@
+Encompass.SubmissionViewerListItemComponent = Ember.Component.extend({
+  elementId: ['submission-viewer-list-item'],
+
+  isChecked: function() {
+   let id = this.get('answer.id');
+   let prop = `selectedMap.${id}`;
+   return this.get(prop);
+  }.property('answer.id', 'selectedMap'),
+  revisionCount: function() {
+    let student = this.get('student');
+    let threads = this.get('threads');
+    if (threads) {
+      let work = threads.get(student);
+      if (work) {
+        return work.length;
+      }
+    }
+    return  0;
+  }.property('threads', 'student'),
+
+  actions: {
+    onSelect: function() {
+      this.get('onSelect')(this.get('answer'), this.get('isChecked'));
+    }
+  }
+
+});
