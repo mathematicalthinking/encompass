d63e92f9aa2ba224f79da97c34c0011d5b5222c0
philipwisner <philipwisner@gmail.com>
2018-12-19 16:43:04 -0500

    added back

--- app/components/import-work-step4.js
+++ app/components/import-work-step4.js
@@ -1,7 +1,19 @@
+/*global _:false */
 Encompass.ImportWorkStep4Component = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'import-work-step4',
 
   actions: {
-    selectChange: function () {}
+    next() {
+      const uploadedFiles = this.get('uploadedFiles');
+      if (uploadedFiles.length >= 1) {
+        this.get('onProceed')();
+        return;
+      }
+      this.set('missingFiles', true);
+
+    },
+    back() {
+      this.get('onBack')(-1);
+    }
   }
 });
--- app/components/import-work-step5.js
+++ app/components/import-work-step5.js
@@ -1,7 +1,19 @@
+/*global _:false */
 Encompass.ImportWorkStep5Component = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'import-work-step5',
 
   actions: {
-    selectChange: function () {}
+    next() {
+      const uploadedFiles = this.get('uploadedFiles');
+      if (uploadedFiles.length >= 1) {
+        this.get('onProceed')();
+        return;
+      }
+      this.set('missingFiles', true);
+
+    },
+    back() {
+      this.get('onBack')(-1);
+    }
   }
 });
