d2194264e024900f47159ff653881ca3c3f97a0b
philipwisner <philipwisner@gmail.com>
2018-12-19 16:23:48 -0500

    added step 3

--- app/components/import-work-step3.js
+++ app/components/import-work-step3.js
@@ -1,7 +1,42 @@
+/*global _:false */
 Encompass.ImportWorkStep3Component = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'import-work-step3',
+  utils: Ember.inject.service('utility-methods'),
 
   actions: {
-    selectChange: function () {}
+    setSelectedSection(val, $item) {
+      if (!val) {
+        return;
+      }
+
+      const isRemoval = _.isNull($item);
+      if (isRemoval) {
+        this.set('selectedSection', null);
+        return;
+      }
+
+      const section = this.get('store').peekRecord('section', val);
+      if (this.get('utils').isNullOrUndefined(section)) {
+        return;
+      }
+
+      this.set('selectedSection', section);
+      if (this.get('missingFiles')) {
+        this.set('missingFiles', null);
+      }
+    },
+    next() {
+      const section = this.get('selectedSection');
+      // workspace is required to go to next step
+      if (this.get('utils').isNonEmptyObject(section)) {
+        this.get('onProceed')();
+        return;
+      }
+      this.set('missingFiles', true);
+
+    },
+    back() {
+      this.get('onBack')(-1);
+    }
   }
 });
