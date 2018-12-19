592a1f2eac98a0b2e22e989fc4210177a18a9500
philipwisner <philipwisner@gmail.com>
2018-12-19 14:34:11 -0500

    added step 1

--- app/components/import-work-step1.js
+++ app/components/import-work-step1.js
@@ -1,7 +1,41 @@
+/*global _:false */
 Encompass.ImportWorkStep1Component = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'import-work-step1',
+  utils: Ember.inject.service('utility-methods'),
 
   actions: {
-    selectChange: function () {}
+    setSelectedProblem(val, $item) {
+      if (!val) {
+        return;
+      }
+
+      const isRemoval = _.isNull($item);
+      if (isRemoval) {
+        this.set('selectedProblem', null);
+        return;
+      }
+
+      const problem = this.get('store').peekRecord('problem', val);
+      if (this.get('utils').isNullOrUndefined(problem)) {
+        return;
+      }
+
+      this.set('selectedProblem', problem);
+      if (this.get('missingProblem')) {
+        this.set('missingProblem', null);
+      }
+    },
+    next() {
+      console.log('next function importStep 1 called');
+      const problem = this.get('selectedProblem');
+
+      // workspace is required to go to next step
+      if (this.get('utils').isNonEmptyObject(problem)) {
+        this.get('onProceed')();
+        return;
+      }
+      this.set('missingProblem', true);
+
+    }
   }
 });
