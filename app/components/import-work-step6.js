2458cd51782f1b0509bb9862e8b63894749bfa5c
philipwisner <philipwisner@gmail.com>
2019-01-08 16:51:08 -0500

    added conditional button display

--- app/components/import-work-step6.js
+++ app/components/import-work-step6.js
@@ -2,6 +2,16 @@
 Encompass.ImportWorkStep6Component = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'import-work-step6',
 
+  shouldHideButtons: function () {
+    if (this.get('isUploadingAnswer') || this.get('isCreatingWorkspace') || this.get('savingAssignment') || this.get('uploadedAnswers')) {
+      console.log('one of 4 is true');
+      return true;
+    } else {
+      console.log('none of the values are true');
+      return false;
+    }
+  }.property('isUploadingAnswer', 'isCreatingWorkspace', 'savingAssignment', 'uploadedAnswers'),
+
   actions: {
     next() {
       this.get('onProceed')();
