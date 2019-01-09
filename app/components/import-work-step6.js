1ff2b50ab65ded0f99abd7b97d2051eab1b718c6
philipwisner <philipwisner@gmail.com>
2019-01-09 10:46:31 -0500

    delay loading message and created workspace link

--- app/components/import-work-step6.js
+++ app/components/import-work-step6.js
@@ -10,6 +10,26 @@ Encompass.ImportWorkStep6Component = Ember.Component.extend(Encompass.CurrentUse
     }
   }.property('isUploadingAnswer', 'isCreatingWorkspace', 'savingAssignment', 'uploadedAnswers'),
 
+  workspaceLink: function () {
+    let createdWorkspace = this.get('createdWorkspace');
+    return `/#/workspaces/${createdWorkspace._id}/submissions/${createdWorkspace.submissions[0]}`;
+  }.property('isCreatingWorkspace', 'createdWorkspace'),
+
+  handleLoadingMessage: function() {
+    const that = this;
+    if (!this.get('isUploadingAnswer') || !this.get('isCreatingWorkspace') || this.get('savingAssignment')) {
+      this.set('showLoadingMessage', false);
+      return;
+    }
+    Ember.run.later(function() {
+      if (that.isDestroyed || that.isDestroying) {
+        return;
+      }
+      that.set('showLoadingMessage', true);
+    }, 800);
+
+  }.observes('isUploadingAnswer', 'isCreatingWorkspace', 'savingAssignment'),
+
   actions: {
     next() {
       this.get('onProceed')();
