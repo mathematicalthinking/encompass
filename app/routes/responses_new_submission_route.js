fbe1190ade0c4037f65c42a22e5c8181010ad5d1
Daniel Kelly <djk8552@gmail.com>
2019-01-11 10:49:06 -0500

    Refactor model hook

--- app/routes/responses_new_submission_route.js
+++ app/routes/responses_new_submission_route.js
@@ -1,44 +1,45 @@
 Encompass.ResponsesNewSubmissionRoute = Ember.Route.extend(Encompass.ConfirmLeavingRoute, {
 
-  controllerName: 'response',
-
   renderTemplate: function(controller, model) {
-    controller.set('model', model);
-    controller.set('editing', false);
+    // controller.set('model', model);
+    // controller.set('editing', false);
     this.render('responses/response');
 
   },
 
-  model: function(params) {
-    var route      = this;
-    var submission = this.store.find('submission', params.submission_id);
+  beforeModel(transition) {
+    let workspaceId = transition.queryParams.workspaceId;
 
-    var promise = new Ember.RSVP.Promise(function(resolve, reject) {
-      submission.then(function(sub){
-        var workspaces = sub.get('workspaces');
-        var selections = sub.get('selections');
-        var comments   = sub.get('comments');
-        workspaces.then(function(w){
-          selections.then(function(s){
-            comments.then(function(c){
-              var response = route.get('store').createRecord('response', {
-                source: 'submission',
-                submission: sub,
-                workspace: w.get('firstObject') //hmmm
-              });
-              response.get('selections').then(function(selections) {
-                selections.pushObjects(s.filterBy('isTrashed', false));
-                response.get('comments').then(function(comments) {
-                  comments.pushObjects(c.filterBy('isTrashed', false));
-                  resolve(response);
-                });
-              });
-            });
-          });
-        });
+    if (workspaceId) {
+      let workspace = this.get('store').peekRecord('workspace', workspaceId);
+      if (workspace) {
+        this.set('workspace', workspace);
+      }
+    }
+
+  },
+
+  model: function(params){
+    return this.get('store').findRecord('submission', params.submission_id)
+    .then((submission) => {
+      return Ember.RSVP.hash({
+        submission,
+        workspace: this.get('workspace'),
+        selections: submission.get('selections'),
+        comments: submission.get('comments')
       });
+    })
+    .then((hash) => {
+      let response = this.get('store').createRecord('response', {
+        submission: hash.submission,
+        workspace: hash.workspace,
+        source: 'submission'
+      });
+      response.get('selections').addObjects(hash.selections);
+      response.get('comments').addObjects(hash.comments);
+      return response;
     });
-    return promise;
-  }
+
+  },
 
 });
