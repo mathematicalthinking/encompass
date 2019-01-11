e37fd28c41630ec8187ddf0b2813f43c2f047aea
Daniel Kelly <djk8552@gmail.com>
2019-01-11 10:51:46 -0500

    Add model hook to response route

--- app/routes/response_route.js
+++ app/routes/response_route.js
@@ -1,5 +1,8 @@
 Encompass.ResponseRoute = Ember.Route.extend(Encompass.ConfirmLeavingRoute, {
-  controllerName: 'response',
+
+  model: function(params) {
+    return this.get('store').findRecord('response', params.response_id);
+  },
 
   renderTemplate: function () {
     this.render('responses/response');
