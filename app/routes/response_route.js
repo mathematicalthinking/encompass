ea7f643e7fdf5cab58fcbd929d6a1972217e7306
Daniel Kelly <djk8552@gmail.com>
2019-01-13 09:24:35 -0500

    Load necessary data in response routes for consistent model structure
    Currently can reach response page from either workspace or responses list

--- app/routes/response_route.js
+++ app/routes/response_route.js
@@ -1,7 +1,23 @@
 Encompass.ResponseRoute = Ember.Route.extend(Encompass.ConfirmLeavingRoute, {
 
   model: function(params) {
-    return this.get('store').findRecord('response', params.response_id);
+    return this.get('store').findRecord('response', params.response_id)
+      .then((response) => {
+        return Ember.RSVP.hash({
+          submission: response.get('submission'),
+          workspace: response.get('workspace'),
+          comments: response.get('comments'),
+          selections: response.get('selections'),
+          response,
+        })
+        .then((hash) => {
+          return {
+            response: hash.response,
+            submission: hash.submission,
+            workspace: hash.workspace
+          };
+        });
+      });
   },
 
   renderTemplate: function () {
--- app/routes/responses_new_submission_route.js
+++ app/routes/responses_new_submission_route.js
@@ -1,8 +1,7 @@
 Encompass.ResponsesNewSubmissionRoute = Ember.Route.extend(Encompass.ConfirmLeavingRoute, {
+  utils: Ember.inject.service('utility-methods'),
 
   renderTemplate: function(controller, model) {
-    // controller.set('model', model);
-    // controller.set('editing', false);
     this.render('responses/response');
 
   },
@@ -10,35 +9,75 @@ Encompass.ResponsesNewSubmissionRoute = Ember.Route.extend(Encompass.ConfirmLeav
   beforeModel(transition) {
     let workspaceId = transition.queryParams.workspaceId;
 
-    if (workspaceId) {
-      let workspace = this.get('store').peekRecord('workspace', workspaceId);
-      if (workspace) {
-        this.set('workspace', workspace);
-      }
+    if (this.get('utils').isValidMongoId(workspaceId)) {
+      this.set('workspace', this.get('store').peekRecord('workspace', workspaceId));
     }
+
+  },
+  resolveWorkspace(workspace, submission) {
+    if (workspace) {
+      return Ember.RSVP.resolve(workspace);
+    }
+    // in current structure do submissions ever have multiple workspaces?
+    return submission.get('workspaces')
+    .then((workspaces) => {
+      return workspaces.get('firstObject');
+    });
+  },
+  resolveRecipient(submission, workspace) {
+    // if creator of submission is enc user, they should always be in store
+    // since to get here you have to click respond from that user's submission
+    let encUserId = submission.get('creator.studentId');
+    if (this.get('utils').isValidMongoId(encUserId)) {
+      let peeked = this.get('store').peekRecord('user', encUserId);
+      console.log('peeked recipient', peeked);
+      return Ember.RSVP.resolve(peeked);
+    }
+    // if creator of submission is not enc user (i.e. old PoWs user)
+    // set recipient as either the first feedbackAuthorizer or the owner of workspace
+    let firstApproverId = workspace.get('feedbackAuthorizers.firstObject');
+
+    if (this.get('utils').isValidMongoId(firstApproverId)) {
+      return this.get('store').findRecord('user', firstApproverId);
+    }
+    return workspace.get('owner');
 
   },
 
   model: function(params){
+    let submission;
+
     return this.get('store').findRecord('submission', params.submission_id)
-    .then((submission) => {
-      return Ember.RSVP.hash({
-        submission,
-        workspace: this.get('workspace'),
-        selections: submission.get('selections'),
-        comments: submission.get('comments')
+      .then((sub) => {
+        submission = sub;
+        return this.resolveWorkspace(this.get('workspace'), submission);
+      })
+      .then((workspace) => {
+        return Ember.RSVP.hash({
+          submission,
+          workspace,
+          recipient: this.resolveRecipient(submission, workspace),
+          selections: submission.get('selections'),
+          comments: submission.get('comments')
+        });
+      })
+      .then((hash) => {
+        let response = this.get('store').createRecord('response', {
+          submission: hash.submission,
+          workspace: hash.workspace,
+          recipient: hash.recipient,
+          responseType: 'mentor',
+          source: 'submission'
+        });
+
+        response.get('selections').addObjects(hash.selections);
+        response.get('comments').addObjects(hash.comments);
+        return {
+          response,
+          submission: hash.submission,
+          workspace: hash.workspace
+        };
       });
-    })
-    .then((hash) => {
-      let response = this.get('store').createRecord('response', {
-        submission: hash.submission,
-        workspace: hash.workspace,
-        source: 'submission'
-      });
-      response.get('selections').addObjects(hash.selections);
-      response.get('comments').addObjects(hash.comments);
-      return response;
-    });
 
   },
 
