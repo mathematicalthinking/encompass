27efd645c474c8068b780c383dfba1e4936fa1a4
Daniel Kelly <djk8552@gmail.com>
2019-01-13 09:27:12 -0500

    Add isOwnSubmission to response-submission-view

--- app/components/response-submission-view.js
+++ app/components/response-submission-view.js
@@ -1,3 +1,11 @@
 Encompass.ResponseSubmissionViewComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'response-submission-view',
+
+  didReceiveAttrs() {
+   console.log('dra rsv');
+  },
+
+  isOwnSubmission: function() {
+    return this.get('submission.creator.studentId') === this.get('currentUser.id');
+  }.property('submission.creator.studentId'),
 });
