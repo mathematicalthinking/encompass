69b89492751a0f20d505dfaf38827d5ab275edc0
Daniel Kelly <djk8552@gmail.com>
2019-01-14 16:41:09 -0500

    Add openProblem action to view submission problem in other window

--- app/components/response-submission-view.js
+++ app/components/response-submission-view.js
@@ -8,4 +8,19 @@ Encompass.ResponseSubmissionViewComponent = Ember.Component.extend(Encompass.Cur
   isOwnSubmission: function() {
     return this.get('submission.creator.studentId') === this.get('currentUser.id');
   }.property('submission.creator.studentId'),
+
+  actions: {
+    openProblem() {
+      let problemId = this.get('submission.answer.problem.id');
+
+      if (!problemId) {
+        return;
+      }
+
+      var getUrl = window.location;
+      var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+
+      window.open(`${baseUrl}#/problems/${problemId}`, 'newwindow', 'width=1200, height=700');
+    },
+  }
 });
