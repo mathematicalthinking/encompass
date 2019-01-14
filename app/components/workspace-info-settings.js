cff2e79f7decc1bf52a11028e743ae68c91562b9
philipwisner <philipwisner@gmail.com>
2019-01-14 14:57:36 -0500

    added edit

--- app/components/workspace-info-settings.js
+++ app/components/workspace-info-settings.js
@@ -1,4 +1,9 @@
 Encompass.WorkspaceInfoSettingsComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: ['workspace-info-settings'],
 
+  actions: {
+    editWorkspaceInfo () {
+      this.set('isEditing', true);
+    },
+  }
 });
