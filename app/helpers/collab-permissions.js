1631ecbd384f2a4bc338b1edba39835963122559
philipwisner <philipwisner@gmail.com>
2019-01-15 10:13:18 -0500

    added helper to transform permissions

---
+++ app/helpers/collab-permissions.js
@@ -0,0 +1,34 @@
+Encompass.CollabPermissionsHelper = Ember.Helper.helper(function(val) {
+  let text;
+  val = val[0];
+  console.log('val is', val);
+  switch (val) {
+    case 0:
+      text = 'Hidden';
+      return text;
+    case 1:
+      text = 'View Only';
+      return text;
+    case 2:
+      text = 'Create';
+      return text;
+    case 3:
+      text = 'Add';
+      return text;
+    case 4:
+      text = 'Delete';
+      return text;
+    case 'preAuth':
+      text = 'Pre-Authorized';
+      return text;
+    case 'none':
+      text = 'None';
+      return text;
+    case 'authReq':
+      text = 'Authorization Required';
+      return text;
+    default:
+      text = "N/A";
+      return text;
+  }
+});
