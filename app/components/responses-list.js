3fc67667e1dd8fda1e89a6d9a3af63038c4304ce
Daniel Kelly <djk8552@gmail.com>
2019-01-16 17:15:30 -0500

    Refactor no responses message

--- app/components/responses-list.js
+++ app/components/responses-list.js
@@ -4,11 +4,16 @@ Encompass.ResponsesListComponent = Ember.Component.extend(Encompass.CurrentUserM
   sortAscending: false,
   showingAllResponses: true,
   showingOnlyMine: false,
-  isShowAll: true,
-  isShowMine: false,
+  isShowAll: Ember.computed.equal('currentFilter', 'all'),
+  isShowMine: Ember.computed.equal('currentFilter', 'toUser'),
+  isShowSent: Ember.computed.equal('currentFilter', 'sent'),
+  isShowPending: Ember.computed.equal('currentFilter', 'pendingApproval'),
+  isShowNeedsRevisions: Ember.computed.equal('currentFilter', 'needsRevisions'),
 
   currentFilter: 'toUser',
   sortParam: 'newest',
+  areDisplayResponses: Ember.computed.gt('displayResponses.length', 0),
+  areNoResponses: Ember.computed.not('areDisplayResponses'),
 
   statusMap: {
     'approved': 'APPROVED',
@@ -19,10 +24,29 @@ Encompass.ResponsesListComponent = Ember.Component.extend(Encompass.CurrentUserM
 
   didReceiveAttrs() {
     this.set('filteredResponses', this.get('toUserResponses'));
-    this.set('currentFilter', 'own');
+    this.set('currentFilter', 'toUser');
 
     this._super(...arguments);
   },
+
+  noResponsesMessage: function() {
+    let val = this.get('currentFilter');
+    if (val === 'sent') {
+      return `Looks like you have not created any responses. You can create one by clicking the "respond" button from a workspace.`;
+    }
+    if (val === 'all') {
+      return 'No responses found.';
+    }
+    if (val === 'toUser') {
+      return 'No responses addressed to you were found';
+    }
+    if (val === 'pendingApproval') {
+      return 'There are no responses needing approval at this time.';
+    }
+    if (val === 'needsRevisions') {
+      return 'There are no responses needing revisions at this time.';
+    }
+  }.property('currentFilter'),
 
   displayResponses: function() {
     return this.sortResponses(this.get('filteredResponses'), this.get('sortParam'));
@@ -73,6 +97,20 @@ Encompass.ResponsesListComponent = Ember.Component.extend(Encompass.CurrentUserM
     });
 
   },
+  filterByCreatedBy(creatorId, responses ) {
+    if (!creatorId) {
+      return responses;
+    }
+
+    if (!responses) {
+      return [];
+    }
+
+    return responses.filter((response) => {
+      let id = response.belongsTo('createdBy').id();
+      return creatorId === id;
+    });
+  },
 
   yourResponses: Ember.computed(function () {
     let currentUser = this.get('currentUser');
@@ -90,12 +128,27 @@ Encompass.ResponsesListComponent = Ember.Component.extend(Encompass.CurrentUserM
 
   actions: {
     showMyResponses: function () {
+      this.set('currentFilter', 'toUser');
       this.set('filteredResponses', this.get('toUserResponses'));
     },
 
     showAllResponses: function () {
+      this.set('currentFilter', 'all');
       this.set('filteredResponses', this.get('nonTrashedResponses'));
-      }
+    },
+      showSentResponses() {
+        this.set('currentFilter', 'sent');
+        this.set('filteredResponses', this.filterByCreatedBy(this.get('currentUser.id'), this.get('nonTrashedResponses')));
+      },
+      showPendingResponses() {
+        this.set('currentFilter', 'pendingApproval');
+        this.set('filteredResponses', this.filterByStatus('pendingApproval', this.get('nonTrashedResponses')));
+      },
+      showNeedsRevisionResponses() {
+        this.set('currentFilter', 'needsRevisions');
+        this.set('filteredResponses', this.filterByStatus('needsRevisions', this.get('nonTrashedResponses')));
+      },
+
     },
 
 });
