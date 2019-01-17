16da6c1d03e9962c35707fdefb610d88c6aa8127
philipwisner <philipwisner@gmail.com>
2019-01-17 14:17:45 -0500

    added toggle boxes

--- app/components/response-submission-view.js
+++ app/components/response-submission-view.js
@@ -1,9 +1,9 @@
 Encompass.ResponseSubmissionViewComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   elementId: 'response-submission-view',
-
-  didReceiveAttrs() {
-   console.log('dra rsv');
-  },
+  isShortExpanded: true,
+  isLongExpanded: true,
+  isImageExpanded: false,
+  isUploadExpanded: false,
 
   isOwnSubmission: function() {
     return this.get('submission.creator.studentId') === this.get('currentUser.id');
@@ -21,6 +21,9 @@ Encompass.ResponseSubmissionViewComponent = Ember.Component.extend(Encompass.Cur
       var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
       window.open(`${baseUrl}#/problems/${problemId}`, 'newwindow', 'width=1200, height=700');
+    },
+    toggleProperty: function (p) {
+      this.toggleProperty(p);
     },
   }
 });
