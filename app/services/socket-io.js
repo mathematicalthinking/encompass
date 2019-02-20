7dbff500395ff9e830c57f4d2f1c91f0845eb345
Daniel Kelly <djk8552@gmail.com>
2019-02-20 14:31:13 -0500

    Handle new approverReply and newWorkToMentor ntfs

--- app/services/socket-io.js
+++ app/services/socket-io.js
@@ -19,6 +19,7 @@ Encompass.SocketIoService = Ember.Service.extend(Encompass.CurrentUserMixin, {
       console.log('emitting ntf', data);
      _.each(data, (val, key) => {
       if (val) {
+        console.log('pushing payload', key, val);
         this.get('store').pushPayload(
           {
             [key]: val
@@ -26,12 +27,44 @@ Encompass.SocketIoService = Ember.Service.extend(Encompass.CurrentUserMixin, {
         );
       }
      });
-     let ntf = data.notifications[0];
 
-     // check if we need to clear any now outdated notifications
-     if (ntf) {
-      this.triggerToast(data.notifications[0]);
+
+     let ntf = data.notifications[0];
+     if (!ntf) {
+       return;
      }
+
+     let recordType = ntf.primaryRecordType;
+
+     if (recordType === 'response') {
+       if (ntf.notificationType === 'newWorkToMentor') {
+         // special case, associated submission, not response
+         if (data.submissions && data.submissions[0]) {
+           let subId = data.submissions[0]._id;
+
+           if (subId) {
+             let newRevision = this.get('store').peekRecord('submission', subId);
+
+             if (newRevision) {
+
+               let uniqueId = ntf.workspace + ntf.createdBy;
+               let existingThread = this.findExistingResponseThread('mentor', uniqueId);
+
+               // should always be existing thread
+               if (existingThread) {
+                 existingThread.get('submissions').addObject(newRevision);
+               }
+             }
+           }
+         }
+       } else if (data.responses && data.responses[0]) {
+        this.handleResponseNtf(ntf, data.responses[0], data.workspaceName);
+
+       }
+     }
+     // check if we need to clear any now outdated notifications
+
+      this.triggerToast(ntf);
     });
 
     socket.on('CLEAR_NOTIFICATION', (data) => {
@@ -67,7 +100,6 @@ Encompass.SocketIoService = Ember.Service.extend(Encompass.CurrentUserMixin, {
   },
 
   setupSocket: function (user) {
-    //TODO: dynamic url
     let windowHref = window.location.href;
     let hashIndex = windowHref.indexOf('#');
     let url = windowHref.slice(0, hashIndex);
@@ -96,5 +128,71 @@ Encompass.SocketIoService = Ember.Service.extend(Encompass.CurrentUserMixin, {
     }
     this.get('alert').showToast('info', toastText, 'top-end', 3000, false, null);
     return;
+  },
+
+  handleResponseNtf(ntf, newResponseObj, workspaceName) {
+
+    let { notificationType } = ntf;
+    let workspaceId = newResponseObj.workspace;
+    let newResponse = this.get('store').peekRecord('response', newResponseObj._id);
+    let submission = this.get('store').peekRecord('submission', newResponseObj.submission);
+
+    let problemTitle;
+    let studentIdentifier; // encUserId or pows username
+
+    if (submission) {
+      problemTitle = submission.get('publication.puzzle.title');
+
+      if (submission.get('creator.studentId')) {
+        studentIdentifier = submission.get('creator.studentId');
+      } else {
+        studentIdentifier = submission.get('creator.username');
+      }
+    }
+
+   if (notificationType === 'newMentorReply') {
+    let uniqueId = `srt${workspaceId}`;
+     let existingThread = this.findExistingResponseThread('submitter', uniqueId);
+     if (existingThread) {
+       existingThread.get('responses').addObject(newResponse);
+     } else {
+
+       // create new thread
+       let newThread = this.get('store').createRecord('response-thread', {
+         threadType: 'submitter',
+         uniqueIdentifier: workspaceId,
+         workspaceName,
+         mentors: [newResponse.get('createdBy')],
+         problemTitle
+       });
+       newThread.get('submissions').addObject(submission);
+       newThread.get('responses').addObject(newResponse);
+     }
+   }
+
+    if (notificationType === 'newApproverReply') {
+      // identifier is object with workspaceId and studentId
+
+      let uniqueId = workspaceId + studentIdentifier;
+
+      let existingThread = this.findExistingResponseThread('mentor', uniqueId);
+      if (existingThread) {
+        existingThread.get('responses').addObject(newResponse);
+      } else {
+        // should always be existing mentoring thread
+      }
+    }
+  },
+
+  findExistingResponseThread(threadType, uniqueIdentifier) {
+    let peekedResponseThreads = this.get('store').peekAll('response-thread').toArray();
+    if (!peekedResponseThreads) {
+      return;
+    }
+
+    return peekedResponseThreads.find((thread) => {
+      return thread.get('threadType') === threadType && _.isEqual(thread.get('id'), uniqueIdentifier);
+    });
+
   }
 });
