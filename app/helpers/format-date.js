6a554fd29c4c531f4dd7ed4c3e87359ce6749032
Daniel Kelly <djk8552@gmail.com>
2019-02-22 14:47:25 -0500

    Add option to use relative time with format-date helper

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,5 +1,9 @@
 Encompass.FormatDateHelper = Ember.Helper.helper(function (args) {
   // args is array of arguments passed in from template
-  let [date, format] = args;
+  let [date, format, doUseRelativeTime] = args;
+
+  if (doUseRelativeTime) {
+    return moment(date).fromNow();
+  }
   return moment(date).format(format);
 });
