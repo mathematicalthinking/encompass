e0b06995cabc95feac4aae1123cb242db14d2b38
Daniel Kelly <djk8552@gmail.com>
2019-02-26 16:31:41 -0500

    Update image model/schema with pdfPageNum field

--- app/models/image.js
+++ app/models/image.js
@@ -4,5 +4,19 @@ Encompass.Image = DS.Model.extend(Encompass.Auditable, {
   mimetype: DS.attr('string'),
   imageData: DS.attr('string'),
   sourceUrl: DS.attr('string'),
-  originalname: DS.attr('string')
+  originalname: DS.attr('string'),
+  pdfPageNum: DS.attr('number'),
+
+  pdfFileDisplay: function() {
+
+  }.property('pdfPageNum', ),
+
+  fileNameDisplay: function() {
+    let num = this.get('pdfPageNum');
+    if (typeof num === 'number') {
+      return `${this.get('originalname')} (pg. ${num})`;
+    }
+
+    return this.get('originalname');
+  }.property('originalname', 'pdfPageNum'),
 });
