62af9bcea5631653344ef59aacd8e4828c088912
Daniel Kelly <djk8552@gmail.com>
2019-03-14 09:09:51 -0400

    Cleanup

--- app/routes/workspace_route.js
+++ app/routes/workspace_route.js
@@ -6,22 +6,25 @@
   */
 Encompass.WorkspaceRoute = Ember.Route.extend({
   needs: 'application',
-  // using regular ajax request because sideloaded  data was not being pushed in to store soon enough
-  // when using ember data
+  /*
+  using regular ajax request because sideloaded  data was not being pushed in to store soon enough
+  when using ember data, which was causing unnecessary api requests being made
+  */
+
   model: function(params) {
     let url = `/api/workspaces/${params.workspace_id}`;
 
     return Ember.$.get({url})
-    .then((results) => {
-      _.each(results, (val, key) => {
-        if (val) {
-          this.get('store').pushPayload({
-            [key]: val
-          });
-        }
+      .then((results) => {
+        _.each(results, (val, key) => {
+          if (val) {
+            this.get('store').pushPayload({
+              [key]: val
+            });
+          }
+        });
+        return this.get('store').peekRecord('workspace', params.workspace_id);
       });
-      return this.get('store').peekRecord('workspace', params.workspace_id);
-    });
   },
 
   actions: {
