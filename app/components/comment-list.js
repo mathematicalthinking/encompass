9f1b06d6ab5353e2ef50a82de5aba0892e2cd8d9
Daniel Kelly <djk8552@gmail.com>
2019-04-23 17:37:33 -0400

    Update comment-list styling

--- app/components/comment-list.js
+++ app/components/comment-list.js
@@ -18,7 +18,7 @@ Encompass.CommentListComponent = Ember.Component.extend(Encompass.CurrentUserMix
   loading: Ember.inject.service('loading-display'),
 
   classNames: ['workspace-flex-item', 'comments'],
-  classNameBindings: ['canComment:can-comment', 'isHidden:hidden'],
+  classNameBindings: ['canComment:can-comment', 'isHidden:hidden', 'onSelection:on-selection'],
 
   permissions: Ember.inject.service('workspace-permissions'),
   myCommentsOnly: true,
