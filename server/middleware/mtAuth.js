f13903c68d94d09e62c36a9c4815cd4ff04ca986
Daniel Kelly <djk8552@gmail.com>
2019-06-05 18:35:52 -0400

    Use new user-auth middleware

---
+++ server/middleware/mtAuth.js
@@ -0,0 +1,78 @@
+const jwt = require('jsonwebtoken');
+
+
+const { User } = require('../datasource/schemas/');
+
+const { isValidMongoId} = require('../utils/mongoose');
+
+const secret = process.env.MT_USER_JWT_SECRET;
+
+const getMtUser = async (req) => {
+  try {
+    let mtToken = req.cookies.mtToken;
+
+    if (!mtToken) {
+      return null;
+    }
+
+    // if token is not verified, error will be thrown
+    let verifiedToken = await jwt.verify(mtToken, secret);
+
+    return verifiedToken;
+
+  }catch(err) {
+    console.error(`Error getMtUser: ${err}`);
+    return null;
+
+  }
+
+};
+
+const prepareMtUser = (req, res, next) => {
+  return getMtUser(req)
+  .then((user) => {
+    // user is null or verified payload from jwt token
+    // set on request for later user to retrieve encompass user record
+    req.mt.auth.user = user;
+    next();
+  })
+  .catch((err) => {
+    console.log('pmt error', err);
+  });
+};
+
+const prepareEncUser = (req, res, next) => {
+  let mtUserDetails = req.mt.auth.user;
+
+  if (mtUserDetails === null) {
+    req.mt.auth.encUser = null;
+    return next();
+  }
+
+  return User.findById(mtUserDetails.encUserId).exec()
+  .then((user) => {
+    user.lastSeen = new Date();
+    user.save();
+    req.mt.auth.encUser = user.toObject();
+    next();
+  });
+};
+
+const getEncUser = (mtUserDetails) => {
+  if (mtUserDetails === null) {
+    // token was not verified; no user authenticated
+    return null;
+  }
+  let { encUserId } = mtUserDetails;
+
+  if (isValidMongoId(encUserId)) {
+    return User.findById(encUserId).lean().exec();
+  }
+  return null;
+
+};
+
+module.exports.getMtUser = getMtUser;
+module.exports.prepareMtUser = prepareMtUser;
+module.exports.getEncUser = getEncUser;
+module.exports.prepareEncUser = prepareEncUser;
--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -4,11 +4,9 @@
  *   It mainly gets the user, protects api paths & loads workspaces
  * @author Daniel Kelly & Philip Wisner
  * @since 2.0.0
- * @todo Remove all the old authentication code
  */
 
 //REQUIRE MODULES
-const cookie = require('cookie');
 const logger = require('log4js').getLogger('auth');
 const _ = require('underscore');
 const randomColor = require('randomcolor');
@@ -20,102 +18,20 @@ const utils = require('./requestHandler');
 const models = require('../datasource/schemas');
 
 function getUser(req) {
-  return req.user;
+  // return req.user;
+  return req.mt.auth.encUser;
 }
 
 function requireUser(req) {
-  var user = getUser(req);
-  if (!user) {
+  let user = getUser(req);
+
+  if (user === null) {
     logger.error('user required but not found');
     throw new Error('user required but not found');
-  }
-  return req.user.toObject();
-}
-
-
-/*
-  Pull the token from the request
-  Stores the token in req.mf.auth.token
-  Presently authorization is cookie based only, however we could switch to a token
-  header if/when we run into cross server issues (or mobile, etc)
-*/
-function processToken(options) {
-  function _processToken(req, res, next) {
-    if (!path.apiRequest(req)) {
-      return next();
-    }
-
-    _.defaults(req, {
-      mf: {
-        auth: {}
-      }
-    });
-
-    var header = req.header('Cookie');
-    if (!header) {
-      logger.debug('no token found');
-      return (next());
-    }
-
-    var key = cookie.parse(header).EncAuth;
-    req.mf.auth.token = key;
-    logger.trace('token processed: ' + key);
-
-    return (next());
+  } else {
+    return user;
   }
 
-  return (_processToken);
-}
-
-/*
-  Based on the token set on the request (req.mf.auth.token) this
-  pulls the user from the database and stores in req.mf.auth.user
-  Updates the user's lastSeen time (findAndModify)
-*/
-function fetchUser(options) {
-  function _fetchUser(req, res, next) {
-    if (!path.apiRequest(req)) {
-      return next();
-    }
-   _.defaults(req, {
-      mf: {
-        auth: {}
-      }
-    });
-    var user = req.user;
-    if (!user) {
-      logger.warn('no user logged in');
-      return (next());
-    }
-
-    models.User.findOneAndUpdate({
-      username: req.user.username
-    }, {
-      lastSeen: new Date()
-    }, {
-      new: true
-    }, function (err, user) {
-      if (err) {
-        logger.error(err);
-        return utils.sendError.InternalError(err, res);
-      } else {
-        if (user) {
-          var url = req.url;
-          var len = url.length;
-          if (len > 50) {
-            url = url.substring(0, 50) + '... (' + len + ')';
-          }
-          logger.info(user.get('username') + ' ' + req.method + ' ' + url);
-          req.mf.auth.user = user.toObject();
-          return (next());
-        } else {
-          return utils.sendError.InvalidCredentialsError('No user with username' + req.user.username, res);
-        }
-      }
-    });
-  }
-
-  return (_fetchUser);
 }
 
 function determineStudentAccess(user, path, method) {
@@ -156,27 +72,23 @@ function protect(options) {
     if (!path.apiRequest(req)) {
       return next();
     }
-    _.defaults(req, {
-      mf: {
-        auth: {}
-      }
-    });
 
-    var user = getUser(req);
+    let user = getUser(req);
 
-    var openPaths = ['/api/users', '/api/stats', '/api/organizations'];
+
+    let openPaths = ['/api/users', '/api/organizations'];
     // /api/user - people need this to login; allows new users to see the user list
-    // /api/stats - nagios checks this
-    var openRequest = _.contains(openPaths, req.path);
+    // /api/organizations - need to see existing orgs when signing up
+
+    let openRequest = _.contains(openPaths, req.path);
     //var newOrgRequest = req.path === 'api.organizations' && req.method === 'POST';
     if ((openRequest && req.method === 'GET')) {
       return next();
     }
 
-    var userAuthenticated = req.isAuthenticated && req.isAuthenticated();
-    var notAuthenticated = !userAuthenticated;
+    let isAuthenticated = user !== null;
 
-    if (notAuthenticated) {
+    if (!isAuthenticated) {
       return utils.sendError.InvalidCredentialsError('You are not Authenticated.', res);
     }
 
@@ -189,10 +101,10 @@ function protect(options) {
     }
 
     // will only reach here if non-student
-    var userAuthorized = (userAuthenticated && (user.accountType === 'A' || user.isAuthorized));
+    let userAuthorized = (isAuthenticated && (user.accountType === 'A' || user.isAuthorized));
 
-    var notAuthorized = !userAuthorized;
-    var isGoogleUser = !!user.googleId;
+    let notAuthorized = !userAuthorized;
+    let isGoogleUser = !!user.googleId;
 
     // users who sign up with google need to be able to update their user info with org, requestReason, and location
     if (notAuthorized && !isGoogleUser) {
@@ -203,55 +115,6 @@ function protect(options) {
   }
 
   return (_protect);
-}
-
-function accessibleWorkspacesQuery(user) {
-  return {
-    $or: [{
-        owner: user
-      },
-      {
-        mode: 'public'
-      },
-      {
-        editors: user
-      }
-    ],
-    isTrashed: false
-  };
-}
-
-function loadAccessibleWorkspaces(options) {
-  function _loadAccessibleWorkspaces(req, res, next) {
-    var user = getUser(req);
-    var schema = path.getSchema(req);
-    if (!user || !path.schemaHasWorkspace(schema)) {
-      return (next()); //no user, no workspaces - revisit when public means public
-    }
-    models.Workspace.find(accessibleWorkspacesQuery(user)).select('_id').lean().exec(function (err, workspaces) {
-      if (err) {
-        logger.error('user required but not found');
-        throw new Error('user required but not found');
-      }
-      var ids = workspaces.map(function (w) {
-        return w._id;
-      });
-      req.mf.auth.workspaces = ids;
-      req.accessibleWorkspaces = ids;
-      return (next());
-    });
-  }
-
-  return (_loadAccessibleWorkspaces);
-}
-
-
-function test(options) {
-  function _test(req, res, next) {
-    return (next());
-  }
-
-  return (_test);
 }
 
 function getEmailAuth() {
@@ -299,18 +162,12 @@ function createUserAvatar(name) {
   const bgString = bgColor.substring(1);
   const formattedName = splitName(name);
   const baseUrl = `https://ui-avatars.com/api/?rounded=true&color=ffffff&background=${bgString}&name=${formattedName}`;
-  console.log('baseUrl is', baseUrl);
   return baseUrl;
 }
 
-module.exports.processToken = processToken;
-module.exports.fetchUser = fetchUser;
 module.exports.protect = protect;
-module.exports.test = test;
 module.exports.getUser = getUser;
 module.exports.requireUser = requireUser;
-module.exports.loadAccessibleWorkspaces = loadAccessibleWorkspaces;
-module.exports.accessibleWorkspacesQuery = accessibleWorkspacesQuery;
 module.exports.getEmailAuth = getEmailAuth;
 module.exports.getUserOrg = getUserOrg;
 module.exports.createUserAvatar = createUserAvatar;
