4a8a20bc07021798780aafad9db6083e93fbe9f7
Daniel Kelly <djk8552@gmail.com>
2019-07-09 09:44:38 -0400

    Update handling of request for new access token

--- server/middleware/mtAuth.js
+++ server/middleware/mtAuth.js
@@ -1,29 +1,77 @@
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
-
 const { User } = require('../datasource/schemas/');
-
 const { isValidMongoId} = require('../utils/mongoose');
+const ssoService = require('../services/sso');
+const { accessCookie, refreshCookie,} = require('../constants/sso');
+const { verifyJWT } = require('../utils/jwt');
 
 const secret = process.env.MT_USER_JWT_SECRET;
 
-const { getEncIssuerId, getMtIssuerId, getMtSsoUrl} = require('../middleware/appUrls');
+const setSsoCookie = (res, encodedToken) => {
+  let doSetSecure =
+  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
 
+  res.cookie(accessCookie.name, encodedToken, { httpOnly: true, maxAge: accessCookie.maxAge, secure: doSetSecure });
+};
 
-const { accessCookie, refreshCookie, apiToken } = require('../constants/sso');
+const setSsoRefreshCookie = (res, encodedToken) => {
+  let doSetSecure =
+  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
 
-const getMtUser = async (req) => {
+  res.cookie(refreshCookie.name, encodedToken, { httpOnly: true,secure: doSetSecure,});
+
+};
+
+const clearAccessCookie = (res) => {
+  res.cookie(accessCookie.name, '', {httpOnly: true, maxAge: 0});
+};
+
+const clearRefreshCookie = (res) => {
+  res.cookie(refreshCookie.name, '', {httpOnly: true, maxAge: 0});
+
+};
+
+const resolveAccessToken = async (token) => {
+  if (typeof token !== 'string') {
+    return null;
+  }
+
+  let [accessTokenErr, verifiedAccessToken] = await verifyJWT(token, secret);
+
+  return verifiedAccessToken;
+
+};
+
+const getMtUser = async (req, res) => {
   try {
-    let accessToken = req.cookies[accessCookie.name];
+    console.log('getmt user');
+    let verifiedAccessToken = await resolveAccessToken(req.cookies[accessCookie.name]);
+    let accessTokenErr;
 
-    if (accessToken === undefined) {
+    if (verifiedAccessToken !== null) {
+      return verifiedAccessToken;
+    }
+    // access token verification failed request new access token with refresh token
+
+    let currentRefreshToken = req.cookies[refreshCookie.name];
+
+    if (currentRefreshToken === undefined) {
       return null;
     }
 
-    // if token is not verified, error will be thrown
-    let verifiedToken = await jwt.verify(accessToken, secret);
+    // request new accessToken with refreshToken
+    let { accessToken } = await ssoService.requestNewAccessToken(currentRefreshToken);
 
-    return verifiedToken;
+    [ accessTokenErr, verifiedAccessToken] = await verifyJWT(accessToken, secret);
+
+    if (accessTokenErr) {
+      // should never happen
+      return null;
+    }
+
+    console.log('received new access token: ', verifiedAccessToken);
+
+    setSsoCookie(res, accessToken);
+    return verifiedAccessToken;
 
   }catch(err) {
     console.error(`Error getMtUser: ${err}`);
@@ -33,30 +81,9 @@ const getMtUser = async (req) => {
 
 };
 
-// used when communicating to MT SSO for forgot/reset password
-const generateAnonApiToken = (expiration = apiToken.expiresIn) => {
-  let payload = { iat: Date.now() };
-  let options = { expiresIn: expiration, issuer: getEncIssuerId(), audience: getMtIssuerId() };
-
-  return this.generateSignedJWT(payload, secret, options);
-};
-
-const requestNewAccessToken = async (refreshToken) => {
-  let url = `${getMtSsoUrl()}/auth/accessToken`;
-  let body = {refreshToken};
-
-  let token = await generateAnonApiToken();
-  let config = {
-    headers: { Authorization: 'Bearer ' + token },
-  };
-  let results = await axios.post(url, body, config);
-  return results.data;
-
-};
-
 const prepareMtUser = async (req, res, next) => {
   try {
-    let verifiedSsoUserDetails = await getMtUser(req);
+    let verifiedSsoUserDetails = await getMtUser(req, res);
     // user is null or verified payload from jwt token
     // set on request for later user to retrieve encompass user record
 
@@ -64,31 +91,16 @@ const prepareMtUser = async (req, res, next) => {
       req.mt.auth.user = verifiedSsoUserDetails;
       return next();
     }
+    // clear any invalid cookies
 
-    // check for refresh token and see
-    let currentRefreshToken = req.cookies[refreshCookie.name];
-    if (currentRefreshToken === undefined) {
-      return next();
-    }
-
-    let { accessToken, refreshToken} = await requestNewAccessToken(currentRefreshToken);
-
-    let verifiedAccessToken = await jwt.verify(accessToken, secret);
-    req.mt.auth.user = verifiedAccessToken;
-
-    this.setSsoCookie(res, accessToken);
-
-    if (typeof refreshToken === 'string') {
-      // only in case where refreshToken had expired and new one was issued
-      this.setSsoRefreshCookie(res, refreshToken);
-    }
+    clearAccessCookie(res);
+    clearRefreshCookie(res);
 
     next();
   }catch(err) {
-    console.log('err prepareMT: ', err);
+    console.log('err prepareMtUSer: ', err);
     next(err);
   }
-
 
 };
 
@@ -139,32 +151,11 @@ const extractBearerToken = req => {
   return authorization.split(' ')[1];
 };
 
-const generateSignedJWT = (payload, secret, options) => {
-  return jwt.sign(payload, secret, options);
-
-};
-
-const setSsoCookie = (res, encodedToken) => {
-  let doSetSecure =
-  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
-
-  res.cookie(accessCookie.name, encodedToken, { httpOnly: true, maxAge: accessCookie.maxAge, secure: doSetSecure });
-};
-
-const setSsoRefreshCookie = (res, encodedToken) => {
-  let doSetSecure =
-  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
-
-  res.cookie(refreshCookie.name, encodedToken, { httpOnly: true,secure: doSetSecure, maxAge: refreshCookie.maxAge });
-
-};
 
 module.exports.getMtUser = getMtUser;
 module.exports.prepareMtUser = prepareMtUser;
 module.exports.getEncUser = getEncUser;
 module.exports.prepareEncUser = prepareEncUser;
 module.exports.extractBearerToken = extractBearerToken;
-module.exports.generateSignedJWT = generateSignedJWT;
 module.exports.setSsoCookie = setSsoCookie;
 module.exports.setSsoRefreshCookie = setSsoRefreshCookie;
-module.exports.generateAnonApiToken = generateAnonApiToken;
