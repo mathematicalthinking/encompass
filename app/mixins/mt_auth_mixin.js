9228ccb295c56b03bbe99e352e6d5b84f7f72c50
Daniel Kelly <djk8552@gmail.com>
2019-07-23 16:09:31 -0400

    Update unauthorized page
    No longer link to NCTM

--- app/mixins/mt_auth_mixin.js
+++ app/mixins/mt_auth_mixin.js
@@ -30,6 +30,14 @@ getMtSignupUrl() {
 
 getSsoGoogleUrl() {
   return `${this.getMtSsoUrl()}/oauth/google?redirectURL=${this.getRedirectUrl()}`;
+},
+
+getContactEmail() {
+  let { host }  = window.location;
+  if (host === 'encompass.mathematicalthinking.org') {
+    return 'encompassmath@gmail.com';
+  }
+  return 'encompassmathtesting@gmail.com';
 }
 
 });
