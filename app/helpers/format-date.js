432fd59a79b2438baf0fce70b198bbb2e239865a
Daniel Kelly <djk8552@gmail.com>
2019-09-01 22:06:58 -0400

    Return 'N/A' from format-date helper if empty date value is passed in

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,9 +1,25 @@
 Encompass.FormatDateHelper = Ember.Helper.helper(function (args) {
   // args is array of arguments passed in from template
-  let [date, format, doUseRelativeTime] = args;
+  let [ date, format, doUseRelativeTime ] = args;
+
+  if (!date) {
+    return 'N/A';
+  }
+
+  let formattedDate;
+
+
+  let momentObj = moment(date);
+
+  if (!momentObj.isValid()) {
+    return 'N/A';
+  }
 
   if (doUseRelativeTime) {
-    return moment(date).fromNow();
+    formattedDate = momentObj.fromNow();
+  } else {
+    formattedDate = momentObj.format(format);
   }
-  return moment(date).format(format);
+
+  return formattedDate;
 });
