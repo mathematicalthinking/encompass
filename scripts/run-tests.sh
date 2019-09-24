d3481ba59c90efd973b8ae4e3282de5899ae1e4c
Daniel Kelly <djk8552@gmail.com>
2019-09-24 15:28:38 -0400

    tt

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 grunt mochaTest:"${TEST_SUITE}"
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  grunt mochaTest:travis
-else
-  grunt mochaTest:"${TEST_SUITE}"
-fi
+# if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+#   grunt mochaTest:travis
+# else
+#   grunt mochaTest:"${TEST_SUITE}"
+# fi
