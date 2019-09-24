efcd15c7bcfa4a0ee07f860eed79b3207fb47452
Daniel Kelly <djk8552@gmail.com>
2019-09-24 15:52:37 -0400

    tt

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -ev
+if [ "${TEST_SUITE}" = "e2e" || "${TEST_SUITE}" = "travis" ]; then
+  export DISPLAY=:99.0
+  sleep 3
+  grunt serve-seed &
+  sleep 6
+else
+  NODE_ENV=seed node server/server.js &
+fi
 grunt mochaTest:"${TEST_SUITE}"
 # if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 #   grunt mochaTest:travis
