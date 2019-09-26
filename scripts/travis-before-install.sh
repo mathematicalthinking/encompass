a9998c114ed7e8c16d0e3227a8bbd9efc598403e
Daniel Kelly <djk8552@gmail.com>
2019-09-26 08:44:25 -0400

    tt

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -13,22 +13,21 @@ if [ $# -eq 1 ]
   githubAccount="mathematicalthinking"
 fi
 
+echo "Branch: $branch"
+echo "Account: $githubAccount"
   # clone mt-sso
-  - "cd .."
-  - "git clone --branch=$branch https://github.com/$githubAccount/mt-sso.git $githubAccount/$branch"
-  - "cd mt-sso && npm i"
-  - "npm run test-travis &"
-  - "sleep 5"
+  cd ..
+  git clone --branch=$branch https://github.com/$githubAccount/mt-sso.git $githubAccount/$branch
+  cd mt-sso && npm i
+  npm run test-travis &
+  sleep 5
 
   # clone vmt
-  - "cd .."
-  - "git clone --branch=$branch https://github.com/$githubAccount/vmt.git $githubAccount/$branch"
-  - "cd vmt/server"
-  - "npm i --only=production"
-  # - npm --prefix ../client install ../client
-  # - npm run build-production-test
-  # - npm run build-enc-test
-  - "npm run test-travis &"
-  - "sleep 6"
-  - "cd ../../encompass"
-  - "/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1600x1024x16"
+  cd ..
+  git clone --branch=$branch https://github.com/$githubAccount/vmt.git $githubAccount/$branch
+  cd vmt/server
+  npm i --only=production
+  npm run test-travis &
+  sleep 6
+  cd ../../encompass
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1600x1024x16
