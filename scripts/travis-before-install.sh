7ca023a9273ba76426ab3c7095cc211c2d6d019f
Daniel Kelly <djk8552@gmail.com>
2019-09-26 08:54:05 -0400

    tt

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -17,14 +17,14 @@ echo "Branch: $branch"
 echo "Account: $githubAccount"
   # clone mt-sso
   cd ..
-  git clone --branch=$branch https://github.com/$githubAccount/mt-sso.git $githubAccount/$branch
+  git clone --branch=$branch https://github.com/$githubAccount/mt-sso.git $githubAccount/mt-sso
   cd $githubAccount/mt-sso && npm i
   npm run test-travis &
   sleep 5
 
   # clone vmt
   cd ..
-  git clone --branch=$branch https://github.com/$githubAccount/vmt.git $githubAccount/$branch
+  git clone --branch=$branch https://github.com/$githubAccount/vmt.git $githubAccount/vmt
   cd $githubAccount/vmt/server
   npm i --only=production
   npm run test-travis &
