4182bcb1586bc618c801779aaf8f47875d7183b5
crispinamuriel <crispinamuriel@gmail.com>
2021-02-19 15:02:25 -0500

    comment for home page route

--- app/routes/home_page_route.js
+++ app/routes/home_page_route.js
@@ -1,7 +1,9 @@
 /**
- * # Home Page Assignments Route
- * @description Route for dealing with all assignment objects
- */
+  * # Home Page Route
+  * @description This is a base route for the dashboard, This model hook is returning all assignments, Assignments is being passed to be displyed in child dashboard table component.
+  * @author Crispina Muriel
+  * @since 2.3.0
+  */
 Encompass.HomePageRoute = Encompass.AuthenticatedRoute.extend({
   model: function () {
     let assignments = this.get("store").findAll("assignment");
