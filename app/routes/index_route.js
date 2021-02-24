cd9b54b6cc358dcedf3bf68e63a6c6e496332f26
crispinamuriel <crispinamuriel@gmail.com>
2021-02-24 16:25:38 -0500

    fix authorization bug

--- app/routes/index_route.js
+++ app/routes/index_route.js
@@ -1,6 +1,21 @@
 /**
   */
 Encompass.IndexRoute = Ember.Route.extend({
+    beforeModel: function() {
+    this._super.apply(this, arguments);
+    this.authenticate();
+  },
+  authenticate: function() { //this is duplicated here and in AuthentecatedRoute and in ApplicationRoute
+    var user = this.modelFor('application');
+    if(!user.get('isAuthenticated')) {
+      this.get('store').unloadAll();
+      this.transitionTo('auth.login');
+    }else if (user.get('email') && !user.get('isEmailConfirmed') && !user.get('isStudent')) {
+      this.transitionTo('unconfirmed');
+    }else if(!user.get('isAuthz')) {
+      this.transitionTo('unauthorized');
+    }
+  },
   model: function () {
     let user = this.modelFor('application');
     let assignments = this.get("store").findAll("assignment");
