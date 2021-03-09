845542152c04de26f56f2f312665acc12d7e542f
crispinamuriel <crispinamuriel@gmail.com>
2021-03-09 15:41:04 -0500

    fix table div and scroll

--- app/components/dashboard-workspaces-list.js
+++ app/components/dashboard-workspaces-list.js
@@ -1,21 +1,23 @@
 Encompass.DashboardWorkspacesListComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   utils: Ember.inject.service('utility-methods'),
   myLinkedAssignments: null,
+  tableHeight: '',
 
 
   didReceiveAttrs: function() {
     this.yourLinkedAssignments();
   },
 
+
   yourLinkedAssignments: function () {
     const workspaces = this.workspaces;
+    this.tableHeight = this.workspaces.content.length  * 31 + 'px';
     let assignments = this.assignments;
 
     const workspaceLinkedAssignmentIds = {};
 
 
     workspaces.forEach(workspace => {
-      // console.log(workspace);
       if (
         workspace._internalModel &&
         workspace._internalModel.__relationships &&
@@ -36,6 +38,8 @@ Encompass.DashboardWorkspacesListComponent = Ember.Component.extend(Encompass.Cu
 
 
     this.myLinkedAssignments = workspaceLinkedAssignmentIds;
-  }
+  },
+
+
 
 });
