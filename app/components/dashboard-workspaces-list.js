5b42a2435bd400c16e688e837e7f8324cece26e3
crispinamuriel <crispinamuriel@gmail.com>
2021-03-26 17:36:08 -0400

    working with pedro to refactor

--- app/components/dashboard-workspaces-list.js
+++ app/components/dashboard-workspaces-list.js
@@ -1,44 +1,14 @@
 Encompass.DashboardWorkspacesListComponent = Ember.Component.extend(Encompass.CurrentUserMixin, {
   utils: Ember.inject.service('utility-methods'),
-  myLinkedAssignments: null,
   tableHeight: '',
 
 
   didReceiveAttrs: function() {
-    this.yourLinkedAssignments();
+    this.calculateWSListHeight();
   },
 
 
-  yourLinkedAssignments: function () {
-    const workspaces = this.workspaces;
+  calculateWSListHeight: function () {
     this.tableHeight = this.workspaces.content.length  * 31 + 'px';
-
-    const workspaceLinkedAssignmentIds = {};
-
-
-    workspaces.forEach(workspace => {
-      if (
-        workspace._internalModel &&
-        workspace._internalModel.__relationships &&
-        workspace._internalModel.__relationships.initializedRelationships &&
-        workspace._internalModel.__relationships.initializedRelationships
-          .linkedAssignment &&
-        workspace._internalModel.__relationships.initializedRelationships
-          .linkedAssignment.canonicalState &&
-        workspace._internalModel.__relationships.initializedRelationships
-          .linkedAssignment.canonicalState.id
-      ) {
-        workspaceLinkedAssignmentIds[workspace.id] =
-          workspace._internalModel.__relationships.initializedRelationships.linkedAssignment.canonicalState.id;
-      }
-
-    });
-
-
-
-    this.myLinkedAssignments = workspaceLinkedAssignmentIds;
   },
-
-
-
 });
