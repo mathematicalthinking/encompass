a523033d3b95bf2276ef1f53836edd8cb5e8cf58
iamtimleonard <timleonardcello@gmail.com>
2021-05-13 14:55:46 -0400

    clean up login function

--- app/components/log-in.js
+++ app/components/log-in.js
@@ -1,63 +1,60 @@
-Encompass.LogInComponent = Ember.Component.extend(Encompass.ErrorHandlingMixin, {
-  classNames: ['login-page'],
-  incorrectPassword: false,
-  incorrectUsername: false,
-  missingCredentials: false,
-  postErrors: [],
+Encompass.LogInComponent = Ember.Component.extend(
+  Encompass.ErrorHandlingMixin,
+  {
+    classNames: ["login-page"],
+    incorrectPassword: false,
+    incorrectUsername: false,
+    missingCredentials: false,
+    postErrors: [],
 
-  oauthErrorMsg: function() {
-    if (this.get('oauthError') === 'emailUnavailable') {
-      return 'The provided email address is already associated with an existing account';
-    }
-  }.property('oauthError'),
-
-  actions: {
-    login: function () {
-      var that = this;
-      var username = that.get('username');
-      var usernameTrim;
-      if (username) {
-        usernameTrim = username.trim();
-      } else {
-        usernameTrim = '';
+    oauthErrorMsg: function () {
+      if (this.get("oauthError") === "emailUnavailable") {
+        return "The provided email address is already associated with an existing account";
       }
-      var password = that.get('password');
+    }.property("oauthError"),
 
-      if (!usernameTrim || !password) {
-        that.set('missingCredentials', true);
-        return;
-      }
-
-      var createUserData = {
-        username: usernameTrim,
-        password: password,
-      };
-      Ember.$.post({
-        url: '/auth/login',
-        data: createUserData
-      }).
-      then((res) => {
-        if (res.message === 'Incorrect password') {
-          that.set('incorrectPassword', true);
-        }else if(res.message === 'Incorrect username') {
-          that.set('incorrectUsername', true);
-        } else  {
-          that.sendAction('toHome');
+    actions: {
+      login: function () {
+        if (!this.get("username") || !this.get("password")) {
+          this.set("missingCredentials", true);
+          return;
         }
-      })
-      .catch((err) => {
-        this.handleErrors(err, 'postErrors');
-      });
+
+        let createUserData = {
+          username: this.get("username").trim(),
+          password: this.get("password").trim(),
+        };
+        Ember.$.post({
+          url: "/auth/login",
+          data: createUserData,
+        })
+          .then((res) => {
+            if (res.message === "Incorrect password") {
+              this.set("incorrectPassword", true);
+            } else if (res.message === "Incorrect username") {
+              this.set("incorrectUsername", true);
+            } else {
+              this.sendAction("toHome");
+            }
+          })
+          .catch((err) => {
+            this.handleErrors(err, "postErrors");
+          });
+      },
+
+      resetErrors() {
+        const errors = [
+          "incorrectUsername",
+          "incorrectPassword",
+          "missingCredentials",
+        ];
+
+        for (let error of errors) {
+          if (this.get(error)) {
+            this.set(error, false);
+          }
+        }
+      },
     },
-
-    resetErrors() {
-      const errors = ['incorrectUsername', 'incorrectPassword', 'missingCredentials'];
-
-      for (let error of errors) {
-        if (this.get(error)) {
-          this.set(error, false);
-        }
-      }
-    }
   }
-});
+);
