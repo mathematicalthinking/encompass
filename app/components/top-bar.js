71a3fceca1a3241ba6bf744b405a3549fa733abc
iamtimleonard <timleonardcello@gmail.com>
2021-06-01 11:29:58 -0400

    remove al_nav id

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -3,7 +3,6 @@ Encompass.TopBarComponent = Ember.Component.extend(
   Encompass.ErrorHandlingMixin,
   {
     tagName: "nav",
-    elementId: "al_header",
     classNames: ["nav"],
     toggleRoleErrors: [],
     alert: Ember.inject.service("sweet-alert"),
