35f943f7ba87baee1d955dc1e6e928fe5be2cf2b
iamtimleonard <timleonardcello@gmail.com>
2021-06-14 14:38:14 -0400

    move sections template up to proper directory, clean up sections_route.js

--- app/routes/sections_route.js
+++ app/routes/sections_route.js
@@ -1,15 +1,5 @@
-/**
-  * # Sections Route
-  * @description Route for dealing with all section objects
-  * @todo This is really the sections_index route and should be named as such by convention
-  */
 Encompass.SectionsRoute = Encompass.AuthenticatedRoute.extend({
   model: function () {
-    let sections = this.get('store').findAll('section');
-    return sections;
+    return this.get('store').findAll('section');
   },
-
-  renderTemplate: function(){
-    this.render('sections/sections');
-  }
 });
