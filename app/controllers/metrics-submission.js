454c6f0424c66d2808674f7f9d0b01d60804c6dd
iamtimleonard <timleonardcello@gmail.com>
2021-06-16 16:59:11 -0400

    toggle comment list

--- app/controllers/metrics-submission.js
+++ app/controllers/metrics-submission.js
@@ -1,6 +1,7 @@
 Encompass.MetricsSubmissionController = Ember.Controller.extend({
   showSelections: false,
   showFolders: false,
+  showComments: false,
   actions: {
     handleToggle: function(prop){
       this.toggleProperty(prop);
