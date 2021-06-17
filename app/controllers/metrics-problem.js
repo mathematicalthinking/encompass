85990b90b84e677b4df9d9ef45b54a8562b3ec76
iamtimleonard <timleonardcello@gmail.com>
2021-06-17 12:36:00 -0400

    show submissions to problem

--- app/controllers/metrics-problem.js
+++ app/controllers/metrics-problem.js
@@ -1,13 +1,20 @@
 Encompass.MetricsProblemController = Ember.Controller.extend({
   showProblemText: false,
   relevantWorkspaces: null,
+  problemSubmissions: null,
   actions: {
     toggleProblemText: function(){
       this.toggleProperty('showProblemText');
     },
     findWorkspaces: function(){
-      let data = this.get('store').findAll('workspace').filterBy("firstSubmission.publication.puzzle.id", this.model.id);
-      this.set('relevantWorkspaces', data);
+      console.log('finding');
+    },
+    findSubmissions: function(){
+      fetch(`/api/submissions`)
+        .then(res=>res.json())
+        .then(({submissions})=>{
+          this.set('problemSubmissions', submissions.filter(s=>s.publication.puzzle.puzzleId === this.get('model.puzzleId')));
+        });
     }
   }
 });
