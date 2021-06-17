34a2d5b9d3d329d012e18546a625ccc54febe404
iamtimleonard <timleonardcello@gmail.com>
2021-06-17 09:14:39 -0400

    add responses to submission views

--- app/controllers/metrics-submission.js
+++ app/controllers/metrics-submission.js
@@ -2,6 +2,7 @@ Encompass.MetricsSubmissionController = Ember.Controller.extend({
   showSelections: false,
   showFolders: false,
   showComments: false,
+  showResponses: false,
   actions: {
     handleToggle: function(prop){
       this.toggleProperty(prop);
