329e98f24aee21952b2a5415938aea83c0d00556
iamtimleonard <timleonardcello@gmail.com>
2021-06-18 13:56:30 -0400

    remove unneeded php files

--- build/static-ui/templates/al-feedback-form.php
+++
@@ -1,7 +0,0 @@
-<form id="al_feedback">
-	<textarea name="feedback" placeholder="Enter your comments here..."></textarea>
-	<div class="al_controller">
-		<a href="#" class="al_popup_close">CANCEL</a>
-		<a href="#" class="al_popup_submit">SEND</a>
-	</div>
-</form>
