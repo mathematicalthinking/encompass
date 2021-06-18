3d547a71d1e234882879c3cf352346d867c63b29
iamtimleonard <timleonardcello@gmail.com>
2021-06-18 10:20:11 -0400

    display answers to a problem, begin docs for querying api

--- app/controllers/metrics-problem.js
+++ app/controllers/metrics-problem.js
@@ -1,7 +1,7 @@
 Encompass.MetricsProblemController = Ember.Controller.extend({
   showProblemText: false,
-  relevantWorkspaces: null,
-  problemSubmissions: null,
+  relevantWorkspaces: [],
+  problemSubmissions: [],
   actions: {
     toggleProblemText: function(){
       this.toggleProperty('showProblemText');
@@ -13,12 +13,14 @@ Encompass.MetricsProblemController = Ember.Controller.extend({
       this.get('store').query('answer', {
         filterBy: {
           problem: this.get('model.id')
-        }
+        },
+        didConfirmLargeRequest: true,
       }).then(res => {
-        this.set('problemSubmissions', res.data);
+        console.log(res);
+        this.set('problemSubmissions', res);
       });
     }
-  }
+  },
 });
 
 // /api/answers?didConfirmLargeRequest=true&filterBy%5Bproblem%5D=5bac0800ea4c0a230b2c81b0&filterBy%5BstartDate%5D=2003-06-04&filterBy%5BendDate%5D=2021-06-17&filterBy%5BisVmtOnly%5D=false&filterBy%5BisTrashedOnly%5D=false
---
+++ queries.md
@@ -0,0 +1,8 @@
+## Basics
+- Look through the API files to see what it's expecting
+
+## Answers
+- filterBy
+  - problemId
+  - other things
+- in order to get more than metadata, didConfirmLargeRequest must be true
