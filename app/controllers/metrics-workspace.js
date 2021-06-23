c991e269356efdb86111a7dd16a9496297ba048e
iamtimleonard <timleonardcello@gmail.com>
2021-06-23 17:05:29 -0400

    get array of comments in order to map word usage

--- app/controllers/metrics-workspace.js
+++ app/controllers/metrics-workspace.js
@@ -1,10 +1,15 @@
 Encompass.MetricsWorkspaceController = Ember.Controller.extend({
   heading: null,
   content: null,
+  wordMap: {},
   actions: {
     setContent: function(name, data){
       this.set('heading', name);
       this.set('content', data);
     },
+    generateWordMap: function(){
+      const comments = this.get("model.comments").mapBy('text').map(comment=>comment.split(' '));
+      console.log(comments);
+    }
   }
 });
