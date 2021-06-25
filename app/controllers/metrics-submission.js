a6036f79d205025af99c4fbd72514631fa57ddac
iamtimleonard <timleonardcello@gmail.com>
2021-06-25 10:51:17 -0400

    submission info as tabular data

--- app/controllers/metrics-submission.js
+++ app/controllers/metrics-submission.js
@@ -5,7 +5,11 @@ Encompass.MetricsSubmissionController = Ember.Controller.extend({
   showResponses: false,
   actions: {
     handleToggle: function(prop){
-      this.toggleProperty(prop);
+      this.set('showSelections', false);
+      this.set('showFolders', false);
+      this.set('showComments', false);
+      this.set('showResponses', false);
+      this.set(prop, true);
     }
   }
 });
