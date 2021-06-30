81b5277069dd97f104951c9e18c7b5488d4a2003
iamtimleonard <timleonardcello@gmail.com>
2021-06-30 09:41:16 -0400

    fix lifecycle for workspace route

--- app/routes/metrics-workspace.js
+++ app/routes/metrics-workspace.js
@@ -1,8 +1,11 @@
 Encompass.MetricsWorkspaceRoute = Ember.Route.extend({
   actions: {
     willTransition(){
-      this.controller.set('heading', null);
-      this.controller.set('content', null);
+      this.controller.set('showSelections', false);
+      this.controller.set('showFolders', false);
+      this.controller.set('showComments', false);
+      this.controller.set('showResponses', false);
+      this.controller.set('showSubmissions', false);
     }
   }
 });
