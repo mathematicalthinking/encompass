53175d325f87583a80a32557b1a1a45e6b569af3
iamtimleonard <timleonardcello@gmail.com>
2021-08-04 12:36:30 -0400

    convert logout route to native class syntax

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,10 +1,10 @@
 import Route from '@ember/routing/route';
 import $ from 'jquery';
 
-export default Route.extend({
-  beforeModel: function () {
+export default class LogoutRoute extends Route {
+  beforeModel() {
     return $.get('/auth/logout').then(() => {
       window.location.href = '/';
     });
-  },
-});
+  }
+}
