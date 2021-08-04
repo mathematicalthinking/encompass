703e4c8e39535080d952ba6c1d19a0cec50874da
iamtimleonard <timleonardcello@gmail.com>
2021-08-04 08:49:24 -0400

    update answers route to native syntax

--- app/routes/answers.js
+++ app/routes/answers.js
@@ -1,16 +1,17 @@
 import AuthenticatedRoute from './_authenticated_route';
+import { inject as service } from '@ember/service';
 /**
  * # Answers Route
  * @description Route for dealing with all answer objects
- * @todo This is really the answers_index route and should be named as such by convention
  */
-export default AuthenticatedRoute.extend({
-  model: function () {
+export default class AnswersRoute extends AuthenticatedRoute {
+  @service store;
+  model() {
     let answers = this.store.findAll('answer');
     return answers;
-  },
+  }
 
-  renderTemplate: function () {
+  renderTemplate() {
     this.render('answers/answers');
-  },
-});
+  }
+}
