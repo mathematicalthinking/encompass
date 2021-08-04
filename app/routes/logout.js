dd49ecc3eedae337f09a15643056f72dd7f3d91e
iamtimleonard <timleonardcello@gmail.com>
2021-08-04 12:51:00 -0400

    remove jquery from logout route

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,10 +1,12 @@
 import Route from '@ember/routing/route';
-import $ from 'jquery';
 
 export default class LogoutRoute extends Route {
   beforeModel() {
-    return $.get('/auth/logout').then(() => {
-      window.location.href = '/';
-    });
+    try {
+      fetch('/auth/logout');
+      return (window.location.href = '/');
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
