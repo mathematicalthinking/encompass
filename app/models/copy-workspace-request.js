3b23c7367561ce86d7d2c050dfdd57b5fb8de3eb
iamtimleonard <timleonardcello@gmail.com>
2021-08-05 17:03:03 -0400

    update copy-workspace-request to modern syntax

---
+++ app/models/copy-workspace-request.js
@@ -0,0 +1,18 @@
+import Model, { attr, belongsTo } from '@ember-data/model';
+import Auditable from './_auditable_mixin';
+
+export default class CopyWorkspaceRequestModel extends Model.extend(Auditable) {
+  @attr('string') name;
+  @belongsTo('user', { inverse: null }) owner;
+  @attr('string') mode;
+  @belongsTo('workspace', { inverse: null }) originalWsId;
+  @attr submissionOptions;
+  @attr folderOptions;
+  @attr selectionOptions;
+  @attr commentOptions;
+  @attr responseOptions;
+  @attr permissionOptions;
+  @attr('string') copyWorkspaceError;
+  @belongsTo('workspace') createdWorkspace;
+  @belongsTo('folder-set') createdFolderSet;
+}
