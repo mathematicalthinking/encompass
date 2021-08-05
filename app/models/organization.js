411c465ac8d1af46fbfb17000ed76bfd9b78dfd5
iamtimleonard <timleonardcello@gmail.com>
2021-08-05 16:11:19 -0400

    remove computed property from organization model

--- app/models/organization.js
+++ app/models/organization.js
@@ -1,9 +1,10 @@
 import Model, { attr, hasMany } from '@ember-data/model';
-import { alias } from '@ember/object/computed';
 import Auditable from '../models/_auditable_mixin';
 
 export default class OrganizationModel extends Model.extend(Auditable) {
-  @alias('id') organizationId;
+  get organizationId() {
+    return this.id;
+  }
   @attr('string') name;
   @hasMany('problem', { async: true, inverse: null }) recommendedProblems;
 }
