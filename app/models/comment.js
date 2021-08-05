bbd96e3237e020a8b6b80acb3b899950ef867f01
iamtimleonard <timleonardcello@gmail.com>
2021-08-05 16:59:29 -0400

    update comment model to modern syntax

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,22 +1,24 @@
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
-import { computed } from '@ember/object';
 import Auditable from '../models/_auditable_mixin';
 
-export default Model.extend(Auditable, {
-  label: attr('string'),
-  text: attr('string'),
-  useForResponse: attr('boolean', { defaultValue: false }),
-  selection: belongsTo('selection'),
+export default class CommentModel extends Model.extend(Auditable) {
+  @attr('string') label;
+  @attr('string') text;
+  @attr('boolean', { defaultValue: false }) useForResponse;
+  @belongsTo('selection') selection;
   //the ultimate origin of this comment
-  origin: belongsTo('comment', { inverse: 'ancestors', async: true }),
-  ancestors: hasMany('comment', { inverse: 'origin', async: true }),
-  parent: belongsTo('comment', { inverse: 'children', async: true }),
-  children: hasMany('comment', { inverse: 'parent', async: true }),
-  submission: belongsTo('submission', { async: true }),
-  workspace: belongsTo('workspace'),
-  relevance: 0, // Used for sorting (gets set by controller)
-  type: computed('selection', 'submission', 'workspace', function () {
+  @belongsTo('comment', { inverse: 'ancestors', async: true }) origin;
+  @hasMany('comment', { inverse: 'origin', async: true }) ancestors;
+  @belongsTo('comment', { inverse: 'children', async: true }) parent;
+  @hasMany('comment', { inverse: 'parent', async: true }) children;
+  @belongsTo('submission', { async: true }) submission;
+  @belongsTo('workspace') workspace;
+  relevance = 0; // Used for sorting (gets set by controller)
+  get type() {
     return 'selection';
-  }),
-  originalComment: belongsTo('comment', { inverse: null }),
-});
+  }
+  // type: computed('selection', 'submission', 'workspace', function () {
+  //   return 'selection';
+  // }),
+  @belongsTo('comment', { inverse: null }) originalComment;
+}
