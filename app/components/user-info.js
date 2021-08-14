8ede539ea1748924b8a26e9309e40a9d2dfcb4ea
iamtimleonard <timleonardcello@gmail.com>
2021-08-14 13:31:34 -0400

    remove later import from user-info component

--- app/components/user-info.js
+++ app/components/user-info.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { computed, observer } from '@ember/object';
-import { later } from '@ember/runloop';
 import { inject as service } from '@ember/service';
 import moment from 'moment';
 import ErrorHandlingMixin from '../mixins/error_handling_mixin';
