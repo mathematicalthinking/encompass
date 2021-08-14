e4f3c223a19f5c3ff2795abf9681bf17d87fab95
iamtimleonard <timleonardcello@gmail.com>
2021-08-14 13:02:39 -0400

    convert user-list to glimmer

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -1,104 +1,118 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { computed } from '@ember/object';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default Component.extend({
-  elementId: 'user-list-admin',
-  showDeletedUsers: false,
+export default class UserListComponent extends Component {
+  @tracked showDeletedUsers = false;
+  @service store;
 
-  unauthUsers: computed(
-    'users.@each.isAuthorized',
-    'users.@each.isTrashed',
-    function () {
-      let users = this.users.filterBy('isTrashed', false);
-      let unauthUsers = users.filterBy('isAuthorized', false);
-      if (this.currentUser.isPdAdmin) {
-        let orgUsersNotYou = unauthUsers
-          .filterBy('organization.id', this.currentUser.get('organization.id'))
-          .rejectBy('username', this.currentUser.user);
-        return orgUsersNotYou.sortBy('createDate').reverse();
-      }
-      return unauthUsers.sortBy('createDate').reverse();
+  @computed('args.users.@each.isAuthorized', 'args.users.@each.isTrashed')
+  get unauthUsers() {
+    let users = this.args.users.filterBy('isTrashed', false);
+    let unauthUsers = users.filterBy('isAuthorized', false);
+    if (this.args.currentUser.isPdAdmin) {
+      let orgUsersNotYou = unauthUsers
+        .filterBy(
+          'organization.id',
+          this.args.currentUser.get('organization.id')
+        )
+        .rejectBy('username', this.args.currentUser.user);
+      return orgUsersNotYou.sortBy('createDate').reverse();
     }
-  ),
+    return unauthUsers.sortBy('createDate').reverse();
+  }
 
-  adminUsers: computed(
-    'users.@each.accountType',
-    'users.@each.isAuthorized',
-    'users.@each.isTrashed',
-    function () {
-      let users = this.users.filterBy('isTrashed', false);
-      let authUsers = users.filterBy('isAuthorized', true);
-      let adminUsers = authUsers.filterBy('accountType', 'A');
-      let adminUsersNotYou = adminUsers.rejectBy(
-        'username',
-        this.currentUser.username
-      );
-      return adminUsersNotYou.sortBy('createDate').reverse();
+  @computed(
+    'args.users.@each.accountType',
+    'args.users.@each.isAuthorized',
+    'args.users.@each.isTrashed',
+    'args.currentUser.username'
+  )
+  get adminUsers() {
+    let users = this.args.users.filterBy('isTrashed', false);
+    let authUsers = users.filterBy('isAuthorized', true);
+    let adminUsers = authUsers.filterBy('accountType', 'A');
+    let adminUsersNotYou = adminUsers.rejectBy(
+      'username',
+      this.args.currentUser.username
+    );
+    return adminUsersNotYou.sortBy('createDate').reverse();
+  }
+
+  @computed(
+    'args.users.@each.accountType',
+    'args.users.@each.isAuthorized',
+    'args.users.@each.isTrashed'
+  )
+  get pdUsers() {
+    let users = this.args.users.filterBy('isTrashed', false);
+    let authUsers = users.filterBy('isAuthorized', true);
+    let students = authUsers.filterBy('accountType', 'P');
+    return students.sortBy('createDate').reverse();
+  }
+
+  @computed(
+    'args.users.@each.isAuthorized',
+    'args.users.@each.accountType',
+    'args.users.@each.isTrashed',
+    'args.currentUser.isPdAdmin',
+    'args.currentUser.username'
+  )
+  get teacherUsers() {
+    let users = this.args.users.filterBy('isTrashed', false);
+    let teacherUsers = users.filterBy('accountType', 'T');
+    let authTeachers = teacherUsers.filterBy('isAuthorized', true);
+    if (this.args.currentUser.isPdAdmin) {
+      let orgUsersNotYou = authTeachers
+        .filterBy(
+          'organization.id',
+          this.args.currentUser.get('organization.id')
+        )
+        .rejectBy('username', this.args.currentUser.username);
+      return orgUsersNotYou.sortBy('createDate').reverse();
     }
-  ),
-
-  pdUsers: computed(
-    'users.@each.accountType',
-    'users.@each.isAuthorized',
-    'users.@each.isTrashed',
-    function () {
-      let users = this.users.filterBy('isTrashed', false);
-      let authUsers = users.filterBy('isAuthorized', true);
-      let students = authUsers.filterBy('accountType', 'P');
-      return students.sortBy('createDate').reverse();
+    return authTeachers.sortBy('createDate').reverse();
+  }
+  @computed(
+    'args.users.@each.accountType',
+    'args.users.@each.isAuthorized',
+    'args.users.@each.isTrashed',
+    'args.currentUser.isPdAdmin',
+    'args.currentUser.username'
+  )
+  get studentUsers() {
+    let users = this.args.users.filterBy('isTrashed', false);
+    let authUsers = users.filterBy('isAuthorized', true);
+    let students = authUsers.filterBy('accountType', 'S');
+    if (this.args.currentUser.isPdAdmin) {
+      let orgUsersNotYou = students
+        .filterBy(
+          'organization.id',
+          this.args.currentUser.get('organization.id')
+        )
+        .rejectBy('username', this.args.currentUser.username);
+      return orgUsersNotYou.sortBy('createDate').reverse();
     }
-  ),
-
-  teacherUsers: computed(
-    'users.@each.isAuthorized',
-    'users.@each.accountType',
-    'users.@each.isTrashed',
-    function () {
-      let users = this.users.filterBy('isTrashed', false);
-      let teacherUsers = users.filterBy('accountType', 'T');
-      let authTeachers = teacherUsers.filterBy('isAuthorized', true);
-      if (this.currentUser.isPdAdmin) {
-        let orgUsersNotYou = authTeachers
-          .filterBy('organization.id', this.currentUser.get('organization.id'))
-          .rejectBy('username', this.currentUser.username);
-        return orgUsersNotYou.sortBy('createDate').reverse();
-      }
-      return authTeachers.sortBy('createDate').reverse();
-    }
-  ),
-
-  studentUsers: computed(
-    'users.@each.accountType',
-    'users.@each.isAuthorized',
-    'users.@each.isTrashed',
-    function () {
-      let users = this.users.filterBy('isTrashed', false);
-      let authUsers = users.filterBy('isAuthorized', true);
-      let students = authUsers.filterBy('accountType', 'S');
-      if (this.currentUser.isPdAdmin) {
-        let orgUsersNotYou = students
-          .filterBy('organization.id', this.currentUser.get('organization.id'))
-          .rejectBy('username', this.currentUser.username);
-        return orgUsersNotYou.sortBy('createDate').reverse();
-      }
-      return students.sortBy('createDate').reverse();
-    }
-  ),
-
-  trashedUsers: computed('users.@each.isTrashed', function () {
+    return students.sortBy('createDate').reverse();
+  }
+  @computed('args.users.@each.isTrashed')
+  get trashedUsers() {
     return this.store.query('user', {
       isTrashed: true,
     });
-  }),
+  }
   // These are all the students that are in sections you are a teacher of
-  yourStudents: computed('users.@each.accountType', function () {
-    let yourSections = this.currentUser.get('sections');
+  @computed('args.users.@each.accountType', 'args.currentUser')
+  get yourStudents() {
+    let yourSections = this.args.currentUser.get('sections');
     let yourTeacherSections = yourSections.filterBy('role', 'teacher');
     let yourSectionIds = yourTeacherSections.map((section) => {
       return section.sectionId;
     });
 
-    let studentUsers = this.users.filterBy('accountType', 'S');
+    let studentUsers = this.args.users.filterBy('accountType', 'S');
 
     let studentListing = studentUsers.map((student) => {
       for (let section of student.get('sections')) {
@@ -111,18 +125,20 @@ export default Component.extend({
     });
 
     return studentListing.without(undefined);
-  }),
+  }
 
   // These are all the users that you have created - filter out duplicates
-  yourUsers: computed('users.@each.isTrashed', function () {
-    let yourId = this.currentUser.get('id');
-    let yourUsers = this.users.filterBy('createdBy.id', yourId);
+  @computed('args.users.@each.isTrashed', 'args.currentUser')
+  get yourUsers() {
+    let yourId = this.args.currentUser.get('id');
+    let yourUsers = this.args.users.filterBy('createdBy.id', yourId);
     return yourUsers.sortBy('createDate').reverse();
-  }),
+  }
 
   // These are all the users that are in the same org as you
-  orgUsers: computed('users.@each.isTrashed', function () {
-    let usersWithOrgs = this.users.filter((user) => {
+  // @computed('args.users.@each.isTrashed', 'args.currentUser.username')
+  get orgUsers() {
+    let usersWithOrgs = this.args.users.filter((user) => {
       let accountType = user.get('accountType');
       return (
         !user.get('isTrashed') &&
@@ -131,18 +147,17 @@ export default Component.extend({
         accountType
       );
     });
-    let yourOrgId = this.currentUser.get('organization').get('id');
+    let yourOrgId = this.args.currentUser.get('organization').get('id');
     usersWithOrgs = usersWithOrgs.filterBy('organization.id', yourOrgId);
     let orgUsersNotYou = usersWithOrgs.rejectBy(
       'username',
-      this.get('currentUser.username')
+      this.args.currentUser.username
     );
     return orgUsersNotYou.sortBy('createDate').reverse();
-  }),
+  }
 
-  actions: {
-    showDeletedUsers: function () {
-      this.set('showDeletedUsers', !this.showDeletedUsers);
-    },
-  },
-});
+  @action
+  toggleDeletedUsers() {
+    this.showDeletedUsers = !this.showDeletedUsers;
+  }
+}
