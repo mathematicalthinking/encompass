5c7bc2bc06c81095d84a8ccb7c8c9920dade8aa2
iamtimleonard <timleonardcello@gmail.com>
2021-08-18 10:30:38 -0400

    display keywords

--- app/routes/problems/problem.js
+++ app/routes/problems/problem.js
@@ -1,6 +1,5 @@
 import AuthenticatedRoute from '../_authenticated_route';
 import { hash } from 'rsvp';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class ProblemsProblemRoute extends AuthenticatedRoute {
