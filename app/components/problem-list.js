0e7824ff79652e63dabbd522fae2d1f31df789d1
iamtimleonard <timleonardcello@gmail.com>
2021-08-18 17:07:34 -0400

    problem-list to glimmer component

--- app/components/problem-list.js
+++ app/components/problem-list.js
@@ -1,37 +1,11 @@
 import { alias } from '@ember/object/computed';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+export default class ProblemLIstComponent extends Component {
+  @tracked dataLoadErrors = [];
 
-
-
-
-
-
-export default Component.extend({
-  elementId: 'problem-list',
-  classNames: ['problem-list', 'left-list'],
-  containerData: alias("parentView"),
-  containerActions: alias("parentView.actions"),
-
-  dataLoadErrors: [],
-
-
-  didReceiveAttrs: function () {
-    // ['problems', 'metadata', 'currentUser']
-    // set initial results from route
-  },
-
-  init: function () {
-    this._super(...arguments);
-  },
-  actions: {
-    toProblemInfo(problem) {
-      this.sendAction("toProblemInfo", problem);
-    },
-    refreshList() {
-      this.refreshList();
-    }
+  @action refreshList() {
+    this.args.refreshList();
   }
-
-
-});
-
+}
