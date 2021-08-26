ae73e4ea0705c9b5600fccbae86b2a46a24b7a49
iamtimleonard <timleonardcello@gmail.com>
2021-08-26 16:29:41 -0400

    simplify confirm route fix test urls

--- app/routes/auth/confirm.js
+++ app/routes/auth/confirm.js
@@ -1,15 +1,7 @@
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
 
 export default class ConfirmRoute extends Route {
   model(params) {
     return params.token;
-  }
-  renderTemplate() {
-    this.render('auth/confirm');
-  }
-
-  @action toHome() {
-    window.location.href = '/';
   }
 }
