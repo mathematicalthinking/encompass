f0a8dc4fcd1eac5f6496faa0a747edc0e2ba4592
iamtimleonard <timleonardcello@gmail.com>
2021-09-10 13:37:47 -0400

    radio-filter to glimmer

--- app/components/radio-filter.js
+++ app/components/radio-filter.js
@@ -1,23 +1,14 @@
-/*global _:false */
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import _ from 'underscore';
 
-export default Component.extend({
-  classNames: ['radio-filter'],
-
-  didReceiveAttrs() {
-    this._super(...arguments);
-  },
-
-  isSelected: computed('groupValue', function () {
-    let groupValue = this.groupValue;
-    let ownValue = this.inputValue;
+export default class RadioFilterComponent extends Component {
+  get isSelected() {
+    let groupValue = this.args.groupValue;
+    let ownValue = this.args.inputValue;
     return _.isEqual(groupValue, ownValue);
-  }),
-
-  actions: {
-    onClick(val) {
-      this.onClick(val);
-    },
-  },
-});
+  }
+  @action onClick(val) {
+    this.args.onClick(val);
+  }
+}
