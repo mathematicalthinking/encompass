7c41f4356f756a4b4cac5115f3a02a2cdc1127a2
iamtimleonard <timleonardcello@gmail.com>
2021-09-15 13:25:00 -0400

    switch to collapsible list in user list

--- app/components/collapsible-list.js
+++ app/components/collapsible-list.js
@@ -1,4 +1,10 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CollapsibleListComponent extends Component {
+  @tracked isOpen = true;
+  @action toggleIsOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
