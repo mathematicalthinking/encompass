6b774f4f74fc4662159efa7c421b3a0af4f2ff1e
iamtimleonard <timleonardcello@gmail.com>
2021-10-06 16:00:52 -0400

    Revert "Revert "make copy of selections for group workspaces""
    This reverts commit 99f50941bd3220eaff22a7220d6351519edcb55d.

--- app_server/datasource/api/selectionApi.js
+++ app_server/datasource/api/selectionApi.js
@@ -1,38 +1,39 @@
 /**
-  * # Selections API
-  * @description This is the API for selection based requests
-  * @author Damola Mabogunje, Daniel Kelly
-  * @since 1.0.0
-  */
+ * # Selections API
+ * @description This is the API for selection based requests
+ * @author Damola Mabogunje, Daniel Kelly
+ * @since 1.0.0
+ */
 
 //REQUIRE MODULES
-const logger   = require('log4js').getLogger('server');
+const logger = require('log4js').getLogger('server');
 const sharp = require('sharp');
 const axios = require('axios');
+const _ = require('underscore');
 
 //REQUIRE FILES
-const utils    = require('../../middleware/requestHandler');
+const utils = require('../../middleware/requestHandler');
 const userAuth = require('../../middleware/userAuth');
-const models   = require('../schemas');
-const wsAccess   = require('../../middleware/access/workspaces');
+const models = require('../schemas');
+const wsAccess = require('../../middleware/access/workspaces');
 const access = require('../../middleware/access/selections');
 
-const { isValidMongoId } = require('../../utils/mongoose');
+const { isValidMongoId, areObjectIdsEqual } = require('../../utils/mongoose');
 
 module.exports.get = {};
 module.exports.post = {};
 module.exports.put = {};
 
 /**
-  * @public
-  * @method getSelections
-  * @description __URL__: /api/selections
-  * @see [buildCriteria](../../middleware/requestHandler.html)
-  * @returns {Object} A 'named' array of selection objects: according to specified criteria
-  * @throws {NotAuthorizedError} User has inadequate permissions
-  * @throws {InternalError} Data retrieval failed
-  * @throws {RestError} Something? went wrong
-  */
+ * @public
+ * @method getSelections
+ * @description __URL__: /api/selections
+ * @see [buildCriteria](../../middleware/requestHandler.html)
+ * @returns {Object} A 'named' array of selection objects: according to specified criteria
+ * @throws {NotAuthorizedError} User has inadequate permissions
+ * @throws {InternalError} Data retrieval failed
+ * @throws {RestError} Something? went wrong
+ */
 async function getSelections(req, res, next) {
   var user = userAuth.requireUser(req);
 
@@ -43,62 +44,68 @@ async function getSelections(req, res, next) {
   let criteria;
   try {
     criteria = await access.get.selections(user, ids);
-
-  }catch(err) {
+  } catch (err) {
     console.error(`Error building selections criteria: ${err}`);
     console.trace();
     return utils.sendError.InternalError(null, res);
   }
 
-
-  models.Selection.find(criteria)
-    .exec(function(err, selections) {
-      if(err) {
-        logger.error(err);
-        return utils.sendError.InternalError(err, res);
-      }
-      var data = {'selections': selections};
-      utils.sendResponse(res, data);
-    });
+  models.Selection.find(criteria).exec(function (err, selections) {
+    if (err) {
+      logger.error(err);
+      return utils.sendError.InternalError(err, res);
+    }
+    var data = { selections: selections };
+    utils.sendResponse(res, data);
+  });
 }
 
 /**
-  * @public
-  * @method getSelection
-  * @description __URL__: /api/selections/:id
-  * @returns {Object} A 'named' selection object
-  * @throws {NotAuthorizedError} User has inadequate permissions
-  * @throws {InternalError} Data retrieval failed
-  * @throws {RestError} Something? went wrong
-  */
+ * @public
+ * @method getSelection
+ * @description __URL__: /api/selections/:id
+ * @returns {Object} A 'named' selection object
+ * @throws {NotAuthorizedError} User has inadequate permissions
+ * @throws {InternalError} Data retrieval failed
+ * @throws {RestError} Something? went wrong
+ */
 async function getSelection(req, res, next) {
   try {
     const user = userAuth.requireUser(req);
 
     if (!user) {
-      return utils.sendError.InvalidCredentialsError('You must be logged in.', res);
+      return utils.sendError.InvalidCredentialsError(
+        'You must be logged in.',
+        res
+      );
     }
 
     let id = req.params.id;
 
     let selection = await models.Selection.findById(id);
 
-    if (!selection || selection.isTrashed) { // record not found in db
+    if (!selection || selection.isTrashed) {
+      // record not found in db
       return utils.sendResponse(res, null);
     }
 
     let canLoadSelection = await access.get.selection(user, id);
 
-    if (!canLoadSelection) { // user does not have permission to access selection
-      return utils.sendError.NotAuthorizedError('You do not have permission.', res);
+    if (!canLoadSelection) {
+      // user does not have permission to access selection
+      return utils.sendError.NotAuthorizedError(
+        'You do not have permission.',
+        res
+      );
     }
 
-    const data = { // user has permission; send back record
-      selection
+    const data = {
+      // user has permission; send back record
+      selection,
     };
 
     return utils.sendResponse(res, data);
-  }catch(err) {
+  } catch (err) {
     console.error(`Error getSelection: ${err}`);
     console.trace();
     return utils.sendError.InternalError(null, res);
@@ -109,7 +116,7 @@ function getImageData(src) {
   if (typeof src !== 'string') {
     return;
   }
-  let first30Chars = src.slice(0,29);
+  let first30Chars = src.slice(0, 29);
 
   let target = 'base64,';
   let targetIndex = first30Chars.indexOf(target);
@@ -138,17 +145,18 @@ function getImageData(src) {
   if (!isValidMongoId(imageId)) {
     // try getting data from src url
     try {
-      return axios.get(src, {responseType: 'stream'})
-      .then((results) => {
+      return axios.get(src, { responseType: 'stream' }).then((results) => {
         const converter = sharp();
         return results.data.pipe(converter);
       });
-    } catch(err) {
+    } catch (err) {
       console.log('sharp err: ', err);
       return;
     }
   }
-  return models.Image.findById(imageId).lean().exec()
+  return models.Image.findById(imageId)
+    .lean()
+    .exec()
     .then((image) => {
       if (image) {
         let imageData = image.imageData;
@@ -159,33 +167,41 @@ function getImageData(src) {
         }
       }
     });
-
 }
 
 /**
-  * @public
-  * @method postSelection
-  * @description __URL__: /api/selections
-  * @throws {NotAuthorizedError} User has inadequate permissions
-  * @throws {InternalError} Data retrieval failed
-  * @throws {RestError} Something? went wrong
-  */
+ * @public
+ * @method postSelection
+ * @description __URL__: /api/selections
+ * @throws {NotAuthorizedError} User has inadequate permissions
+ * @throws {InternalError} Data retrieval failed
+ * @throws {RestError} Something? went wrong
+ */
 async function postSelection(req, res, next) {
   try {
     let user = userAuth.requireUser(req);
     let workspaceId = req.body.selection.workspace;
 
-    let ws = await models.Workspace.findById(workspaceId).lean().populate('owner').populate('editors').populate('createdBy').lean().populate('submissions').exec();
+    let ws = await models.Workspace.findById(workspaceId)
+      .lean()
+      .populate('owner')
+      .populate('editors')
+      .populate('createdBy')
+      .lean()
+      .populate('submissions')
+      .exec();
 
     if (!wsAccess.canModify(user, ws, 'selections', 2)) {
-      return utils.sendError.NotAuthorizedError('You don\'t have permission for this workspace', res);
+      return utils.sendError.NotAuthorizedError(
+        "You don't have permission for this workspace",
+        res
+      );
     }
 
     let selection = new models.Selection(req.body.selection);
 
     selection.createdBy = user;
     selection.createDate = Date.now();
-
 
     let coordinates = selection.coordinates;
     let splitCoords = coordinates.split(' ');
@@ -225,7 +241,12 @@ async function postSelection(req, res, next) {
         let croppedHeight = Math.floor(relativeSize.heightPct * height);
 
         let extraction = await sharp(bytes)
-          .extract({left: croppedLeft, top: croppedTop, width: croppedWidth, height: croppedHeight })
+          .extract({
+            left: croppedLeft,
+            top: croppedTop,
+            width: croppedWidth,
+            height: croppedHeight,
+          })
           .toBuffer();
 
         let newMetadata = await sharp(extraction).metadata();
@@ -239,7 +260,7 @@ async function postSelection(req, res, next) {
           mimetype: `image/${newMetadata.format}`,
           imageData: `data:image/${newMetadata.format};base64,${croppedImageData}`,
           createdBy: user,
-          createDate: Date.now()
+          createDate: Date.now(),
         });
 
         await croppedImage.save();
@@ -251,31 +272,27 @@ async function postSelection(req, res, next) {
         // something went wrong
         return utils.sendError.InvalidContentError(null, res);
       }
-
     }
 
     let savedSelection = await selection.save();
 
-    let data = {'selection': savedSelection};
+    let data = { selection: savedSelection };
     utils.sendResponse(res, data);
-
-  }catch(err) {
+  } catch (err) {
     console.error(`Error postSelection: ${err}`);
     console.trace();
     return utils.sendError.InternalError(err, res);
-
   }
-
 }
 
 /**
-  * @public
-  * @method putSelection
-  * @description __URL__: /api/selections
-  * @throws {NotAuthorizedError} User has inadequate permissions
-  * @throws {InternalError} Data retrieval failed
-  * @throws {RestError} Something? went wrong
-  */
+ * @public
+ * @method putSelection
+ * @description __URL__: /api/selections
+ * @throws {NotAuthorizedError} User has inadequate permissions
+ * @throws {InternalError} Data retrieval failed
+ * @throws {RestError} Something? went wrong
+ */
 async function putSelection(req, res, next) {
   try {
     let user = userAuth.requireUser(req);
@@ -287,12 +304,14 @@ async function putSelection(req, res, next) {
     let selection = await models.Selection.findById(req.params.id);
 
     if (!selection) {
-      logger.info(`${user.username} attempted to update nonexistant selection with id ${req.params.id}`);
+      logger.info(
+        `${user.username} attempted to update nonexistant selection with id ${req.params.id}`
+      );
       return utils.sendResponse(res, null);
     }
 
-    for(let field in req.body.selection) {
-      if((field !== '_id') && (field !== undefined)) {
+    for (let field in req.body.selection) {
+      if (field !== '_id' && field !== undefined) {
         selection[field] = req.body.selection[field];
       }
     }
@@ -304,14 +323,56 @@ async function putSelection(req, res, next) {
 
     let data = { selection };
     utils.sendResponse(res, data);
-
-  }catch(err) {
+  } catch (err) {
     logger.error(err);
     return utils.sendError.InternalError(err, res);
   }
 }
 
+const resolveGroupWorkspaces = async (selection) => {
+  const sourceWorkspace = await models.Workspace.findById(selection.workspace);
+  //this is only supposed to add selections to group workspaces from individual workspaces
+  if (sourceWorkspace.group || sourceWorkspace.workspaceType === 'parent') {
+    return;
+  }
+  const targetGroups = await models.Group.find({
+    students: sourceWorkspace.owner,
+  });
+  //find the group workspaces
+  const targetWorkspaces = await models.Workspace.find({
+    group: targetGroups,
+    isTrashed: false,
+    linkedAssignment: sourceWorkspace.linkedAssignment,
+  });
+  if (!targetWorkspaces.length) {
+    return;
+  }
+  //add a copy of the selection to each of those workspaces
+  const createdSelections = await Promise.all(
+    targetWorkspaces.map(async (workspace) => {
+      await selection.populate('submission').execPopulate();
+      await workspace.populate('submissions').execPopulate();
+      let childPlain = selection.toObject();
+      let fieldsToOmit = ['_id', 'workspace', 'comments', 'taggings'];
+      let childCopy = _.omit(childPlain, fieldsToOmit);
+      childCopy.originalSelection = selection._id;
+      let parentSub = _.find(workspace.submissions, (popSubmission) => {
+        return areObjectIdsEqual(
+          popSubmission.answer,
+          childCopy.submission.answer
+        );
+      });
+      childCopy.submission = parentSub._id;
+      childCopy.workspace = workspace._id;
+      const newSelection = await models.Selection.create(childCopy);
+      return newSelection;
+    })
+  );
+  return createdSelections;
+};
+
 module.exports.get.selections = getSelections;
 module.exports.get.selection = getSelection;
 module.exports.post.selection = postSelection;
 module.exports.put.selection = putSelection;
+module.exports.resolveGroupWorkspaces = resolveGroupWorkspaces;
