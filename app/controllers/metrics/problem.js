e058be7eb7a2cbbf0cb155bd07d70b08156511f7
iamtimleonard <timleonardcello@gmail.com>
2021-10-18 15:38:05 -0400

    simplify problem template

--- app/controllers/metrics/problem.js
+++ app/controllers/metrics/problem.js
@@ -3,11 +3,13 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class MetricsProblemController extends Controller {
-  @tracked showWorkspaces = false;
+  @tracked showTable = false;
   @tracked showProblemText = false;
   @tracked showAnswers = false;
-  @tracked relevantWorkspaces = [];
+  // @tracked relevantWorkspaces = [];
   @tracked problemAnswers = [];
+  @tracked currentTableColumns = this.workspacesHead;
+  @tracked currentTableRows = this.model.workspacesRows;
   workspacesHead = [
     { name: 'Name', valuePath: 'name' },
     { name: 'Owner', valuePath: 'owner' },
@@ -23,31 +25,31 @@ export default class MetricsProblemController extends Controller {
   ];
   @action toggleProblemText() {
     this.showProblemText = !this.showProblemText;
-    this.showWorkspaces = false;
-    this.showAnswers = false;
+    this.showTable = false;
   }
   @action findWorkspaces() {
-    this.showAnswers = false;
-    this.showWorkspaces = true;
-    this.showProblemText = false;
+    this.showTable = true;
+    this.currentTableColumns = this.workspacesHead;
+    this.currentTableRows = this.model.workspacesRows;
   }
-  @action findWorkspaces2() {
-    this.store
-      .query('workspace', {
-        filterBy: {
-          'submissionSet.criteria.puzzle.puzzleId': this.model.puzzleId,
-        },
-      })
-      .then((res) => {
-        this.relevantWorkspaces = res;
-        this.showAnswers = false;
-        this.showWorkspaces = true;
-        this.showProblemText = false;
-      });
-  }
+  // unsure if there are enough workspaces to need this
+  // @action findWorkspaces2() {
+  //   this.store
+  //     .query('workspace', {
+  //       filterBy: {
+  //         'submissionSet.criteria.puzzle.puzzleId': this.model.puzzleId,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       this.relevantWorkspaces = res;
+  //       this.showAnswers = false;
+  //       this.showWorkspaces = true;
+  //       this.showProblemText = false;
+  //     });
+  // }
   @action findSubmissions() {
-    this.showAnswers = true;
-    this.showWorkspaces = false;
-    this.showProblemText = false;
+    this.showTable = true;
+    this.currentTableColumns = this.answersHead;
+    this.currentTableRows = this.model.answersRows;
   }
 }
