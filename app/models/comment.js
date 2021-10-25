926c0c22c88d24a37f7112312b154218f0c35e59
iamtimleonard <timleonardcello@gmail.com>
2021-10-25 09:19:54 -0400

    revert comment model

--- app/models/comment.js
+++ app/models/comment.js
@@ -9,7 +9,7 @@ export default class CommentModel extends Auditable {
   @belongsTo('comment', { inverse: 'ancestors', async: true }) origin;
   @hasMany('comment', { inverse: 'origin', async: true }) ancestors;
   @belongsTo('comment', { inverse: 'children', async: true }) parent;
-  // @hasMany('comment', { inverse: 'parent', async: true }) children;
+  @hasMany('comment', { inverse: 'parent', async: true }) children;
   @belongsTo('submission', { async: true }) submission;
   @belongsTo('workspace') workspace;
   relevance = 0; // Used for sorting (gets set by controller)
