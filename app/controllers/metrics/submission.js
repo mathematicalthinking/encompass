eb3bbd3ea82814b3d17990bcdd3096123c8055cd
iamtimleonard <timleonardcello@gmail.com>
2021-10-25 12:41:17 -0400

    "reorder submission table
    - show text to the left
    - include problem text "

--- app/controllers/metrics/submission.js
+++ app/controllers/metrics/submission.js
@@ -17,9 +17,9 @@ export default class MetricsSubmissionController extends Controller {
   submissionsColumns = [
     { name: 'Record', valuePath: 'constructor.modelName' },
     { name: 'Workspace', valuePath: 'workspaces.firstObject.name' },
+    { name: 'Text', valuePath: 'text' },
     { name: 'Folder Name', valuePath: 'folder.name' },
     { name: 'Creator', valuePath: 'createdBy.username' },
     { name: 'Comment Type', valuePath: 'label' },
-    { name: 'Text', valuePath: 'text' },
   ];
 }
