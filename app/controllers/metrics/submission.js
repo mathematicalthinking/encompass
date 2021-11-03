392338786ec06fe27b512688c59e4b5944a49e3c
iamtimleonard <timleonardcello@gmail.com>
2021-11-03 09:54:00 -0400

    move metrics.submission logic to controller
    - construct rows in getter
    - no modifying documents directly

--- app/controllers/metrics/submission.js
+++ app/controllers/metrics/submission.js
@@ -15,11 +15,48 @@ export default class MetricsSubmissionController extends Controller {
     return this.selectedWorkspaces.pushObject(workspace);
   }
   submissionsColumns = [
-    { name: 'Record', valuePath: 'constructor.modelName' },
-    { name: 'Workspace', valuePath: 'workspaces.firstObject.name' },
-    { name: 'Text', valuePath: 'text' },
-    { name: 'Folder Name', valuePath: 'folder.name' },
-    { name: 'Creator', valuePath: 'createdBy.displayName' },
-    { name: 'Comment Type', valuePath: 'label' },
+    { name: 'Record', valuePath: 'modelName' },
+    { name: 'Workspace', valuePath: 'workspaceName' },
+    { name: 'Text/Folder Name', valuePath: 'text' },
+    // { name: 'Folder Name', valuePath: 'folder.name' },
+    { name: 'Creator', valuePath: 'creator.displayName' },
+    // { name: 'Comment Type', valuePath: 'label' },
   ];
+  get tableRows() {
+    return this.selectedWorkspaces.map((workspace) => {
+      const selections = workspace.get('selections');
+      const selectionsArray = selections.map((selection) => {
+        const folders = selection.get('folders').toArray();
+        const comments = selection.get('comments').toArray();
+        const foldersArray = folders.map((folder) => {
+          return {
+            modelName: 'folder',
+            text: folder.get('name'),
+            creator: folder.get('createdBy'),
+          };
+        });
+        const commentsArray = comments.map((comment) => {
+          return {
+            modelName: comment.constructor.modelName,
+            text: comment.text,
+            creator: comment.get('createdBy'),
+          };
+        });
+        return {
+          modelName: selection.constructor.modelName,
+          text: selection.text,
+          creator: selection.get('createdBy'),
+          children: [...foldersArray, ...commentsArray],
+        };
+      });
+      return {
+        modelName: workspace.constructor.modelName,
+        workspaceName: workspace.get('workspaces.firstObject.name'),
+        text: 'See Above',
+        creator: workspace.get('createdBy'),
+        children: selectionsArray,
+        id: workspace.id,
+      };
+    });
+  }
 }
--- app/routes/metrics/submission.js
+++ app/routes/metrics/submission.js
@@ -11,44 +11,10 @@ export default class MetricsSubmissionRoute extends Route {
       findByAnswer: true,
       answer,
     });
-    allSubmissions = await allSubmissions.toArray();
-    allSubmissions.forEach(async (submission) => {
-      submission.text = '';
-      const selections = await submission.selections;
-      selections.forEach(async (selection) => {
-        let comments = await selection.comments;
-        let commentsToDisplay = comments.map((comment) => {
-          return {
-            text: comment.text,
-            constructor: {
-              modelName: comment.constructor.modelName,
-            },
-            createdBy: {
-              displayName: comment.get('createdBy.displayName'),
-            },
-            label: comment.label,
-          };
-        });
-        let folders = await selection.get('taggings');
-        let foldersToDisplay = folders.map((folder) => {
-          return {
-            name: folder.name,
-            constuctor: {
-              modelName: 'folder',
-            },
-            createdBy: {
-              displayName: folder.get('createdBy.displayName'),
-            },
-          };
-        });
-        selection.children = [...foldersToDisplay, ...commentsToDisplay];
-      });
-      submission.children = selections.toArray();
-    });
     return hash({
       submission,
       answer,
-      allSubmissions,
+      allSubmissions: allSubmissions.toArray(),
     });
   }
   resetController(controller, isExiting, transition) {
