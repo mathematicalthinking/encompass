4eaa299adbf08b165168fe5cae74d209e1aab633
iamtimleonard <timleonardcello@gmail.com>
2021-11-19 11:27:36 -0500

    handle errors in json-csv
    - allow user to choose data to send to getter
    - limit long strings to 100 chars

--- app/services/json-csv.js
+++ app/services/json-csv.js
@@ -2,17 +2,24 @@ import Service from '@ember/service';
 
 export default class JsonCsvService extends Service {
   arrayToCsv(array) {
-    const keys = [Object.keys(array[0])].concat(array);
-    const res = keys
-      .map((row) => {
-        return Object.values(row)
-          .map((value) => {
-            return typeof value === 'string' ? JSON.stringify(value) : value;
-          })
-          .toString();
-      })
-      .join('\n');
-    console.log(res);
-    return res;
+    try {
+      const keys = [Object.keys(array[0])].concat(array);
+      const res = keys
+        .map((row) => {
+          return Object.values(row)
+            .map((value) => {
+              return typeof value === 'string'
+                ? JSON.stringify(value).replace(/[#,]/gm, '').slice(0, 100)
+                : value;
+            })
+            .toString();
+        })
+        .join('\n');
+      console.log(res);
+      return res;
+    } catch (err) {
+      console.log(err);
+      return 'data invalid';
+    }
   }
 }
