655ba0090d1f1bcf495ec6bd7701eef6002ab8fe
iamtimleonard <timleonardcello@gmail.com>
2021-12-01 10:39:05 -0500

    convert workspace-info to glimmer

--- app/components/workspace-info.js
+++ app/components/workspace-info.js
@@ -1,28 +1,28 @@
-import Component from '@ember/component';
+import ErrorHandlingComponent from './error-handling';
+import { tracked } from '@glimmer/tracking';
+import _ from 'underscore';
 // import { inject as controller } from '@ember/controller';
-import { computed } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import ErrorHandlingMixin from '../mixins/error_handling_mixin';
 
-export default Component.extend(ErrorHandlingMixin, {
-  currentUser: service('current-user'),
-  elementId: 'workspace-info',
+export default class WorkspaceInfoComponent extends ErrorHandlingComponent {
+  @service('current-user') currentUser;
   // comments: controller(),
-  alert: service('sweet-alert'),
-  store: service(),
-  permissions: service('workspace-permissions'),
-  utils: service('utility-methods'),
-  updateRecordErrors: [],
-  isShowingCustomViewer: false,
-  customSubmissionIds: [],
-  didReceiveAttrs() {
-    this._super(...arguments);
+  @service('sweet-alert') alert;
+  @service store;
+  @service('workspace-permissions') permissions;
+  @service('utility-methods') utils;
+  @tracked updateRecordErrors = [];
+  @tracked isShowingCustomViewer = false;
+  @tracked customSubmissionIds = [];
+  constructor() {
+    super(...arguments);
 
-    const collaborators = this.get('workspace.collaborators');
+    const collaborators = this.args.workspace.get('collaborators');
     // array of Ids, query for users;
 
     if (!this.utils.isNonEmptyArray(collaborators)) {
-      this.set('originalCollaborators', []);
+      this.originalCollaborators = [];
       return;
     }
 
@@ -31,39 +31,36 @@ export default Component.extend(ErrorHandlingMixin, {
         ids: collaborators,
       })
       .then((users) => {
-        this.set('originalCollaborators', users.toArray());
+        this.originalCollaborators = users.toArray();
       })
       .catch((err) => {
         this.handleErrors(err, 'queryErrors');
       });
-  },
+  }
 
-  canEdit: computed('workspace.id', function () {
-    let workspace = this.workspace;
-    let ownerId = workspace.get('owner.id');
+  get canEdit() {
+    let workspace = this.args.workspace;
+    // let ownerId = workspace.get('owner.id');
     let creatorId = workspace.get('createdBy.id');
     let currentUser = this.currentUser.user;
-    let accountType = currentUser.get('accountType');
+    let accountType = currentUser.accountType;
     let isAdmin = accountType === 'A';
-    let isOwner = ownerId === currentUser.id;
+    // let isOwner = ownerId === currentUser.id;
     let isCreator = creatorId === currentUser.id;
 
     return isAdmin || isCreator;
-  }),
+  }
 
-  canEditCollaborators: computed(
-    'workspace.feedbackAuthorizers.[]',
-    function () {
-      if (this.canEdit) {
-        return true;
-      }
-      return this.get('workspace.feedbackAuthorizers').includes(
-        this.get('currentUser.user.id')
-      );
+  get canEditCollaborators() {
+    if (this.canEdit) {
+      return true;
     }
-  ),
+    return this.args.workspace
+      .get('feedbackAuthorizers')
+      .includes(this.currentUser.user.id);
+  }
 
-  initialCollabOptions: computed('selectedCollaborators', function () {
+  get initialCollabOptions() {
     let peeked = this.store.peekAll('user');
     let collabs = this.selectedCollaborators;
 
@@ -79,66 +76,59 @@ export default Component.extend(ErrorHandlingMixin, {
         username: obj.get('username'),
       };
     });
-  }),
+  }
 
-  selectedCollaborators: computed(
-    'originalCollaborators.[]',
-    'workspace.owner.id',
-    function () {
-      let hash = {};
-      let wsOwnerId = this.get('workspace.owner.id');
+  get selectedCollaborators() {
+    let hash = {};
+    let wsOwnerId = this.args.workspace.get('owner.id');
 
-      // no reason to set owner as a collaborator
-      if (wsOwnerId) {
-        hash[wsOwnerId] = true;
-      }
-      const originalCollaborators = this.originalCollaborators;
+    // no reason to set owner as a collaborator
+    if (wsOwnerId) {
+      hash[wsOwnerId] = true;
+    }
+    const originalCollaborators = this.originalCollaborators;
 
-      if (!this.utils.isNonEmptyArray(originalCollaborators)) {
-        return hash;
-      }
-      originalCollaborators.forEach((user) => {
-        if (_.isString(user)) {
-          hash[user] = true;
-        } else if (_.isObject(user)) {
-          hash[user.get('id')] = true;
-        }
-      });
+    if (!this.utils.isNonEmptyArray(originalCollaborators)) {
       return hash;
     }
-  ),
+    originalCollaborators.forEach((user) => {
+      if (_.isString(user)) {
+        hash[user] = true;
+      } else if (_.isObject(user)) {
+        hash[user.get('id')] = true;
+      }
+    });
+    return hash;
+  }
 
-  actions: {
-    removeErrorString: function (arrayPropName, errorString) {
-      let errors = this.get(arrayPropName);
-      if (Array.isArray(errors)) {
-        errors.removeObject(errorString);
-      }
-    },
-    updateCustomSubs(id) {
-      if (!this.utils.isNonEmptyArray(this.customSubmissionIds)) {
-        this.set('customSubmissionIds', []);
-      }
-      const customSubmissionIds = this.customSubmissionIds;
+  @action removeErrorString(arrayPropName, errorString) {
+    let errors = this[arrayPropName];
+    if (Array.isArray(errors)) {
+      errors.removeObject(errorString);
+    }
+  }
+  @action updateCustomSubs(id) {
+    if (!this.utils.isNonEmptyArray(this.customSubmissionIds)) {
+      this.customSubmissionIds = [];
+    }
+    const customSubmissionIds = this.customSubmissionIds;
 
-      const isIn = customSubmissionIds.includes(id);
-      if (isIn) {
-        customSubmissionIds.removeObject(id);
-      } else {
-        customSubmissionIds.addObject(id);
-      }
-    },
-    selectAllSubmissions: function () {
-      this.set(
-        'customSubmissionIds',
-        this.get('workspace.submissions').mapBy('id')
-      );
-    },
-    deselectAllSubmissions: function () {
-      this.set('customSubmissionIds', []);
-    },
-    toggleIsShowingCustomViewer: function () {
-      this.set('isShowingCustomViewer', !this.isShowingCustomViewer);
-    },
-  },
-});
+    const isIn = customSubmissionIds.includes(id);
+    if (isIn) {
+      customSubmissionIds.removeObject(id);
+    } else {
+      customSubmissionIds.addObject(id);
+    }
+  }
+  @action selectAllSubmissions() {
+    this.customSubmissionIds = this.args.workspace
+      .get('submissions')
+      .mapBy('id');
+  }
+  @action deselectAllSubmissions() {
+    this.customSubmissionIds = [];
+  }
+  @action toggleIsShowingCustomViewer() {
+    this.isShowingCustomViewer = !this.isShowingCustomViewer;
+  }
+}
