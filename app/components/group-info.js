5a093487df52c101ee456c1cfb0ab62503bc5202
iamtimleonard <timleonardcello@gmail.com>
2021-12-02 12:29:08 -0500

    only show student inputs when editing group

--- app/components/group-info.js
+++ app/components/group-info.js
@@ -9,6 +9,13 @@ export default class GroupInfoComponent extends Component {
     return this.args.addGroup || this.displayStudents;
   }
   @tracked displayStudents = false;
+  @tracked updateGroup = false;
+  @action toggleUpdateGroup() {
+    this.updateGroup = !this.updateGroup;
+  }
+  get isUpdating() {
+    return this.args.addGroup && this.updateGroup;
+  }
   @action toggleDisplayStudents() {
     this.displayStudents = !this.displayStudents;
   }
