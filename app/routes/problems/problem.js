891876d10fb4b851444ef3e246e44aaa52175d75
iamtimleonard <timleonardcello@gmail.com>
2021-12-08 14:59:52 -0500

    bug: get flaggedBy user
    - move user fetch to route to stop inifinite loop
    - pass user as separate attr

--- app/routes/problems/problem.js
+++ app/routes/problems/problem.js
@@ -8,15 +8,23 @@ export default class ProblemsProblemRoute extends AuthenticatedRoute {
     const currentUser = this.modelFor('application');
     const userOrg = await currentUser.get('organization');
     const recommendedProblems = await userOrg.get('recommendedProblems');
-    const problem = this.store.findRecord('problem', params.problem_id);
-    const sectionList = this.store.findAll('section');
-    const orgList = this.store.findAll('organization');
+    const problem = await this.store.findRecord('problem', params.problem_id);
+    const sectionList = await this.store.findAll('section');
+    const orgList = await this.store.findAll('organization');
+    let flaggedBy;
+    if (problem.flagReason.flaggedBy) {
+      flaggedBy = await this.store.findRecord(
+        'user',
+        problem.flagReason.flaggedBy
+      );
+    }
     return hash({
       currentUser,
       problem,
       sectionList,
       orgList,
       recommendedProblems,
+      flaggedBy,
     });
   }
 }
