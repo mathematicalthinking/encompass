3a6001244cb75db2d13494aa08e4562dd035161e
iamtimleonard <timleonardcello@gmail.com>
2021-12-09 11:40:10 -0500

    fix parent workspace default name
    - fix spacing in template constructor
    - fix expected name in tests

--- app/components/parent-workspace-new.js
+++ app/components/parent-workspace-new.js
@@ -17,7 +17,7 @@ export default class ParentWorkspaceNewComponent extends Component {
   }
 
   get defaultName() {
-    let base = 'Parent Workspace: ';
+    let base = 'Parent Workspace:';
     let assignmentName = this.args.assignmentName || this.args.assignment.name;
 
     if (assignmentName) {
