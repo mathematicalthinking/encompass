3bb4a0881d93c19082d5d9ab57c11cd07a1abf95
iamtimleonard <timleonardcello@gmail.com>
2021-12-15 12:45:18 -0500

    export folders as csv

--- app/controllers/metrics/index.js
+++ app/controllers/metrics/index.js
@@ -20,7 +20,23 @@ export default class MetricsIndexController extends Controller {
       const assignmentsWorkspaces = assignments.map((assignment) =>
         assignment.get('linkedWorkspaces').toArray()
       );
-      return assignmentsWorkspaces;
+      const folders = assignmentsWorkspaces
+        .flat()
+        .map((workspace) => workspace.get('folders').toArray());
+      const taggings = folders
+        .flat()
+        .map((folder) => folder.get('taggings').toArray());
+      const formatted = taggings.flat().map((tagging) => {
+        return {
+          workspace: tagging.get('workspace.name'),
+          folder: tagging.get('folder.name'),
+          selection: tagging.get('selection.text'),
+          mentor: tagging.get('createdBy.username'),
+          student: tagging.get('selection.submission.student'),
+          actionDate: tagging.createDate,
+        };
+      });
+      return this.JsonCsv.arrayToCsv(formatted);
     }
     return '';
   }
