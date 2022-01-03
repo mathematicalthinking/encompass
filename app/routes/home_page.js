6b37bd7e1b1bc35518432507ee8850c5c1036b7a
iamtimleonard <timleonardcello@gmail.com>
2022-01-03 09:50:28 -0500

    remove home_page route

--- app/routes/home_page.js
+++
@@ -1,8 +0,0 @@
-import AuthenticatedRoute from './_authenticated_route';
-
-export default AuthenticatedRoute.extend({
-  model: function () {
-    let assignments = this.store.findAll('assignment');
-    return assignments;
-  },
-});
