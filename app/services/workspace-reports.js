553a413f3345dc15b8341198c7875ffbdb654ee6
Yousof Wakili <ywakili18@gmail.com>
2023-12-04 16:31:50 -0800

    concataced data in an array, all selections should show now for export data

--- app/services/workspace-reports.js
+++ app/services/workspace-reports.js
@@ -8,71 +8,64 @@ export default class WorkspaceReportsService extends Service {
 
   submissionReportCsv(model) {
     const submissionsArray = model.submissions.toArray();
+    let allRows = []; // This will store all the rows for the CSV file
+
     const sortedSubmissions = submissionsArray.sort((a, b) => {
       const dateA = new Date(a.createDate);
       const dateB = new Date(b.createDate);
       return dateA - dateB; // For descending order
     });
 
-    let maxRevisions = 0;
     sortedSubmissions.forEach((submission, index) => {
-      submission.submissionLabel =
-        index === 0 ? 'Original Submission' : `Revision: ${index}`;
-      if (index > maxRevisions) {
-        maxRevisions = index;
+      // Common data for all rows related to this workspace report
+      const commonData = {
+        'Name of workspace': submission.get('workspaces.firstObject.name'),
+        'Workspace URL': this.currentUrl.currentUrl,
+        'Workspace Owner': model.workspace.get('owner.username'),
+        'Original Submitter': submission.student,
+        'Text of Submission':
+          submission.shortAnswer || submission.get('answer.answer') || '',
+        'Date of Submission': moment(submission.createDate).format(
+          'MM/DD/YYYY'
+        ),
+        'Submission ID': submission.id,
+        'Submission or Revision':
+          index === 0 ? 'Original Submission' : `Revision: ${index}`,
+        'Number of Folders': model.workspace.foldersLength,
+        'Number of Notice/Wonder/Feedback': model.workspace.commentsLength,
+        'Submission Order': index + 1,
+      };
+
+      // If no selections, output the submission with empty selection details
+      if (!submission.get('selections.length')) {
+        allRows.push({
+          ...commonData,
+          'Selector of text': '',
+          'Text of Selection': '',
+          'Date of Selection': '',
+          'Original Annotator': '',
+          'Text of annotator': '',
+          'Date of annotation': '',
+        });
+      } else {
+        // For submissions with selections, output a row for each selection
+        submission.get('selections').forEach((selection) => {
+          const selectorInfo = this.createSelectorInfo(selection);
+          let selectionRow = {
+            ...commonData, // Spread common data
+            'Selector of text': selectorInfo.username,
+            'Text of Selection': selectorInfo.text,
+            'Date of Selection': selectorInfo.createDate,
+            'Original Annotator': selectorInfo.username,
+            'Text of annotator': selectorInfo.commentText,
+            'Date of annotation': selectorInfo.createDate,
+          };
+          allRows.push(selectionRow);
+        });
       }
     });
 
-    return sortedSubmissions.map((submission, index) => {
-      const text = `Summary: ${
-        submission.shortAnswer
-          ? submission.shortAnswer
-          : submission.get('answer.answer')
-      }  Full Answer: ${
-        submission.longAnswer
-          ? submission.longAnswer
-          : submission.get('answer.explanation')
-          ? submission.get('answer.explanation').replace(/<\/?[^>]+(>|$)/g, '')
-          : ''
-      }`;
-
-      const workspaceUrl = this.currentUrl.currentUrl;
-      const workspace = submission.get('workspaces.firstObject.name');
-      const submitter = submission.student;
-      const workspaceOwner = model.workspace.get('owner.username');
-      const submissionNumber = index + 1;
-      const submissionId = submission.id;
-      const foldersLength = model.workspace.foldersLength;
-      const commentsLength = model.workspace.commentsLength;
-      const dateOfSubmission = moment(submission.createDate).format(
-        'MM/DD/YYYY'
-      );
-
-      let selectorInfo = {};
-      submission.get('selections').map((selection) => {
-        selectorInfo = this.createSelectorInfo(selection);
-      });
-
-      return {
-        'Name of workspace': workspace,
-        'Workspace URL': workspaceUrl,
-        'Workspace Owner': workspaceOwner,
-        'Original Submitter': submitter,
-        'Text of Submission': text,
-        'Date of Submission': dateOfSubmission,
-        'Submission ID': submissionId,
-        'Submission or Revision': submission.submissionLabel,
-        'Selector of text': selectorInfo.username,
-        'Text of Selection': selectorInfo.text,
-        'Date of Selection': selectorInfo.createDate,
-        'Original Annotator': selectorInfo.username,
-        'Text of annotator': selectorInfo.commentText,
-        'Date of annotation': selectorInfo.createDate,
-        'Number of Folders': foldersLength,
-        'Number of Notice/Wonder/Feedback': commentsLength,
-        'Submission Order': submissionNumber,
-      };
-    });
+    return allRows;
   }
 
   createSelectorInfo(selector) {
@@ -83,15 +76,21 @@ export default class WorkspaceReportsService extends Service {
       commentText: '',
     };
 
-    if (!selector) return defaultSelection;
+    if (!selector) {
+      return defaultSelection;
+    }
 
     const createDate = moment(selector.get('createDate')).format('MM/DD/YYYY');
     const text = selector.get('text');
     const username = selector.get('createdBy.username');
     const commentText = selector.get('comments.firstObject.text');
 
-    const selectorInfo = { createDate, text, username, commentText };
-    return Object.assign({}, defaultSelection, selectorInfo);
+    return {
+      createDate: createDate,
+      text: text,
+      username: username,
+      commentText: commentText,
+    };
   }
 
   generateRevisionFields(submissionLabel, maxRevisions) {
