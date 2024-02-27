0111aa9667ae899278b098172aa551ba5a13b522
Yousof Wakili <yousofwakili@Yousofs-MacBook-Pro.local>
2024-02-27 13:37:24 -0800

    Added getters for rendering new responses, mentor replies, margin fixes

--- app/components/response-mentor-thread.js
+++ app/components/response-mentor-thread.js
@@ -3,11 +3,20 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 export default class ResponseMentorThreadComponent extends Component {
   // to have responses be collapsed at first
-  @tracked showResponses = false;
+  @tracked show = false;
 
   // action to toggle the showResponses property
+  get showResponsesText() {
+    return this.showResponses ? 'Hide responses' : 'Show responses';
+  }
+  get showResponses() {
+    return this.show || this.args.isCreating;
+  }
+  get showButton() {
+    return !this.args.isCreating;
+  }
   @action
   toggleShowResponses() {
-    this.showResponses = !this.showResponses;
+    this.show = !this.show;
   }
 }
