154ed4beba6b37e0589f4f52b8d889604097cb7f
Yousof Wakili <yousofwakili@Yousofs-MacBook-Pro.local>
2024-04-17 14:02:49 -0700

    all mentor responses will show instead of being hidden

--- app/components/response-mentor-thread.js
+++ app/components/response-mentor-thread.js
@@ -1,9 +1,17 @@
+/**
+ * #
+ * @description 
+  Class component file to response-mentor-thread component. Purpose is to show responses for a mentor thread.
+ * @author Yousof Wakili
+ * @since 3.6.2
+ */
+
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class ResponseMentorThreadComponent extends Component {
-  @tracked show = false;
+  @tracked show = true;
 
   get showResponsesText() {
     return this.showResponses
