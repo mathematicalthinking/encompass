76c239b3c162b270580335af7f27c66f51c94ee3
Yousof Wakili <yousofwakili@Yousofs-MacBook-Pro.local>
2024-07-31 22:03:23 -0700

    workspace.js controller migrated

--- app/controllers/workspace.js
+++ app/controllers/workspace.js
@@ -2,25 +2,30 @@
  * # Workspace Controller
  * @description This controller for the workspace assists in linking between submissions
  * @todo Linking between submissions should really be moved to workspace_submissions_index_controller
- * @author Amir Tahvildaran <amir@mathforum.org>, Damola Mabogunje <damola@mathforum.org>
  * @since 1.0.0
  */
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Controller.extend({
-  // comments: controller(),
+export default class WorkspaceController extends Controller {
+  // comments: controller();
 
-  currentSelection: null, //ENC-397, ENC-398
+  @tracked currentSelection = null; //ENC-397, ENC-398
 
-  showOverlay: computed('makingSelection', 'taggingSelection', function () {
+  @computed('makingSelection', 'taggingSelection')
+  get showOverlay() {
     return this.makingSelection || this.taggingSelection;
-  }),
+  }
 
-  actions: {
-    popupMaskClicked: function () {
-      this.transitionToRoute('workspace.submission', this.currentSubmission);
-    },
-    tagSelection: function (selection, tags) {},
-  },
-});
+  @action
+  popupMaskClicked() {
+    this.transitionToRoute('workspace.submission', this.currentSubmission);
+  }
+  // Was this ever used? If not, remove it.
+  // @action
+  // tagSelection(selection, tags) {
+  //   // Your implementation here
+  // }
+}
