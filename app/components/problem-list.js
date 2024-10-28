f28bdc21db0edde185e5781511861524c3d16771
Irv Katz <60225276+exidy80@users.noreply.github.com>
2024-10-27 20:00:30 -0400

    fixed typos

--- app/components/problem-list.js
+++ app/components/problem-list.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-export default class ProblemLIstComponent extends Component {
+export default class ProblemListComponent extends Component {
   @tracked dataLoadErrors = [];
 
   @action refreshList() {
--- app/components/problem-new.js
+++ app/components/problem-new.js
@@ -1,6 +1,6 @@
 import ErrorHandlingComponent from './error-handling';
 import { tracked } from '@glimmer/tracking';
-import { observer, action } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import _ from 'underscore';
 import $ from 'jquery';
