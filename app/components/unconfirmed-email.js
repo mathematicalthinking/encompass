05b84efa21b0aafda1eefe9aeaedade1a627b7a0
Irv Katz <60225276+exidy80@users.noreply.github.com>
2024-11-19 19:37:04 -0500

    Move upgraded component templates to app/components; delete unused components

--- app/components/unconfirmed-email.js
+++ app/components/unconfirmed-email.js
@@ -1,22 +1,33 @@
 import $ from 'jquery';
-import Component from '@ember/component';
-import CurrentUserMixin from '../mixins/current_user_mixin';
+import Component from '@ember/glimmer';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
-export default Component.extend(CurrentUserMixin, {
-  elementId: ['unconfirmed-page'],
-  emailErrors: [],
-  errorHandling: service('error-handling'),
-  actions: {
-    sendEmail: function () {
-      $.get('/auth/resend/confirm')
-        .then((res) => {
-          if (res.isSuccess) {
-            this.set('emailSuccess', true);
-          }
-        })
-        .catch((err) => {
-          this.errorHandling.handleErrors(err, 'emailErrors');
-        });
-    },
-  },
-});
+export default class UnconfirmedEmailComponent extends Component {
+  @service errorHandling;
+  @service currentUser;
+  @tracked emailErrors = [];
+  @tracked emailSuccess = false;
+
+  get displayName() {
+    return this.currentUser.user.displayName;
+  }
+
+  get email() {
+    return this.currentUser.user.email;
+  }
+
+  @action
+  sendEmail() {
+    $.get('/auth/resend/confirm')
+      .then((res) => {
+        if (res.isSuccess) {
+          this.emailSuccess = true;
+        }
+      })
+      .catch((err) => {
+        this.errorHandling.handleErrors(err, 'emailErrors');
+      });
+  }
+}
