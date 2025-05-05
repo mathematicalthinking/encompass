971a74df5980389adb3d159a749a8c1d022c3034
Irv Katz <60225276+exidy80@users.noreply.github.com>
2025-05-05 15:35:44 -0400

    typeahead upgrade

--- app/components/ui/twitter-typeahead.js
+++ app/components/ui/twitter-typeahead.js
@@ -1,221 +1,79 @@
-import Ember from 'ember';
-import 'typeahead.js';
+// app/components/ui/typeahead.js
 
-export default Ember.Component.extend({
-  classNames: ['twitter-typeahead'],
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { debounce } from 'lodash-es';
 
-  init() {
-    this._super(...arguments);
-  },
+export default class UiTypeaheadComponent extends Component {
+  @tracked query = '';
+  @tracked isOpen = false;
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    let templates = {
-      ...(this.showNotFound && { notFound: this.showNotFound }),
-      ...(this.header && { header: this.header }),
-      ...(this.footer && { footer: this.footer }),
-      ...(this.pending && { pending: this.pending }),
-    };
+  debouncedCloseDropdown = debounce(() => {
+    this.isOpen = false;
+  }, 200);
 
-    this.set('templates', templates);
-  },
+  get placeholder() {
+    return this.args.placeholder || '';
+  }
 
-  didInsertElement() {
-    this._super(...arguments);
+  get minLength() {
+    return this.args.minLength ?? 1;
+  }
 
-    let options = this.getOptions();
-    let dataSetOptions = this.getDataSetOptions();
+  get optionLabelPath() {
+    return this.args.optionLabelPath || 'id';
+  }
 
-    let path = this.optionLabelPath;
-    let selectedValue = this.selectedValue;
+  get filteredList() {
+    const dataList = this.args.dataList || [];
+    const query = this.query.trim().toLowerCase();
 
-    const that = this;
-
-    $('.typeahead').typeahead(options, dataSetOptions);
-
-    let startingValue;
-
-    if (selectedValue === undefined || selectedValue === null) {
-      startingValue = '';
-    } else if (typeof selectedValue === 'string') {
-      startingValue = selectedValue;
-    } else if (typeof selectedValue === 'object') {
-      let val = selectedValue.get(path);
-      if (val) {
-        startingValue = val;
-      } else {
-        startingValue = '';
-      }
+    if (query.length < this.minLength) {
+      return [];
     }
 
-    $('.typeahead').typeahead('val', startingValue);
+    return dataList
+      .map((item) => ({
+        item,
+        label: this.getItemLabel(item),
+      }))
+      .filter(({ label }) => label?.toLowerCase().includes(query));
+  }
 
-    $('.typeahead').on('typeahead:select', function (ev, suggestion) {
-      that.set('selectedValue', suggestion);
-      if (typeof that.onSelect === 'function') {
-        that[that.onSelect](suggestion);
-      }
-      if (that.get('allowMultiple')) {
-        that.$('.typeahead').typeahead('val', '');
-      }
-    });
+  get hasResults() {
+    return this.isOpen && this.filteredList.length > 0;
+  }
 
-    if (this.setSelectedValueOnChange) {
-      $('.typeahead').on('typeahead:change', function (ev, val) {
-        if (that.get('isDestroyed') || that.get('isDestroying')) {
-          return;
-        }
-        let selectedValue = that.get('selectedValue');
-        let inputValue = that.$('.typeahead').typeahead('val');
+  getItemLabel(item) {
+    const path = this.args.optionLabelPath || 'id';
+    return typeof item.get === 'function' ? item.get(path) : item[path];
+  }
 
-        if (selectedValue && typeof selectedValue === 'object') {
-          // check if text value in typeahead component is same as the object's path value
-          let str = selectedValue.get(path);
-          if (inputValue === str) {
-            return;
-          }
-        } else if (typeof selectedValue === 'string') {
-          if (selectedValue === inputValue) {
-            return;
-          }
-        }
+  @action
+  updateQuery(event) {
+    this.query = event.target.value;
+    this.isOpen = true;
+  }
 
-        that.set('selectedValue', inputValue);
-        if (typeof that.onSelect === 'function') {
-          that[that.onSelect](inputValue);
-        }
-        if (that.get('allowMultiple')) {
-          that.$('.typeahead').typeahead('val', '');
-        }
-      });
-    }
-  },
-
-  willDestroyElement: function () {
-    this._super(...arguments);
-    $('.typeahead').typeahead('destroy');
-  },
-
-  getOptions: function () {
-    let minLength = this.minLength;
-    let limit = this.limit;
-    let async = this.isAsync;
-    let hint = false;
-    let highlight = true;
-
-    if (
-      minLength === undefined ||
-      minLength === null ||
-      typeof minLength !== 'number'
-    ) {
-      minLength = 1;
+  @action
+  selectItem(entry) {
+    const item = entry.item;
+    if (this.args.onSelect) {
+      this.args.onSelect(item);
     }
 
-    if (limit === undefined || limit === null || typeof limit !== 'number') {
-      limit = 5;
-    }
-
-    let ret = {
-      minLength,
-      limit,
-      async,
-      hint,
-      highlight,
-    };
-
-    // name will default to random number if not provided
-    return ret;
-  },
-
-  getDataSetOptions: function () {
-    let name = this.listName;
-    let dataList = this.dataList;
-    let sourceFunction = this.sourceFunction;
-    let path = this.optionLabelPath;
-    let display = this.display;
-    let templates = this.templates;
-    let isAsync = this.isAsync;
-    let debounceTime = this.debounceTime;
-
-    if (!debounceTime) {
-      debounceTime = 100;
-    }
-
-    let source;
-
-    if (!sourceFunction || typeof sourceFunction !== 'function') {
-      source = this.substringMatcher.call(this, dataList);
-    } else if (!isAsync) {
-      source = sourceFunction.call(this, dataList);
+    if (this.args.allowMultiple) {
+      this.query = '';
+      this.isOpen = false;
     } else {
-      source = window._.debounce(sourceFunction, debounceTime);
+      this.query = entry.label || '';
+      this.isOpen = false;
     }
+  }
 
-    if (!path || typeof path !== 'string') {
-      path = 'id';
-    }
-    if (!display) {
-      display = function (suggestion) {
-        return suggestion.get(path);
-      };
-    }
-    let ret = {
-      source,
-      display,
-      templates,
-    };
-    if (name) {
-      ret.name = name;
-    }
-    return ret;
-  },
-
-  substringMatcher: function (data) {
-    // data should be array of ember objects
-    let path = this.optionLabelPath;
-    if (!path) {
-      path = 'id';
-    }
-
-    if (!data) {
-      data = [];
-    }
-
-    let suggestions;
-    suggestions = data;
-    const that = this;
-
-    return function findMatches(q, cb, async) {
-      var matches, substrRegex, filtered, pool;
-      if (that.get('allowMultiple')) {
-        // filter out already selected items
-        let selectedItems = that.get('selectedItems');
-        if (!selectedItems) {
-          selectedItems = [];
-        }
-        filtered = suggestions.filter((item) => {
-          let alreadySelected = selectedItems.includes(item);
-          return !alreadySelected;
-        });
-      }
-
-      if (filtered) {
-        pool = filtered;
-      } else {
-        pool = suggestions;
-      }
-      // an array that will be populated with substring matches
-      matches = [];
-      // regex used to determine if a string contains the substring `q`
-      substrRegex = new RegExp(q, 'i');
-      // iterate through the pool of strings and for any string that
-      // contains the substring `q`, add it to the `matches` array
-      matches = pool.filter((obj) => {
-        let str = obj.get(path);
-        return substrRegex.test(str);
-      });
-
-      cb(matches);
-    };
-  },
-});
+  @action
+  handleBlur() {
+    this.debouncedCloseDropdown();
+  }
+}
