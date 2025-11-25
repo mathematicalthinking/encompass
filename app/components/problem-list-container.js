fcf5eff3346ac0dfce17ca374f05802f8a031d3f
aadhakal <33aashish.dhakal@gmail.com>
2025-11-25 16:08:50 -0500

    fix(problem-list-container): send search parameters to API
    - Add buildSearchBy() method to build search query parameters
    - Update getProblems() to include searchBy in API request when searching
    - Fixes pre-existing bug where search didn't send criterion/query to backend
    - Problem search now works correctly with SearchBar criterion dropdown

--- app/components/problem-list-container.js
+++ app/components/problem-list-container.js
@@ -356,6 +356,15 @@ export default class ProblemListContainerComponent extends Component {
     );
   }
 
+  buildSearchBy() {
+    let criterion = this.searchCriterion;
+    let query = this.searchQuery;
+    return {
+      criterion,
+      query,
+    };
+  }
+
   configureCategoryFilter() {
     this.inputState.createStates(this.categoryFilterName, [
       {
@@ -488,6 +497,9 @@ export default class ProblemListContainerComponent extends Component {
     if (!this.showOnlyTrashed) {
       queryParams.filterBy = this.buildFilterBy();
       queryParams.sortBy = this.buildSortBy();
+      if (this.isDisplayingSearchResults) {
+        queryParams.searchBy = this.buildSearchBy();
+      }
     } else {
       queryParams.isTrashedOnly = true;
     }
