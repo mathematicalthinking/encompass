cbbcca73382bb4ef61125230b8c84d21cc92c525
exidy80 <exidy80@gmail.com>
2026-02-02 10:18:11 -0600

    first try at full updating of workspace creation

--- app/components/submission-viewer-list.js
+++ app/components/submission-viewer-list.js
@@ -1,45 +1,90 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import $ from 'jquery';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  elementId: 'submission-viewer-list',
-  scrollBottom: true,
+/**
+ * SubmissionViewerList Component
+ *
+ * Displays a list of submission answers with selection capability and scroll navigation.
+ *
+ * @component
+ * @example
+ * <SubmissionViewerList
+ *   @answers={{this.sortedAnswers}}
+ *   @metadata={{this.answersMetadata}}
+ *   @isList={{true}}
+ *   @isGrid={{false}}
+ *   @moreMenuOptions={{this.moreMenuOptions}}
+ *   @onSelect={{this.updateSelectedAnswers}}
+ *   @selectedAnswers={{this.selectedAnswers}}
+ *   @threads={{this.submissionThreads}}
+ * />
+ */
+export default class SubmissionViewerListComponent extends Component {
+  @tracked scrollBottom = true;
+  @tracked showScrollIcon = false;
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-  },
+  // Scroll threshold in pixels before showing scroll icon
+  SCROLL_THRESHOLD = 800;
 
-  answersSelectedHash: computed(
-    'answers.[]',
-    'selectedAnswers.[]',
-    function () {
-      let hash = {};
+  // Bound scroll handler reference for cleanup
+  _scrollHandler = null;
 
-      this.answers.forEach((answer) => {
-        let isSelected = this.selectedAnswers.includes(answer);
-        hash[answer.get('id')] = isSelected;
+  get answersSelectedHash() {
+    let hash = {};
+    const answers = this.args.answers ?? [];
+    const selectedAnswers = this.args.selectedAnswers ?? [];
+
+    answers.forEach((answer) => {
+      let isSelected = selectedAnswers.includes(answer);
+      hash[answer.get('id')] = isSelected;
+    });
+    return hash;
+  }
+
+  @action
+  onSelect(answer, isChecked) {
+    this.args.onSelect?.(answer, isChecked);
+  }
+
+  @action
+  superScroll() {
+    if (!this.scrollBottom) {
+      // Scroll to top
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
       });
-      return hash;
+    } else {
+      // Scroll to bottom
+      window.scrollTo({
+        top: document.documentElement.scrollHeight - window.innerHeight,
+        behavior: 'smooth',
+      });
     }
-  ),
+    this.scrollBottom = !this.scrollBottom;
+  }
 
-  actions: {
-    onSelect: function (answer, isChecked) {
-      this.onSelect(answer, isChecked);
-    },
-    superScroll: function () {
-      //should only show scroll option after the user scrolls a little
-      if (!this.scrollBottom) {
-        $('html, body').animate({
-          scrollTop: 0,
-        });
-      } else {
-        $('html, body').animate({
-          scrollTop: $(document).height() - $(window).height(),
-        });
-      }
-      this.set('scrollBottom', !this.scrollBottom);
-    },
-  },
-});
+  @action
+  setupScrollListener(element) {
+    this._scrollIconElement = element;
+
+    this._scrollHandler = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      this.showScrollIcon = scrollY > this.SCROLL_THRESHOLD;
+    };
+
+    window.addEventListener('scroll', this._scrollHandler, { passive: true });
+
+    // Initial check
+    this._scrollHandler();
+  }
+
+  @action
+  teardownScrollListener() {
+    if (this._scrollHandler) {
+      window.removeEventListener('scroll', this._scrollHandler);
+      this._scrollHandler = null;
+    }
+  }
+}
