99bb72031bac8cc48e4527be83a13fa0365ca7c8
exidy80 <exidy80@gmail.com>
2026-04-08 15:50:35 -0400

    replace rejectBy (being phased out) with vanilla JS filter

--- app/routes/sections/new.js
+++ app/routes/sections/new.js
@@ -16,7 +16,7 @@ export default class SectionsNewRoute extends AuthenticatedRoute {
     const organizations = this.store.findAll('organization');
     const sections = this.store.findAll('section');
     const users = await this.store.query('user', {});
-    const addableTeachers = users.rejectBy('accountType', 'S');
+    const addableTeachers = users.filter(user => user.accountType !== 'S');
     const organization = await this.currentUser.user.organization;
     return hash({
       organization,
