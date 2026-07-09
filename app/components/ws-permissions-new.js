857cf9e5df34198b46608619f1cb17f3354db250
aadhakal <33aashish.dhakal@gmail.com>
2026-07-09 17:50:46 -0400

    refactor(ws-permissions-new): classic → Glimmer
    - ws-permissions-new.js: Component.extend → @glimmer/component; state → @tracked,
      config → fields, computeds → getters, actions → @action; didReceiveAttrs prefill
      → constructor + {{did-update}} on @selectedUser; remove observer/willDestroyElement
    - fix two-way bindings: Cancel → parent @stopEditing (was mut selectedUser);
      observer-synced sub-view flag → @onSubViewChange callback
    - ws-permissions-new.hbs: co-locate; explicit #ws-permissions-new wrapper (was
      elementId); {{on}}/closures; Show Viewer span → button

--- app/components/ws-permissions-new.js
+++ app/components/ws-permissions-new.js
@@ -1,41 +1,30 @@
-import Component from '@ember/component';
-import { computed, observer } from '@ember/object';
-import { equal } from '@ember/object/computed';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { service } from '@ember/service';
 import each from 'lodash-es/each';
 import isArray from 'lodash-es/isArray';
 
-export default Component.extend({
-  elementId: 'ws-permissions-new',
-  utils: service('utility-methods'),
-  showCustom: equal('global', 'custom'),
-  showCustomSubmissions: computed(
-    'submissions',
-    'showCustomSubmissionViewer',
-    function () {
-      return this.submissions === 'custom' && this.showCustomSubmissionViewer;
-    }
-  ),
-  showCustomSubmissionViewer: true,
-  closedCustomView: computed(
-    'showCustomSubmissionViewer',
-    'submissions',
-    function () {
-      return this.submissions === 'custom' && !this.showCustomSubmissionViewer;
-    }
-  ),
+export default class WsPermissionsNewComponent extends Component {
+  @service('utility-methods') utils;
 
-  global: 'viewOnly',
-  submissionItems: {
+  @tracked global = 'viewOnly';
+  @tracked submissions = 'all';
+  @tracked comments = 1;
+  @tracked selections = 1;
+  @tracked feedback = 'authReq';
+  @tracked folders = 1;
+  @tracked customSubmissionIds = [];
+  @tracked showCustomSubmissionViewer = true;
+  @tracked saveError = null;
+
+  submissionItems = {
     groupName: 'submissions',
     groupLabel: 'Accessible Submissions',
     info: 'Accessible submissions dictate what submissions this user will see in the workspace. Hover over the question marks for more info',
     required: true,
     inputs: [
-      {
-        label: 'All',
-        value: 'all',
-      },
+      { label: 'All', value: 'all' },
       {
         label: 'Own Only',
         value: 'userOnly',
@@ -49,19 +38,15 @@ export default Component.extend({
           'View all submissions and select specific ones this user can see',
       },
     ],
-  },
+  };
 
-  folderItems: {
+  folderItems = {
     groupName: 'folders',
     groupLabel: 'Folder Permissions',
     info: 'Folder permissions decide what users can do with folders in this workspace. Delete is the highest setting which means this user can do anything related to folders',
     required: true,
     inputs: [
-      {
-        label: 'None',
-        value: 0,
-        moreInfo: 'User will see no folders',
-      },
+      { label: 'None', value: 0, moreInfo: 'User will see no folders' },
       {
         label: 'View Only',
         value: 1,
@@ -79,18 +64,15 @@ export default Component.extend({
           'User will be able to rename, reorder, and delete existing folders',
       },
     ],
-  },
-  selectionItems: {
+  };
+
+  selectionItems = {
     groupName: 'selections',
     groupLabel: 'Selection Permissions',
     info: 'Selection permissions decide what users can do with selections in this workspace. Delete is the highest setting which means this user can do anything related to selections. If you can create selections, you can file them in any available folders.',
     required: true,
     inputs: [
-      {
-        label: 'None',
-        value: 0,
-        moreInfo: 'User will see no selections',
-      },
+      { label: 'None', value: 0, moreInfo: 'User will see no selections' },
       {
         label: 'View Only',
         value: 1,
@@ -112,18 +94,15 @@ export default Component.extend({
         moreInfo: 'User will be able to see, add, edit, and delete selections',
       },
     ],
-  },
-  commentItems: {
+  };
+
+  commentItems = {
     groupName: 'comments',
     groupLabel: 'Comment Permissions',
     info: 'Comment permissions decide what users can do with comments in this workspace. Delete is the highest setting which means this user can do anything related to comments',
     required: true,
     inputs: [
-      {
-        label: 'None',
-        value: 0,
-        moreInfo: 'User will see no comments',
-      },
+      { label: 'None', value: 0, moreInfo: 'User will see no comments' },
       {
         label: 'View Only',
         value: 1,
@@ -145,9 +124,9 @@ export default Component.extend({
         moreInfo: 'User will be able to see, add, edit and delete comments',
       },
     ],
-  },
+  };
 
-  feedbackItems: {
+  feedbackItems = {
     groupName: 'feedback',
     groupLabel: 'Feedback Permissions',
     info: 'Feedback permissions dictate whether this user can send feedback to the creator of the submissions. Hover over the question marks for more info.',
@@ -177,9 +156,9 @@ export default Component.extend({
           'User can send feedback directly and approve other feedback that is pending approval',
       },
     ],
-  },
+  };
 
-  globalItems: {
+  globalItems = {
     groupName: 'global',
     groupLabel: 'Workspace Permissions',
     info: 'Workspace permissions apply to all aspects of a workspace for this user. This means whatever you select applies to all the selections, comments, folders, etc.',
@@ -222,68 +201,87 @@ export default Component.extend({
           'Select this if you want to set permissions for each aspect of a workspace',
       },
     ],
-  },
+  };
 
-  folders: 1,
-  submissions: 'all',
-  comments: 1,
-  selections: 1,
-  feedback: 'authReq',
-  customSubmissionIds: [],
+  constructor() {
+    super(...arguments);
+    // prefill for editing an existing collaborator's permissions
+    this.prefillFromPermissions();
+    // let the parent know whether the custom submission viewer is showing
+    this.syncSubView();
+  }
 
-  didReceiveAttrs() {
-    this._super();
-    const selectedUserId = this.get('selectedUser.id');
-    const permissions = this.permissions;
+  get showCustom() {
+    return this.global === 'custom';
+  }
+
+  get showCustomSubmissions() {
+    return this.submissions === 'custom' && this.showCustomSubmissionViewer;
+  }
+
+  get closedCustomView() {
+    return this.submissions === 'custom' && !this.showCustomSubmissionViewer;
+  }
+
+  prefillFromPermissions() {
+    const selectedUserId = this.args.selectedUser?.id;
+    const permissions = this.args.permissions;
     const utils = this.utils;
 
     if (
-      !utils.isNullOrUndefined(selectedUserId) &&
-      utils.isNonEmptyArray(permissions)
+      utils.isNullOrUndefined(selectedUserId) ||
+      !utils.isNonEmptyArray(permissions)
     ) {
-      const userPermissions = permissions.find((obj) => {
-        let user = obj.user;
-        if (utils.isNonEmptyObject(user)) {
-          return user.get('id') === selectedUserId;
-        }
-        return user === selectedUserId;
-      });
+      return;
+    }
 
-      if (utils.isNonEmptyObject(userPermissions)) {
-        //prefill for editing
-        each(
-          ['folders', 'comments', 'selections', 'feedback', 'global'],
-          (prop) => {
-            let val = userPermissions[prop];
-            if (!utils.isNullOrUndefined(val)) {
-              this.set(prop, val);
-            }
-          }
-        );
+    const userPermissions = permissions.find((obj) => {
+      let user = obj.user;
+      if (utils.isNonEmptyObject(user)) {
+        return user.get('id') === selectedUserId;
+      }
+      return user === selectedUserId;
+    });
 
-        const submissions = userPermissions.submissions;
-        if (utils.isNonEmptyObject(submissions)) {
-          if (submissions.all === true) {
-            this.set('submissions', 'all');
-          } else if (submissions.userOnly === true) {
-            this.set('submissions', 'userOnly');
-          } else if (isArray(submissions.submissionIds)) {
-            this.set('submissions', 'custom');
-            this.set('customSubmissionIds', [...submissions.submissionIds]);
-          }
+    if (!utils.isNonEmptyObject(userPermissions)) {
+      return;
+    }
+
+    each(
+      ['folders', 'comments', 'selections', 'feedback', 'global'],
+      (prop) => {
+        let val = userPermissions[prop];
+        if (!utils.isNullOrUndefined(val)) {
+          this[prop] = val;
         }
       }
+    );
+
+    const submissions = userPermissions.submissions;
+    if (utils.isNonEmptyObject(submissions)) {
+      if (submissions.all === true) {
+        this.submissions = 'all';
+      } else if (submissions.userOnly === true) {
+        this.submissions = 'userOnly';
+      } else if (isArray(submissions.submissionIds)) {
+        this.submissions = 'custom';
+        this.customSubmissionIds = [...submissions.submissionIds];
+      }
     }
-  },
+  }
 
-  willDestroyElement() {
-    this.set('selectedUser', null);
-    this._super(...arguments);
-  },
+  syncSubView() {
+    this.args.onSubViewChange?.(this.showCustomSubmissions);
+  }
 
-  isShowingCustomSubs: observer('showCustomSubmissions', function () {
-    this.set('isShowingSubView', this.showCustomSubmissions);
-  }),
+  // re-prefill when the edited collaborator changes without the component being
+  // recreated (the collaborator list and this editor can be open at once), which
+  // is what the classic didReceiveAttrs used to handle.
+  @action
+  prefillOnUserChange() {
+    this.prefillFromPermissions();
+    this.syncSubView();
+  }
 
   buildCustomSubmissionIds(submissionsValue) {
     if (submissionsValue === 'custom') {
@@ -291,14 +289,12 @@ export default Component.extend({
       if (this.utils.isNonEmptyArray(ids)) {
         return ids;
       }
-      return [];
     }
-
     return [];
-  },
+  }
 
   buildPermissionsObject() {
-    const user = this.selectedUser;
+    const user = this.args.selectedUser;
     const globalSetting = this.global;
     const submissions = this.submissions;
 
@@ -321,108 +317,127 @@ export default Component.extend({
       submissions: submissionOptions,
       global: globalSetting,
     };
-    if (globalSetting === 'viewOnly') {
-      results.folders = 1;
-      results.selections = 1;
-      results.comments = 1;
-      results.feedback = 'none';
 
-      return results;
+    if (globalSetting === 'viewOnly') {
+      return {
+        ...results,
+        folders: 1,
+        selections: 1,
+        comments: 1,
+        feedback: 'none',
+      };
     }
     if (globalSetting === 'editor') {
-      results.folders = 3;
-      results.selections = 4;
-      results.comments = 4;
-      results.feedback = 'none';
-
-      return results;
+      return {
+        ...results,
+        folders: 3,
+        selections: 4,
+        comments: 4,
+        feedback: 'none',
+      };
     }
     if (globalSetting === 'indirectMentor') {
-      results.folders = 2;
-      results.selections = 2;
-      results.comments = 2;
-      results.feedback = 'authReq';
-
-      return results;
+      return {
+        ...results,
+        folders: 2,
+        selections: 2,
+        comments: 2,
+        feedback: 'authReq',
+      };
     }
     if (globalSetting === 'directMentor') {
-      results.folders = 2;
-      results.selections = 2;
-      results.comments = 2;
-      results.feedback = 'preAuth';
-
-      return results;
+      return {
+        ...results,
+        folders: 2,
+        selections: 2,
+        comments: 2,
+        feedback: 'preAuth',
+      };
     }
     if (globalSetting === 'approver') {
-      results.folders = 3;
-      results.selections = 4;
-      results.comments = 4;
-      results.feedback = 'approver';
-
-      return results;
+      return {
+        ...results,
+        folders: 3,
+        selections: 4,
+        comments: 4,
+        feedback: 'approver',
+      };
     }
-    results.folders = this.folders;
-    results.selections = this.selections;
-    results.comments = this.comments;
-    results.feedback = this.feedback;
 
-    return results;
-  },
+    return {
+      ...results,
+      folders: this.folders,
+      selections: this.selections,
+      comments: this.comments,
+      feedback: this.feedback,
+    };
+  }
 
-  actions: {
-    updateSubmissions(val) {
-      this.set('submissions', val);
-    },
-    updateGlobal(val) {
-      this.set('global', val);
-    },
-    updateSelections(val) {
-      this.set('selections', val);
-    },
-    updateComments(val) {
-      this.set('comments', val);
-    },
-    updateFolders(val) {
-      this.set('folders', val);
-    },
-    updateFeedback(val) {
-      this.set('feedback', val);
-    },
-    savePermissions() {
-      if (this.saveError) {
-        this.set('saveError', null);
-      }
-      const permissions = this.buildPermissionsObject();
-      if (this.utils.isNonEmptyObject(permissions)) {
-        this.onSave(permissions);
-        return;
-      }
-      this.set('saveError', true);
-    },
-    updateCustomSubs(id) {
-      if (!this.utils.isNonEmptyArray(this.customSubmissionIds)) {
-        this.set('customSubmissionIds', []);
-      }
+  @action updateSubmissions(val) {
+    this.submissions = val;
+    this.syncSubView();
+  }
 
-      const customSubmissionIds = this.customSubmissionIds;
+  @action updateGlobal(val) {
+    this.global = val;
+  }
 
-      const isIn = customSubmissionIds.includes(id);
-      if (isIn) {
-        // remove
-        customSubmissionIds.removeObject(id);
-      } else {
-        //add
-        customSubmissionIds.addObject(id);
-      }
-    },
-    selectAllSubmissions: function () {
-      this.set(
-        'customSubmissionIds',
-        this.get('workspace.submissions').mapBy('id')
-      );
-    },
-    deselectAllSubmissions: function () {
-      this.set('customSubmissionIds', []);
-    },
-  },
-});
+  @action updateSelections(val) {
+    this.selections = val;
+  }
+
+  @action updateComments(val) {
+    this.comments = val;
+  }
+
+  @action updateFolders(val) {
+    this.folders = val;
+  }
+
+  @action updateFeedback(val) {
+    this.feedback = val;
+  }
+
+  @action savePermissions() {
+    if (this.saveError) {
+      this.saveError = null;
+    }
+    const permissions = this.buildPermissionsObject();
+    if (this.utils.isNonEmptyObject(permissions)) {
+      this.args.onSave(permissions);
+      return;
+    }
+    this.saveError = true;
+  }
+
+  @action updateCustomSubs(id) {
+    const ids = this.utils.isNonEmptyArray(this.customSubmissionIds)
+      ? this.customSubmissionIds
+      : [];
+    this.customSubmissionIds = ids.includes(id)
+      ? ids.filter((existing) => existing !== id)
+      : [...ids, id];
+  }
+
+  @action selectAllSubmissions() {
+    this.customSubmissionIds = this.args.workspace.submissions.mapBy('id');
+  }
+
+  @action deselectAllSubmissions() {
+    this.customSubmissionIds = [];
+  }
+
+  @action showViewer() {
+    this.showCustomSubmissionViewer = true;
+    this.syncSubView();
+  }
+
+  @action closeCustomSelect() {
+    this.showCustomSubmissionViewer = false;
+    this.syncSubView();
+  }
+
+  @action resetSaveError() {
+    this.saveError = null;
+  }
+}
